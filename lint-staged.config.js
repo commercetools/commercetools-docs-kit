ac7d01d50266e9671199996d486e0fbfb05e3703
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-05 22:04:49 +0100

    chore: setup lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  '*.{md,mdx}': ['prettier --write --parser markdown', 'git add'],
+  '*.yaml': ['prettier --write --parser yaml', 'git add'],
+  '*.js': [
+    'prettier --write',
+    'git add',
+    // NOTE: apparently if you pass some argument that is not a flag AFTER the `reporters`
+    // flag, jest does not seem correctly parse the arguments.
+    //
+    //   No tests found related to files changed since last commit.
+    //   Run Jest without `-o` or with `--all` to run all tests.
+    //   Error: An error occurred while adding the reporter at path "/path/to/file".Reporter is not a constructor
+    //
+    // For that reason, we move the `--onlyChanged` flag next to it.
+    'yarn lint:js --reporters=jest-silent-reporter --onlyChanged',
+  ],
+};
