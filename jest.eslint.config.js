4be17af7a85509417468fe0c2e06c34f1f0636ce
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-05 16:29:32 +0100

    chore: setup eslint (#2)
    * chore: setup eslint
    
    * chore: add env config to eslint

---
+++ jest.eslint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  runner: 'jest-runner-eslint',
+  displayName: 'eslint',
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  modulePathIgnorePatterns: ['dist', 'public'],
+  testMatch: ['<rootDir>/**/*.js', '<rootDir>/**/*.ts', '<rootDir>/**/*.tsx'],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-master'],
+};
