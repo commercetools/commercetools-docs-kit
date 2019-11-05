6ae4871cf521ac66d7bc441b1d2bb8869914c2be
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-05 21:35:02 +0100

    feat(theme): export some of the things needed in the appkit website

--- packages/gatsby-theme-docs/index.js
+++ packages/gatsby-theme-docs/index.js
@@ -1,1 +1,12 @@
-// no-op file required for loading the plugin inside the private workspace
+import * as designSystem from './src/design-system';
+
+export { designSystem };
+export { Card, SEO, Spacings, IconButton, Markdown } from './src/components';
+export {
+  default as LayoutApplication,
+} from './src/layouts/internals/layout-application';
+export { default as LayoutHeader } from './src/layouts/internals/layout-header';
+export { default as LayoutFooter } from './src/layouts/internals/layout-footer';
+export { default as LayoutMain } from './src/layouts/internals/layout-main';
+export { default as Globals } from './src/layouts/internals/globals';
+export { default as createStyledIcon } from './src/utils/create-styled-icon';
