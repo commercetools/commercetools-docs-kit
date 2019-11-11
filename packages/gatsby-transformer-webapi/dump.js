6ef0dc56279d9f76d6f1499364b88848de88109c
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2019-11-11 23:43:26 +0100

    feat: add metadata collector via the programmatic API that re-collects the originally declared data type name

--- packages/gatsby-transformer-webapi/dump.js
+++ packages/gatsby-transformer-webapi/dump.js
@@ -17,6 +17,34 @@ async function dump(ramlFile) {
   );
   const resolver = webapi.Resolver('RAML 1.0');
   const resolved = resolver.resolve(model, 'editing'); // Note the 'editing' argument, inoffical flag to retain the types
+
+  // const payload = resolved.encodes.endPoints[0].operations[0].responses[0].payloads[0]
+  // const typeName = payload.schema.name.value()
+  const metaData = {
+    resources: {},
+    types: {},
+  };
+
+  resolved.encodes.endPoints.forEach(endpoint => {
+    const endpointMeta = { operations: {} };
+    metaData.resources[endpoint.path.value()] = endpointMeta;
+    endpoint.operations.forEach(operation => {
+      const operationMeta = { responses: {} };
+      endpointMeta.operations[operation.method.value()] = operationMeta;
+      operation.responses.forEach(response => {
+        const responseMeta = { payloads: {} };
+        operationMeta.responses[response.statusCode.value()] = responseMeta;
+        response.payloads.forEach(payload => {
+          const payloadMeta = {
+            declaredTypeName: payload.schema.name.value(),
+            declaredTypeDisplayName: payload.schema.displayName.value(),
+          };
+          responseMeta.payloads[payload.mediaType.value()] = payloadMeta;
+        });
+      });
+    });
+  });
+
   await webapi.WebApiParser.raml10.generateFile(
     resolved,
     `file://${absolutePath}.resolved.raml`
