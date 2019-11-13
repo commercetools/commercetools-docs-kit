01bb78dd92cae4d8c2e6dca88106ac6437a38c88
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-13 20:37:54 +0100

    feat(theme): setup Sentry and ErrorBoundary

--- packages/gatsby-theme-docs/gatsby-browser.js
+++ packages/gatsby-theme-docs/gatsby-browser.js
@@ -6,3 +6,25 @@
 
 require('prism-themes/themes/prism-dracula.css');
 require('prismjs/plugins/command-line/prism-command-line.css');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const commitSha = process.env.NOW_GITHUB_COMMIT_SHA;
+const environment =
+  process.env.NOW_GITHUB_COMMIT_REF === 'master' ? 'production' : 'preview';
+
+exports.onClientEntry = (
+  _, // eslint-disable-line
+  pluginOptions
+) => {
+  if (isProduction) {
+    require.ensure(['@sentry/browser'], require => {
+      const Sentry = require('@sentry/browser');
+      Sentry.init({
+        dsn: 'https://e43538aae75e412eb16b27d8011f5a8b@sentry.io/1819068',
+        release: commitSha,
+        environment: `${environment}-${pluginOptions.websiteName}`,
+        whitelistUrls: ['docs.commercetools.com', 'now.sh'],
+      });
+    });
+  }
+};
--- websites/docs-smoke-test/gatsby-config.js
+++ websites/docs-smoke-test/gatsby-config.js
@@ -8,7 +8,10 @@ module.exports = {
   plugins: [
     {
       resolve: '@commercetools-docs/gatsby-theme-docs',
-      options: { gaTrackingId: 'UA-38285631-3' },
+      options: {
+        websiteName: 'docs-smoke-test',
+        gaTrackingId: 'UA-38285631-3',
+      },
     },
   ],
 };
