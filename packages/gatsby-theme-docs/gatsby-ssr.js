54f783560e0efa1d82c138f339da365c9a15a7f2
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-13 23:14:09 +0100

    feat(theme): setup cookie consent script

--- packages/gatsby-theme-docs/gatsby-ssr.js
+++ packages/gatsby-theme-docs/gatsby-ssr.js
@@ -3,5 +3,23 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+import React from 'react';
 
-// You can delete this file if you're not using it
+// eslint-disable-next-line import/prefer-default-export
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  // Activate the cookie consent banner only on the live website environment.
+  // We can narrow it down to the build step of Zeit Now for the master branch.
+  if (
+    process.env.NODE_ENV === 'production' &&
+    Boolean(process.env.NOW_GITHUB_DEPLOYMENT) &&
+    process.env.NOW_GITHUB_COMMIT_REF === 'master'
+  )
+    setPostBodyComponents([
+      <script
+        key="cookie-consent"
+        type="text/javascript"
+        src="https://cdn.cookielaw.org/langswitch/99bb6fae-765a-4fc5-9b6e-8c89a353335d.js"
+        onLoad="function OptanonWrapper() {};"
+      />,
+    ]);
+};
