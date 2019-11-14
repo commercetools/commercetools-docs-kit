92228e719c5de8e03eeb6951286a3563cf38bed7
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-14 20:57:24 +0100

    refactor(e2e): take snapshots from empty page

--- cypress/integration/docs-smoke-test/menu-navigation.js
+++ cypress/integration/docs-smoke-test/menu-navigation.js
@@ -4,7 +4,7 @@ describe('Menu navigation', () => {
   describe('when viewport is smaller than desktop', () => {
     it('should toggle main left side navigation', () => {
       cy.setTabletViewport();
-      cy.visit(URL_DOCS_SMOKE_TEST);
+      cy.visit(`${URL_DOCS_SMOKE_TEST}/views/empty`);
       cy.findByLabelText('Open main navigation').click();
       cy.get('aside').should('be.visible');
       cy.get('aside').within(() => {
--- cypress/integration/docs-smoke-test/viewports.js
+++ cypress/integration/docs-smoke-test/viewports.js
@@ -3,7 +3,7 @@ import { URL_DOCS_SMOKE_TEST } from '../../support/urls';
 describe('Viewports', () => {
   it('should take snapshots for mobile, tablet and desktop sizes', () => {
     cy.setDesktopViewport();
-    cy.visit(URL_DOCS_SMOKE_TEST);
+    cy.visit(`${URL_DOCS_SMOKE_TEST}/views/empty`);
     cy.findAllByText('Docs Smoke Test');
     cy.percySnapshot(cy.state('runnable').fullTitle(), {
       widths: [512, 1200, 1634],
