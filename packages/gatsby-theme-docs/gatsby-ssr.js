62ad3c1ef38711cf7330267505480bfffc49cb14
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-14 12:59:21 +0100

    refactor(theme): move env vars outside of function

--- packages/gatsby-theme-docs/gatsby-ssr.js
+++ packages/gatsby-theme-docs/gatsby-ssr.js
@@ -5,16 +5,16 @@
  */
 import React from 'react';
 
+const isProduction = process.env.NODE_ENV === 'production';
+const isNowBuild = Boolean(process.env.NOW_GITHUB_DEPLOYMENT);
+const isMasterBranch = process.env.NOW_GITHUB_COMMIT_REF === 'master';
+console.log('[DEBUG] Environment variables', process.env);
+
 // eslint-disable-next-line import/prefer-default-export
 export const onRenderBody = ({ setPostBodyComponents }) => {
   // Activate the cookie consent banner only on the live website environment.
   // We can narrow it down to the build step of Zeit Now for the master branch.
-  console.log('[DEBUG] Environment variables', process.env);
-  if (
-    process.env.NODE_ENV === 'production' &&
-    Boolean(process.env.NOW_GITHUB_DEPLOYMENT) &&
-    process.env.NOW_GITHUB_COMMIT_REF === 'master'
-  )
+  if (isProduction && isNowBuild && isMasterBranch)
     setPostBodyComponents([
       <script
         key="cookie-consent"
