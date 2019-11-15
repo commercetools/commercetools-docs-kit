5b923136e5f1d7df6ca154e5ac944b4690cc6530
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-15 09:59:59 +0100

    test(e2e): improve links interaction

--- cypress/integration/docs-smoke-test/images.js
+++ cypress/integration/docs-smoke-test/images.js
@@ -2,10 +2,14 @@ import { URL_DOCS_SMOKE_TEST } from '../../support/urls';
 
 describe('Images', () => {
   it('should take a clean snapshot of the images page', () => {
+    cy.setDesktopViewport();
     cy.visit(`${URL_DOCS_SMOKE_TEST}components/images`);
     cy.get('[role="page-navigation"]').within(() => {
-      cy.findByText('Last section').click();
-      cy.url().should('include', '#last-section');
+      cy.get('a').each($el => {
+        cy.wrap($el).click();
+        cy.url().should('include', $el[0].getAttribute('href'));
+        cy.wait(500); // give it a moment for the images to properly load
+      });
       cy.percySnapshot();
     });
   });
