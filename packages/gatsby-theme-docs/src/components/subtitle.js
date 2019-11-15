f5a9eec7d79b6bd521682614a92bfa31e20fcc36
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-15 16:08:28 +0100

    fix(theme): use div element for subtitle

--- packages/gatsby-theme-docs/src/components/subtitle.js
+++ packages/gatsby-theme-docs/src/components/subtitle.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { typography } from '../design-system';
 
-const Subtitle = styled.p`
+const Subtitle = styled.div`
   font-size: ${typography.fontSizes.h3};
 `;
 
