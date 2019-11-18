25a49bbd5451d22162a604d3e4f58f42674d4bce
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2019-11-18 15:29:43 +0100

    refactor: change name of replacement string in template to not contain whitespace and use it consistently

--- websites/site-template/gatsby-config.js
+++ websites/site-template/gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  pathPrefix: '/site-template',
+  pathPrefix: '/CHANGE-ME',
   siteMetadata: {
-    title: 'CHANGE ME',
-    description: 'CHANGE ME',
+    title: 'CHANGE-ME',
+    description: 'CHANGE-ME',
     author: 'commercetools',
   },
   plugins: [
     {
       resolve: '@commercetools-docs/gatsby-theme-docs',
       options: {
-        websiteName: 'CHANGE ME',
-        /* gaTrackingId: 'CHANGE OR REMOVE ME' */ // to enable trackign with Google Analytics, enter the site ID here
+        websiteName: 'CHANGE-ME',
+        /* gaTrackingId: 'CHANGE-ME' */ // to enable trackign with Google Analytics, enter the site ID here
       },
     },
   ],
