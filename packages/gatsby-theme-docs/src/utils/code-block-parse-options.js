627df48f588b5aa2ba92ed8bb8e5b468b0548a97
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-20 15:11:16 +0100

    refactor(code-block): use parse5 library to do the infostring parsing

--- packages/gatsby-theme-docs/src/utils/code-block-parse-options.js
+++ packages/gatsby-theme-docs/src/utils/code-block-parse-options.js
@@ -1,6 +1,5 @@
+import { parseFragment } from 'parse5';
 import rangeParser from 'parse-numeric-range';
-
-const regexInfoStringGroups = /(\w+=(['"][\w\s-]+['"]))+/g;
 
 const getLinesForRange = value => {
   if (!value) return [];
@@ -16,22 +15,18 @@ const intialOptions = {
 export default function parseCodeBlockOptions(props = {}) {
   if (!props.metastring) return intialOptions;
 
-  const matchedOptions = props.metastring.match(regexInfoStringGroups);
-  if (!matchedOptions) return intialOptions;
-
   // We need to parse the `metastring` value on our own as the default
   // implementation does not support values with whitespaces.
   // https://github.com/mdx-js/mdx/blob/e95e2c114bead01164a8af068ae052cebf1534c2/packages/mdx/mdx-ast-to-mdx-hast.js#L38-L46
   // We mostly need this for things like `title="This is a title"`.
-  const normalizedOptions = matchedOptions.reduce(
-    (normalizedGroup, stringGroup) => {
-      const [key, value] = stringGroup.split('=');
-      return {
-        ...normalizedGroup,
-        // Strip quotes wrapping the text value
-        [key]: value.replace(/(['"](.*)['"])/g, '$2'),
-      };
-    },
+  const parsedOptions = parseFragment(`<x ${props.metastring} >`);
+  if (!parsedOptions || parsedOptions.childNodes === 0) return intialOptions;
+
+  const normalizedOptions = parsedOptions.childNodes[0].attrs.reduce(
+    (normalizedAttributes, attribute) => ({
+      ...normalizedAttributes,
+      [attribute.name]: attribute.value,
+    }),
     {}
   );
   return {
