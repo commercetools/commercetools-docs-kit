3ad8b4d8da6f10dc131712bb8d84858f46f8ea01
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-26 09:10:17 +0100

    refactor(theme): use left-aligned layout

--- packages/gatsby-theme-docs/src/layouts/content.js
+++ packages/gatsby-theme-docs/src/layouts/content.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
-import styled from '@emotion/styled';
 import {
   ContentPagination,
   ContentNotifications,
   Markdown,
 } from '../components';
-import { dimensions, tokens } from '../design-system';
+import { dimensions } from '../design-system';
 import PlaceholderPageHeaderSide from '../overrides/page-header-side';
 import LayoutApplication from './internals/layout-application';
 import LayoutHeader from './internals/layout-header';
@@ -19,24 +18,6 @@ import LayoutPageHeaderSide from './internals/layout-page-header-side';
 import LayoutPageNavigation from './internals/layout-page-navigation';
 import LayoutPageContent from './internals/layout-page-content';
 import { useSiteData } from '../hooks/use-site-data';
-
-const GridArea = styled.div`
-  grid-area: ${props => props.name};
-`;
-const ResizableGrid = styled.div`
-  box-shadow: ${tokens.shadow1};
-  position: relative;
-  display: block;
-  width: 100vw;
-
-  @media screen and (${dimensions.viewports.desktop}) {
-    display: grid;
-    grid:
-      [row1-start] 'left center right' auto [row1-end]
-      / minmax(0, 100%) 1fr minmax(0, 100%);
-    width: auto;
-  }
-`;
 
 const LayoutContent = props => {
   const [isMenuOpen, setMenuOpen] = React.useState(false);
@@ -65,56 +46,51 @@ const LayoutContent = props => {
           }
         `}
       >
-        <ResizableGrid>
-          <GridArea name="left" />
-          <GridArea name="right" />
-          <GridArea
-            id="anchor-page-top"
-            name="center"
-            css={css`
-              display: block;
+        <div
+          id="anchor-page-top"
+          css={css`
+            display: block;
 
-              @media screen and (${dimensions.viewports.tablet}) {
-                display: grid;
-                grid-template-columns:
-                  calc(
-                    ${dimensions.widths.pageContent} + ${dimensions.spacings.xl} *
-                      2
-                  )
-                  0;
-                grid-template-rows: auto 1fr;
-              }
-              @media screen and (${dimensions.viewports.largeTablet}) {
-                grid-template-columns:
-                  calc(
-                    ${dimensions.widths.pageContent} + ${dimensions.spacings.xl} *
-                      2
-                  )
-                  ${dimensions.widths.pageNavigation};
-              }
-            `}
-          >
-            <LayoutPageHeader>
-              <Markdown.H1>{props.pageData.frontmatter.title}</Markdown.H1>
-            </LayoutPageHeader>
-            <LayoutPageHeaderSide>
-              <PlaceholderPageHeaderSide />
-            </LayoutPageHeaderSide>
-            <LayoutPageContent>
-              {props.pageData.frontmatter.beta && (
-                <ContentNotifications.BetaInfo />
-              )}
-              {props.children}
-              <ContentPagination slug={props.pageContext.slug} />
-            </LayoutPageContent>
-            <LayoutPageNavigation
-              pageTitle={
-                props.pageContext.shortTitle || props.pageData.frontmatter.title
-              }
-              tableOfContents={props.pageData.tableOfContents}
-            />
-          </GridArea>
-        </ResizableGrid>
+            @media screen and (${dimensions.viewports.tablet}) {
+              display: grid;
+              grid-template-columns:
+                calc(
+                  ${dimensions.widths.pageContent} + ${dimensions.spacings.xl} *
+                    2
+                )
+                0;
+              grid-template-rows: auto 1fr;
+            }
+            @media screen and (${dimensions.viewports.largeTablet}) {
+              grid-template-columns:
+                calc(
+                  ${dimensions.widths.pageContent} + ${dimensions.spacings.xl} *
+                    2
+                )
+                ${dimensions.widths.pageNavigation};
+            }
+          `}
+        >
+          <LayoutPageHeader>
+            <Markdown.H1>{props.pageData.frontmatter.title}</Markdown.H1>
+          </LayoutPageHeader>
+          <LayoutPageHeaderSide>
+            <PlaceholderPageHeaderSide />
+          </LayoutPageHeaderSide>
+          <LayoutPageContent>
+            {props.pageData.frontmatter.beta && (
+              <ContentNotifications.BetaInfo />
+            )}
+            {props.children}
+            <ContentPagination slug={props.pageContext.slug} />
+          </LayoutPageContent>
+          <LayoutPageNavigation
+            pageTitle={
+              props.pageContext.shortTitle || props.pageData.frontmatter.title
+            }
+            tableOfContents={props.pageData.tableOfContents}
+          />
+        </div>
         <LayoutFooter />
       </LayoutMain>
     </LayoutApplication>
