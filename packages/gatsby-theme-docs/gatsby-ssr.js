637482be0405edbf440e26ea0409b3bb5cad89e8
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-26 15:08:06 +0100

    refactor(theme): to add favicon for light/dark theme based on media query

--- packages/gatsby-theme-docs/gatsby-ssr.js
+++ packages/gatsby-theme-docs/gatsby-ssr.js
@@ -3,14 +3,24 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+import fs from 'fs';
+import { withPrefix } from 'gatsby';
+import { createContentDigest } from 'gatsby-core-utils';
 import React from 'react';
 
 const isProduction = process.env.GATSBY_NODE_ENV === 'production';
 const isNowBuild = Boolean(process.env.GATSBY_NOW_GITHUB_DEPLOYMENT);
 const isMasterBranch = process.env.GATSBY_NOW_GITHUB_COMMIT_REF === 'master';
 
+const iconDarkDigest = createContentDigest(
+  fs.readFileSync(require.resolve('./static/favicon-dark.png'))
+);
+const iconLightDigest = createContentDigest(
+  fs.readFileSync(require.resolve('./static/favicon-light.png'))
+);
+
 // eslint-disable-next-line import/prefer-default-export
-export const onRenderBody = ({ setPostBodyComponents }) => {
+export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
   // Activate the cookie consent banner only on the live website environment.
   // We can narrow it down to the build step of Zeit Now for the master branch.
   if (isProduction && isNowBuild && isMasterBranch)
@@ -22,4 +32,19 @@ export const onRenderBody = ({ setPostBodyComponents }) => {
         onLoad="function OptanonWrapper() {};"
       />,
     ]);
+
+  setHeadComponents([
+    <link
+      key="favicon-dark"
+      rel="icon"
+      href={withPrefix(`favicon-dark.png?v=${iconDarkDigest}`)}
+      media="(prefers-color-scheme:light)"
+    />,
+    <link
+      key="favicon-light"
+      rel="icon"
+      href={withPrefix(`favicon-light.png?v=${iconLightDigest}`)}
+      media="(prefers-color-scheme:dark)"
+    />,
+  ]);
 };
