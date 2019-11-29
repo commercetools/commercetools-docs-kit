6451dc36db44488256ca484019c7fc936d7d6656
Nicola Molinari <nicola.molinari@commercetools.de>
2019-11-29 21:42:12 +0100

    refactor(theme): allow to slightly configure header/footer (#111)

--- packages/gatsby-theme-docs/index.js
+++ packages/gatsby-theme-docs/index.js
@@ -11,7 +11,7 @@ export {
 } from './src/components';
 export { default as LayoutApplication } from './src/layouts/internals/layout-application';
 export { default as LayoutHeader } from './src/layouts/internals/layout-header';
-export { default as LayoutFooter } from './src/layouts/internals/layout-footer';
 export { default as LayoutMain } from './src/layouts/internals/layout-main';
+export { default as Footer } from './src/layouts/internals/footer';
 export { default as createStyledIcon } from './src/utils/create-styled-icon';
 export { useSiteData } from './src/hooks/use-site-data';
