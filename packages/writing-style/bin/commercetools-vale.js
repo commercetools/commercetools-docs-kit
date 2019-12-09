5840ac1d0076a30525e15736b57e7501b1e16881
Nicola Molinari <nicola.molinari@commercetools.de>
2019-12-09 22:42:23 +0100

    refactor(writing-styles): rewrite binary download to be cross platform compatible

--- packages/writing-style/bin/commercetools-vale.js
+++ packages/writing-style/bin/commercetools-vale.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
-const { spawnSync } = require('child_process');
-const path = require('path');
 
-const valeBinary = path.join(__dirname, `./vale`);
+const os = require('os');
+const path = require('path');
+const shelljs = require('shelljs');
+
+const binaryFileName = os.platform() === 'win32' ? 'vale.exe' : 'vale';
+
+const valeBinary = path.join(__dirname, `./${binaryFileName}`);
 const valeConfig = path.join(__dirname, `../.vale.ini`);
-const result = spawnSync(
-  valeBinary,
-  ['--config', valeConfig, ...process.argv.slice(2)],
-  {
-    windowsHide: true,
-    stdio: 'inherit',
-  }
+const result = shelljs.exec(
+  `${valeBinary} --config ${valeConfig} ${process.argv.slice(2).join(' ')}`
 );
-process.exit(result.status);
+process.exit(result.code);
