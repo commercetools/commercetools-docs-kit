cebf33bd1691f03ca5f37812a49ae00c075949e7
Nicola Molinari <nicola.molinari@commercetools.de>
2019-12-12 22:16:52 +0100

    fix(theme): overflow shorthand (#158)

--- packages/gatsby-theme-docs/src/layouts/internals/layout-main.js
+++ packages/gatsby-theme-docs/src/layouts/internals/layout-main.js
@@ -1,12 +1,14 @@
 import styled from '@emotion/styled';
 import { dimensions } from '../../design-system';
 
+/* NOTE: `overflow` shorthand is only supported is Chrome and FF */
 const LayoutMain = styled.div`
   display: grid;
   grid:
     [row1-start] 'page' 1fr [row1-end]
     / 1fr;
-  overflow: hidden auto;
+  overflow-x: hidden;
+  overflow-y: auto;
 
   @media screen and (${dimensions.viewports.laptop}) {
     grid:
