babfc9bd6c02f2a1a283f79fce8f77a7489e9869
Nicola Molinari <nicola.molinari@commercetools.de>
2019-12-13 20:32:43 +0100

    fix(ui-kit): bundle package using rollup (#162)
    * fix(ui-kit): bundle package using rollup
    
    * chore: use babel-jest
    
    * chore: build packages before websites
    
    * fix: use rollup svgr plugin
    
    * fix: ensure to set NODE_ENV when running cypress tests

--- jest.test.config.js
+++ jest.test.config.js
@@ -6,4 +6,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/transform-file.js',
   },
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest',
+  },
 };
---
+++ rollup.config.js
@@ -0,0 +1,68 @@
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BUILD_ROLLUP = true;
+
+const fs = require('fs');
+const babel = require('rollup-plugin-babel');
+const readPkgUp = require('read-pkg-up');
+const resolve = require('@rollup/plugin-node-resolve');
+// eslint-disable-next-line import/no-unresolved
+const json = require('@rollup/plugin-json');
+const commonjs = require('rollup-plugin-commonjs');
+const peerDeps = require('rollup-plugin-peer-deps-external');
+const builtins = require('rollup-plugin-node-builtins');
+const svgr = require('@svgr/rollup').default;
+
+const { packageJson: pkg } = readPkgUp.sync({
+  cwd: fs.realpathSync(process.cwd()),
+});
+const [, packageName] = pkg.name.split('@commercetools-docs/');
+const extensions = ['.js', '.ts', '.tsx'];
+
+const plugins = [
+  peerDeps({
+    includeDependencies: true,
+  }),
+  babel({
+    extensions,
+    runtimeHelpers: true,
+    rootMode: 'upward',
+  }),
+  // To convert CJS modules to ES6
+  commonjs({
+    include: 'node_modules/**',
+  }),
+  resolve({
+    extensions,
+    mainFields: ['module', 'main', 'jsnext'],
+    preferBuiltins: true,
+    modulesOnly: true,
+  }),
+  json({ namedExports: false }),
+  builtins(),
+  svgr(),
+];
+
+const createConfig = cliArgs => [
+  // Bundle for cjs format
+  {
+    input: cliArgs.input,
+    output: {
+      format: 'cjs',
+      file: `dist/${packageName}.cjs.js`,
+      sourcemap: true,
+    },
+    plugins,
+  },
+  // Bundle for es format
+  {
+    input: cliArgs.input,
+    output: {
+      format: 'es',
+      file: `dist/${packageName}.es.js`,
+      sourcemap: true,
+    },
+    plugins,
+  },
+];
+
+module.exports = createConfig;
