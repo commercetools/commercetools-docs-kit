2d652f396bb7abb708c704ce47c95815841b8edd
Adewale Oluyinka <davifantasia@hotmail.com>
2019-12-13 13:45:36 +0100

    feat: now parsing only the api

--- packages/gatsby-transformer-raml/gatsby-node.js
+++ packages/gatsby-transformer-raml/gatsby-node.js
@@ -1,12 +1,17 @@
+const firstline = require('firstline');
 const wap = require('webapi-parser').WebApiParser;
 
 async function onCreateNode({ node }) {
   if (!['File'].includes(node.internal.type)) return;
   if (node.internal.mediaType !== 'application/raml+yaml') return;
 
-  const model = await wap.raml10.parse(`file://${node.absolutePath}`);
-  const api = model.encodes;
+  const ramlIndicator = await firstline(node.absolutePath);
 
-  console.log('Title:', api.name.value());
+  if (ramlIndicator.trim() === '#%RAML 1.0') {
+    const model = await wap.raml10.parse(`file://${node.absolutePath}`);
+    const api = model.encodes;
+
+    console.log('Title:', api.name.value());
+  }
 }
 exports.onCreateNode = onCreateNode;
