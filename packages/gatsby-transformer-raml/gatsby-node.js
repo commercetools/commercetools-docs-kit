db5760b027c97fac4540753b26b28acc2cfc3210
Adewale Oluyinka <davifantasia@hotmail.com>
2019-12-13 13:42:02 +0100

    feat: can now parse all raml documents

--- packages/gatsby-transformer-raml/gatsby-node.js
+++ packages/gatsby-transformer-raml/gatsby-node.js
@@ -1,7 +1,12 @@
+const wap = require('webapi-parser').WebApiParser;
+
 async function onCreateNode({ node }) {
   if (!['File'].includes(node.internal.type)) return;
   if (node.internal.mediaType !== 'application/raml+yaml') return;
 
-  console.log('begin type/endpoint node creation');
+  const model = await wap.raml10.parse(`file://${node.absolutePath}`);
+  const api = model.encodes;
+
+  console.log('Title:', api.name.value());
 }
 exports.onCreateNode = onCreateNode;
