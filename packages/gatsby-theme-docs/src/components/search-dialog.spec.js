a5b8200f379d217647f1796ab2487ad6b89fb47c
Nicola Molinari <nicola.molinari@commercetools.de>
2019-12-27 12:22:06 +0100

    test(theme): adjust search dialog tests

--- packages/gatsby-theme-docs/src/components/search-dialog.spec.js
+++ packages/gatsby-theme-docs/src/components/search-dialog.spec.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render, wait, fireEvent } from '@testing-library/react';
-import SearchBar from './search-bar';
+import SearchDialog from './search-dialog';
+
+const createTestProps = custom => ({
+  onClose: jest.fn(),
+  ...custom,
+});
 
 describe('Rendering', () => {
   it('should initialize autocomplete search input and focus on it', async () => {
-    const rendered = render(<SearchBar />);
+    const props = createTestProps();
+    const rendered = render(<SearchDialog {...props} />);
 
     const input = rendered.queryByLabelText('Search');
     await wait(() => {
@@ -21,7 +27,8 @@ describe('Rendering', () => {
     });
   });
   it('should trigger focus using key binding "/"', async () => {
-    const rendered = render(<SearchBar />);
+    const props = createTestProps();
+    const rendered = render(<SearchDialog {...props} />);
 
     const input = rendered.queryByLabelText('Search');
     await wait(() => {
@@ -35,6 +42,24 @@ describe('Rendering', () => {
     fireEvent.keyUp(document.body, { key: '/' });
     await wait(() => {
       expect(document.activeElement).toEqual(input);
+    });
+  });
+  it('should dismiss dialog when clicking on "escape" key', async () => {
+    const props = createTestProps();
+    const rendered = render(<SearchDialog {...props} />);
+
+    const input = rendered.queryByLabelText('Search');
+    await wait(() => {
+      expect(input).toHaveAttribute(
+        'aria-owns',
+        expect.stringContaining('algolia-autocomplete-listbox')
+      );
+    });
+
+    // Dismiss
+    fireEvent.keyUp(document.body, { key: 'escape' });
+    await wait(() => {
+      expect(props.onClose).toHaveBeenCalled();
     });
   });
 });
