568f5350ac1f7d317297984087fd76f87cc51ddc
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2020-01-03 15:01:49 +0100

    docs: include code highlighting CSS

---
+++ docs/training-slides/gatsby-browser.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+import '@commercetools-docs/ui-kit/code-block-themes';
