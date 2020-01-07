7aba9730212dd6a307cc073a5aa473fe22346195
Adewale Oluyinka <davifantasia@hotmail.com>
2020-01-07 11:18:14 +0100

    feat: schema for resource.post

--- packages/gatsby-transformer-raml/src/schema/define-raml-resource.js
+++ packages/gatsby-transformer-raml/src/schema/define-raml-resource.js
@@ -6,12 +6,14 @@ const defineRamlResource = ({ schema, createTypes }) => {
         resourceName: 'String!',
         resourcePathUri: 'String!',
         uriParameters: '[RamlResourceUriParameter!]',
+        post: 'RamlResourcePost',
       },
       interfaces: ['Node'],
       extensions: {
         infer: false,
       },
     }),
+
     schema.buildObjectType({
       name: 'RamlResourceUriParameter',
       fields: {
@@ -19,6 +21,55 @@ const defineRamlResource = ({ schema, createTypes }) => {
         type: 'String!',
         builtinType: 'String!',
         description: 'String',
+      },
+    }),
+
+    schema.buildObjectType({
+      name: 'RamlResourcePost',
+      fields: {
+        securedBy: '[RamlResourceSecuredByOAuth]',
+        displayName: 'String',
+        description: 'String',
+        body: 'RamlResourcePostBody',
+        responses: '[RamlResourcePostResponses!]',
+      },
+    }),
+
+    schema.buildObjectType({
+      name: 'RamlResourceSecuredByOAuth',
+      fields: {
+        oauth_2_0: 'RamlResourceSecuredByOAuthScopes',
+      },
+    }),
+
+    schema.buildObjectType({
+      name: 'RamlResourceSecuredByOAuthScopes',
+      fields: {
+        scopes: '[String!]',
+      },
+    }),
+
+    schema.buildObjectType({
+      name: 'RamlResourcePostResponses',
+      fields: {
+        code: 'Int',
+        description: 'String',
+        body: 'RamlResourcePostBody!',
+      },
+    }),
+
+    schema.buildObjectType({
+      name: 'RamlResourcePostBody',
+      fields: {
+        applicationjson: 'RamlResourcePostBodyApplicationJson',
+      },
+    }),
+
+    schema.buildObjectType({
+      name: 'RamlResourcePostBodyApplicationJson',
+      fields: {
+        type: 'String!',
+        builtinType: 'String!',
       },
     }),
   ];
