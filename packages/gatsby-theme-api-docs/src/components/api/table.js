c55eb5b671e92bf503a9b82adcaac9950a8a5692
Adewale Oluyinka <davifantasia@hotmail.com>
2020-01-13 12:21:03 +0100

    feat:  table header and row styling

--- packages/gatsby-theme-api-docs/src/components/api/table.js
+++ packages/gatsby-theme-api-docs/src/components/api/table.js
@@ -4,41 +4,49 @@ const Table = styled.table`
   width: 100%;
   max-width: 100%;
   background-color: transparent;
-  border: 0.0625rem solid #e6e6e6;
-  border-radius: 0.375rem;
+  border: 1px solid #e6e6e6;
+  border-radius: 6px;
   background-color: #fff;
-  box-shadow: 0 0.0625rem 0.0625rem 0 rgba(0, 0, 0, 0.25);
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.25);
   border-collapse: separate;
   border-spacing: 0;
+
+  thead {
+    display: block;
+    padding: 8px;
+    background-color: #f2f2f2;
+    border-radius: 6px 6px 0 0;
+  }
 
   th {
     vertical-align: bottom;
     border-bottom: 2px solid #dee2e6;
     text-align: left;
     border: none;
-    border-radius: 0.375rem 0.375rem 0 0;
-    background-color: #f2f2f2;
-    padding: 0.375rem 0.5rem;
     font-weight: 700;
+    line-height: 21px;
   }
 
   tbody {
-    tr:not(:first-of-type) {
-      border-top: 1px solid #e6e6e6;
+    tr {
+      display: block;
+      padding: 8px 16px;
+
+      :not(:first-of-type) {
+        border-top: 1px solid #e6e6e6;
+      }
     }
 
     td {
       vertical-align: top;
       border-top: none;
-      padding: 0.5rem;
 
       :first-of-type {
-        width: 15rem;
-        padding-left: 1rem;
+        width: 200px;
       }
 
       :last-of-type {
-        padding-right: 1rem;
+        padding-left: 16px;
       }
     }
   }
