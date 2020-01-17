0364745b49f516d5c6dec0796587c46e6cbf8125
Adewale Oluyinka <davifantasia@hotmail.com>
2020-01-17 11:21:07 +0100

    refactor: removed unnecessary styling

--- packages/gatsby-theme-api-docs/src/components/api/resource/method/url-scopes-responses/scopes.js
+++ packages/gatsby-theme-api-docs/src/components/api/resource/method/url-scopes-responses/scopes.js
@@ -1,32 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
-import { customProperties } from '@commercetools-uikit/design-system';
 import { Markdown } from '@commercetools-docs/ui-kit';
 
 import Title from './title';
-
-const ListContainer = styled.p`
-  margin-top: ${customProperties.spacingXs};
-`;
 
 const Scopes = ({ scopes, title }) => {
   return (
     <div>
       {title ? <Title>{title}</Title> : null}
 
-      <ListContainer>
-        {scopes.map((scope, index) =>
-          index === scopes.length - 1 ? (
-            <Markdown.InlineCode key={scope}>{scope}</Markdown.InlineCode>
-          ) : (
-            <span key={scope}>
-              <Markdown.InlineCode>{scope}</Markdown.InlineCode>
-              {', '}
-            </span>
-          )
-        )}
-      </ListContainer>
+      {scopes.map((scope, index) =>
+        index === scopes.length - 1 ? (
+          <Markdown.InlineCode key={scope}>{scope}</Markdown.InlineCode>
+        ) : (
+          <span key={scope}>
+            <Markdown.InlineCode>{scope}</Markdown.InlineCode>
+            {', '}
+          </span>
+        )
+      )}
     </div>
   );
 };
