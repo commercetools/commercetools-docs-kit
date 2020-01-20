8dad83a913189cb115e4dc414e18c5275f97eabb
Adewale Oluyinka <davifantasia@users.noreply.github.com>
2020-01-20 20:02:55 +0100

    Missing RAML fix (#219)
    * chore: autogenerated raml
    
    * refactor: generating keys this way to accommodate different folder structures for types and resources
    
    * refactor: sourcing from appropriate folders
    
    * fix: unique element key

--- packages/gatsby-theme-api-docs/gatsby-node.js
+++ packages/gatsby-theme-api-docs/gatsby-node.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 // Ensure that certain directories exist.
 // https://www.gatsbyjs.org/tutorial/building-a-theme/#create-a-data-directory-using-the-onprebootstrap-lifecycle
 exports.onPreBootstrap = ({ reporter }) => {
-  const requiredDirectories = ['src/generated-api-specs'];
+  const requiredDirectories = ['src/api-specs'];
   requiredDirectories.forEach(dir => {
     if (!fs.existsSync(dir)) {
       reporter.info(`creating the ${dir} directory`);
