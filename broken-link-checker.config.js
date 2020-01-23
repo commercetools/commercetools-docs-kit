a6090a0e63a8ba4434e934ce513d8d4e789875ed
Nicola Molinari <nicola.molinari@commercetools.de>
2020-01-23 14:13:56 +0100

    chore: exclude global links from link checker

--- broken-link-checker.config.js
+++ broken-link-checker.config.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+  excludedKeywords: [
+    '/getting-started#sign-up',
+    '/login',
+    '/http-api',
+    '/graphql-api',
+    '/release-notes',
+    '/software-development-kits',
+    '/import-api',
+    '/sunrise',
+    '/tutorials-overview',
+    '/custom-applications',
+    '/faqs',
+    '/merchant-center',
+    '/merchant-center-releases',
+  ],
+};
