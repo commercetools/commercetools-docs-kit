6925c6d218ff81969a71a1bb6ed1274fca106861
jazevedo620 <joseph.az@gatech.edu>
2020-01-24 21:35:19 -0500

    fix(introspection): fix missing GQL alias in query

--- packages/gatsby-theme-api-docs/src/hooks/use-type-locations.js
+++ packages/gatsby-theme-api-docs/src/hooks/use-type-locations.js
@@ -37,7 +37,7 @@ export const useTypeLocations = () => {
               name
               value
             }
-            mdx {
+            mdx: page {
               fields {
                 slug
               }
