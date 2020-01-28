640ba709bd5a51f04af5fff48eb339b3b86b9d5e
Adewale Oluyinka <davifantasia@hotmail.com>
2020-01-28 10:16:42 +0100

    style: spacing between parameter name and type

--- packages/gatsby-theme-api-docs/src/components/resource/method/parameters.js
+++ packages/gatsby-theme-api-docs/src/components/resource/method/parameters.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SpacingsStack from '@commercetools-uikit/spacings-stack';
 import { Markdown } from '@commercetools-docs/ui-kit';
 import capitalizeFirst from '../../../utils/capitalize-first';
 import Required from '../../required';
@@ -21,13 +22,15 @@ const Parameters = ({ title, parameters }) => {
           return (
             <tr key={parameter.name}>
               <td>
-                <p className="name-type">
-                  <Markdown.InlineCode>{parameter.name}</Markdown.InlineCode>
-                  {parameter.required ? <Required>*</Required> : null}
-                </p>
-                <p className="name name-type">
-                  {capitalizeFirst(parameter.type)}
-                </p>
+                <SpacingsStack scale="xs">
+                  <p className="name-type">
+                    <Markdown.InlineCode>{parameter.name}</Markdown.InlineCode>
+                    {parameter.required ? <Required>*</Required> : null}
+                  </p>
+                  <p className="name name-type">
+                    {capitalizeFirst(parameter.type)}
+                  </p>
+                </SpacingsStack>
               </td>
               <td>{parameter.description ? parameter.description : '-'}</td>
             </tr>
