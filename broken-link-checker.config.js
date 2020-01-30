945fabbe39f386c796cfe0c633bfa604a4940a86
Nicola Molinari <nicola.molinari@commercetools.de>
2020-01-30 14:28:49 +0100

    chore: exclude links to smoke tests websites
    Co-Authored-By: Nikolaus Kühn <nikolaus.kuehn@commercetools.de>

--- broken-link-checker.config.js
+++ broken-link-checker.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  excludedKeywords: [],
+  excludedKeywords: [
+    'https://docs.commercetools.com/docs-smoke-test',
+    'https://docs.commercetools.com/api-docs-smoke-test',
+    'https://docs.commercetools.com/site-template',
+  ],
 };
