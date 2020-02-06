211c79f8035f10f22d08c5dcc4d1ffcb296fa64d
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2020-02-06 18:16:13 +0100

    fix: adjust test assertion to new labels part 2

--- cypress/integration/docs-smoke-test/top-menu.js
+++ cypress/integration/docs-smoke-test/top-menu.js
@@ -3,14 +3,14 @@ import { URL_DOCS_SMOKE_TEST } from '../../support/urls';
 describe('Top menu', () => {
   it('should toggle top menu and take a snapshot', () => {
     cy.visit(URL_DOCS_SMOKE_TEST);
-    cy.findByLabelText('Open top menu').click();
+    cy.findByLabelText('Open Top Menu').click();
     cy.findByRole('top-menu').should('be.visible');
     cy.findByLabelText('Close top menu').should('exist');
     cy.percySnapshot();
   });
   it('should close top menu when clicking on the search input', () => {
     cy.visit(URL_DOCS_SMOKE_TEST);
-    cy.findByLabelText('Open top menu').click();
+    cy.findByLabelText('Open Top Menu').click();
     cy.findByRole('top-menu').should('be.visible');
     cy.findByLabelText('Search').click();
     cy.findByText('Developer Center').should('not.be.visible');
