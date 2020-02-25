f6f1feb54031a420a637dfc99f49b3ca6a53c9e1
Adewale Oluyinka <davifantasia@hotmail.com>
2020-02-25 15:28:14 +0100

    feat: now using default arguments from cli

--- packages/raml-generator/bin/raml-generator.js
+++ packages/raml-generator/bin/raml-generator.js
@@ -1,2 +1,3 @@
 #! /usr/bin/env node
-console.log('console log more');
+
+require('../src/cli').cli(process.argv);
---
+++ packages/raml-generator/src/cli.js
@@ -0,0 +1,5 @@
+function cli(args) {
+  console.log(args);
+}
+
+module.exports = { cli };
