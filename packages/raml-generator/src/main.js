dd0147cd9443f9cac652628e5844580b403054bc
Adewale Oluyinka <davifantasia@hotmail.com>
2020-02-25 17:23:38 +0100

    feat: now using arguments from cli to generate raml

--- packages/raml-generator/src/main.js
+++ packages/raml-generator/src/main.js
@@ -1,7 +1,41 @@
+const fs = require('fs');
+const path = require('path');
 const shell = require('shelljs');
+const chalk = require('chalk');
 
 function generateRaml(options) {
-  shell.exec(`echo options ${JSON.stringify(options)}`);
+  // Check if the spec file exists.
+  fs.access(options.apiSpecPath, fs.constants.F_OK, err => {
+    if (err) {
+      console.error(
+        `${options.apiSpecPath} does not exist`,
+        chalk.red.bold('ERROR')
+      );
+      process.exit(1);
+    }
+
+    generateRamlIfSpecPath(options);
+  });
+}
+
+function generateRamlIfSpecPath(options) {
+  const jarFile = path.resolve(
+    __dirname,
+    '../download/cli-application-1.0.0-20200221085820-all.jar'
+  );
+
+  // check if jar file was successfully downloaded.
+  fs.access(jarFile, fs.constants.F_OK, err => {
+    if (err) {
+      console.error(`${jarFile} does not exist`, chalk.red.bold('ERROR'));
+
+      // TODO: try download again, if failure, log error.
+    }
+
+    shell.exec(
+      `java -jar ${jarFile} generate ${options.apiSpecPath} -o ./src/api-specs/${options.apiSpecName} -t ramldoc`
+    );
+  });
 }
 
 module.exports = { generateRaml };
