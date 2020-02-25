f2aa809d82aee192b6750a085336f3aab9a446f9
Adewale Oluyinka <davifantasia@hotmail.com>
2020-02-25 17:34:36 +0100

    refactor: no need to download jar

--- packages/raml-generator/src/main.js
+++ packages/raml-generator/src/main.js
@@ -23,18 +23,9 @@ function generateRamlIfSpecPath(options) {
     '../jar/cli-application-1.0.0-20200221085820-all.jar'
   );
 
-  // check if jar file was successfully downloaded.
-  fs.access(jarFile, fs.constants.F_OK, err => {
-    if (err) {
-      console.error(`${jarFile} does not exist`, chalk.red.bold('ERROR'));
-
-      // TODO: try download again, if failure, log error.
-    }
-
-    shell.exec(
-      `java -jar ${jarFile} generate ${options.apiSpecPath} -o ./src/api-specs/${options.apiSpecName} -t ramldoc`
-    );
-  });
+  shell.exec(
+    `java -jar ${jarFile} generate ${options.apiSpecPath} -o ./src/api-specs/${options.apiSpecName} -t ramldoc`
+  );
 }
 
 module.exports = { generateRaml };
