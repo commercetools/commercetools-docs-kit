c40fe1f23d1f62f1338daff98df14883284cfabf
Adewale Oluyinka <davifantasia@hotmail.com>
2020-03-03 11:19:01 +0100

    feat: code sample rendered with code block

--- packages/gatsby-theme-docs/src/components/code-sample-from-file.js
+++ packages/gatsby-theme-docs/src/components/code-sample-from-file.js
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { CodeBlock } from '@commercetools-docs/ui-kit';
 
 // import Java from '../code-samples/test.java';
 
+// CodeBlockFromImport
 function CodeSampleFromFile(props) {
   const [sample, setSample] = useState('');
 
+  // props.import - path to code sample
   import(`../code-samples/${props.name}`).then(result => {
     setSample(result.default);
   });
 
-  return (
-    <div>
-      <code>{sample}</code>;
-    </div>
-  );
+  return <CodeBlock content={sample} language="js" />;
 }
 
 CodeSampleFromFile.propTypes = {
