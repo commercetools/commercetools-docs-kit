41a1595ec925f816b1212bc662949824dc4dd1ed
Adewale Oluyinka <davifantasia@hotmail.com>
2020-03-13 10:55:47 +0100

    refactor: select styling and refactoring how header is rendered

--- packages/ui-kit/src/components/code-block.js
+++ packages/ui-kit/src/components/code-block.js
@@ -70,6 +70,44 @@ const TooltipBodyComponent = styled.div`
   font-size: ${typography.fontSizes.extraSmall};
   padding: ${dimensions.spacings.xs} ${dimensions.spacings.s};
 `;
+const languagesBackgroundImageUrl = color => {
+  return `url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg width='24px' height='24px' viewBox='0 0 24 24' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3C!-- Generator: sketchtool 45.2 (43514) - http://www.bohemiancoding.com/sketch --%3E%3Ctitle%3Ecaret-down%3C/title%3E%3Cdesc%3ECreated with sketchtool.%3C/desc%3E%3Cdefs%3E%3C/defs%3E%3Cg id='Icons' stroke='none' stroke-width='1' fill-rule='evenodd'%3E%3Cg id='MC-icon-set' transform='translate(-168.000000, -936.000000)' fill='%23${color}'%3E%3Cg id='Directions' transform='translate(24.000000, 888.000000)'%3E%3Cg id='Caret-Down' transform='translate(144.000000, 48.000000)'%3E%3Cpath d='M20.6658731,7.4053255 C20.4433682,7.16948908 20.1796129,7.05166867 19.8748538,7.05166867 L4.12508466,7.05166867 C3.82020235,7.05166867 3.55663185,7.16948908 3.33394217,7.4053255 C3.11125249,7.64142273 3,7.92055342 3,8.24323919 C3,8.56585976 3.11125249,8.84499045 3.33394217,9.08089208 L11.2088575,17.4207121 C11.4317935,17.6565485 11.695364,17.7746297 12,17.7746297 C12.304636,17.7746297 12.5684528,17.6565485 12.7909578,17.4207121 L20.6658731,9.08082687 C20.8883165,8.84499045 21,8.56585976 21,8.24317399 C21,7.92055342 20.8883165,7.64142273 20.6658731,7.4053255 L20.6658731,7.4053255 Z' id='shape'%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`;
+};
+const LanguagesDropDown = styled.select`
+  display: block;
+  font-size: 16px;
+  line-height: 23px;
+  color: #fff;
+  background: none;
+  border: none;
+  cursor: pointer;
+  text-transform: uppercase;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  padding-right: 16px;
+  background-image: ${languagesBackgroundImageUrl('ffffff')};
+  background-repeat: no-repeat, repeat;
+  background-position: right 0 top 50%, 0 0;
+  background-size: 12px auto, 100%;
+
+  ::-ms-expand {
+    display: none;
+  }
+
+  :hover {
+    color: #e6e6e6;
+    background-image: ${languagesBackgroundImageUrl('e6e6e6')};
+  }
+
+  :focus {
+    outline: none;
+  }
+
+  option {
+    margin-right: 1em;
+  }
+`;
 
 const getLineStyles = options => {
   let promptLineStyles;
@@ -221,8 +259,6 @@ const CodeBlock = props => {
       : props.noPromptLines
   );
 
-  // TODO: use parsing functionality of building array of highlightLines and noPromptLines
-
   const language = languageAliases[languageCode] || languageCode;
   const isCommandLine = ['terminal', 'console'].includes(languageCode);
 
@@ -240,11 +276,11 @@ const CodeBlock = props => {
   return (
     <Container>
       {props.multiLanguage
-        ? renderMultiLanguageHeader(
+        ? renderHeaderForMultiLanguage(
             props.multiLanguage.title,
             props.multiLanguage.props.map(prop => prop.language)
           )
-        : renderTitle(props.title, languageCode)}
+        : renderHeaderForSingleLanguage(props.title, languageCode)}
       <Highlight
         {...defaultProps}
         code={content}
@@ -319,61 +355,56 @@ const CodeBlock = props => {
     </Container>
   );
 
-  function renderTitle(title, langCode) {
+  function renderHeaderForSingleLanguage(title, langCode) {
     if (title) {
-      return (
-        <Header>
-          <HeaderInner>
-            <HeaderText>{title}</HeaderText>
-            <SpacingsInline
-              scale="m"
-              alignItems="center"
-              justifyContent="flex-end"
-            >
-              {langCode === 'text' ? null : <HeaderText>{langCode}</HeaderText>}
-            </SpacingsInline>
-          </HeaderInner>
-        </Header>
+      return renderHeader(
+        title,
+        langCode === 'text' ? null : <HeaderText>{langCode}</HeaderText>
       );
     }
 
     return null;
   }
-  function renderMultiLanguageHeader(title, languages = []) {
+  function renderHeaderForMultiLanguage(title, languages = []) {
     if (title || languages.length) {
-      return (
-        <Header>
-          <HeaderInner>
-            <HeaderText>{title}</HeaderText>
-            <SpacingsInline
-              scale="m"
-              alignItems="center"
-              justifyContent="flex-end"
-            >
-              <select onChange={handleOnLanguageChange}>
-                {languages.map(lang => (
-                  <option key={lang} value={lang}>
-                    {lang}
-                  </option>
-                ))}
-              </select>
-            </SpacingsInline>
-          </HeaderInner>
-        </Header>
+      return renderHeader(
+        title,
+        <LanguagesDropDown onChange={handleOnLanguageChange}>
+          {languages.map(lang => (
+            <option key={lang} value={lang}>
+              {lang}
+            </option>
+          ))}
+        </LanguagesDropDown>
       );
     }
 
     return null;
   }
-
+  function renderHeader(title, languages) {
+    return (
+      <Header>
+        <HeaderInner>
+          <HeaderText>{title}</HeaderText>
+          <SpacingsInline
+            scale="m"
+            alignItems="center"
+            justifyContent="flex-end"
+          >
+            {languages}
+          </SpacingsInline>
+        </HeaderInner>
+      </Header>
+    );
+  }
   function handleOnLanguageChange(e) {
     const code = props.multiLanguage.props.find(
       prop => prop.language === e.target.value
     );
     setLanguageCode(code.language);
     setContent(code.content);
-    setHighlightLines(code.highlightLines || []);
-    setNoPromptLines(code.noPromptLines || []);
+    setHighlightLines(code.highlightLines);
+    setNoPromptLines(code.noPromptLines);
   }
 };
 CodeBlock.propTypes = {
@@ -392,7 +423,7 @@ CodeBlock.propTypes = {
         noPromptLines: PropTypes.arrayOf(PropTypes.number),
         content: PropTypes.string.isRequired,
       })
-    ),
+    ).isRequired,
   }),
   oneOfContentOrMultiLanguage: (props, propName, componentName) => {
     if (!props.content && !props.multiLanguage) {
