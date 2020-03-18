aa62aaafb46856fa198e4c65e69b64849fb6d950
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2020-03-18 22:43:15 +0100

    fix: proper native style of select across browsers

--- packages/ui-kit/src/components/code-block.js
+++ packages/ui-kit/src/components/code-block.js
@@ -73,6 +73,14 @@ const TooltipBodyComponent = styled.div`
 const languagesBackgroundImageUrl = color => {
   return `url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg width='24px' height='24px' viewBox='0 0 24 24' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3C!-- Generator: sketchtool 45.2 (43514) - http://www.bohemiancoding.com/sketch --%3E%3Ctitle%3Ecaret-down%3C/title%3E%3Cdesc%3ECreated with sketchtool.%3C/desc%3E%3Cdefs%3E%3C/defs%3E%3Cg id='Icons' stroke='none' stroke-width='1' fill-rule='evenodd'%3E%3Cg id='MC-icon-set' transform='translate(-168.000000, -936.000000)' fill='%23${color}'%3E%3Cg id='Directions' transform='translate(24.000000, 888.000000)'%3E%3Cg id='Caret-Down' transform='translate(144.000000, 48.000000)'%3E%3Cpath d='M20.6658731,7.4053255 C20.4433682,7.16948908 20.1796129,7.05166867 19.8748538,7.05166867 L4.12508466,7.05166867 C3.82020235,7.05166867 3.55663185,7.16948908 3.33394217,7.4053255 C3.11125249,7.64142273 3,7.92055342 3,8.24323919 C3,8.56585976 3.11125249,8.84499045 3.33394217,9.08089208 L11.2088575,17.4207121 C11.4317935,17.6565485 11.695364,17.7746297 12,17.7746297 C12.304636,17.7746297 12.5684528,17.6565485 12.7909578,17.4207121 L20.6658731,9.08082687 C20.8883165,8.84499045 21,8.56585976 21,8.24317399 C21,7.92055342 20.8883165,7.64142273 20.6658731,7.4053255 L20.6658731,7.4053255 Z' id='shape'%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`;
 };
+
+const LanguagesDropDownWrapper = styled.div`
+  background-color: ${colors.light.textPrimary};
+  background-image: ${languagesBackgroundImageUrl('ffffff')};
+  background-repeat: no-repeat, repeat;
+  background-position: right 0 top 50%, 0 0;
+  background-size: ${dimensions.widths.selectDropDownArrowWith} auto, 100%;
+`;
 const LanguagesDropDown = styled.select`
   display: block;
   font-size: ${typography.fontSizes.body};
@@ -83,15 +91,12 @@ const LanguagesDropDown = styled.select`
   box-sizing: border-box;
   cursor: pointer;
   border: none;
-  -moz-appearance: none;
-  -webkit-appearance: none;
   appearance: none;
+  background: transparent;
 
-  background-color: ${colors.light.textPrimary};
-  background-image: ${languagesBackgroundImageUrl('ffffff')};
-  background-repeat: no-repeat, repeat;
-  background-position: right 0 top 50%, 0 0;
-  background-size: ${dimensions.widths.selectDropDownArrowWith} auto, 100%;
+  option {
+    color: black;
+  }
 
   ::-ms-expand {
     display: none;
@@ -99,7 +104,6 @@ const LanguagesDropDown = styled.select`
 
   :hover {
     color: ${colors.light.surfaceInlineCode};
-    background-image: ${languagesBackgroundImageUrl('e6e6e6')};
   }
 
   :focus {
@@ -367,13 +371,15 @@ const CodeBlock = props => {
     if (title || languages.length) {
       return renderHeader(
         title,
-        <LanguagesDropDown onChange={handleOnLanguageChange}>
-          {languages.map(lang => (
-            <option key={lang} value={lang}>
-              {lang}
-            </option>
-          ))}
-        </LanguagesDropDown>
+        <LanguagesDropDownWrapper>
+          <LanguagesDropDown onChange={handleOnLanguageChange}>
+            {languages.map(lang => (
+              <option key={lang} value={lang}>
+                {lang}
+              </option>
+            ))}
+          </LanguagesDropDown>
+        </LanguagesDropDownWrapper>
       );
     }
 
