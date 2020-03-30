ffa5fda0fa5d74a59cb0ecca3bca26e98cdd3cfc
Nicola Molinari <nicola.molinari@commercetools.de>
2020-03-30 10:07:18 +0200

    feat(theme): add support for specifying new languages for Prism (#325)
    * fix(docs-smoke-test): missing scala example
    
    * feat(theme): add support for specifying new languages for Prism

--- websites/docs-smoke-test/gatsby-config.js
+++ websites/docs-smoke-test/gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
       options: {
         websiteKey: 'docs-smoke-test',
         excludeFromSearchIndex: isProd && !shouldEnableSearch,
+        additionalPrismLanguages: ['scala', 'csharp'],
       },
     },
   ],
