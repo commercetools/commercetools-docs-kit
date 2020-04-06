dd4578ad05351e1f4afc2730ef6afdc45c7332e6
Nicola Molinari <nicola.molinari@commercetools.de>
2020-04-06 12:55:39 +0200

    Revert "chore: add debug logs"
    This reverts commit b9f55041a966010529177b2aaba286f10e1655cb.

--- packages/gatsby-theme-docs/gatsby-config.js
+++ packages/gatsby-theme-docs/gatsby-config.js
@@ -12,11 +12,6 @@ const proxyEnvironmentVariables = [
 ];
 proxyEnvironmentVariables.forEach((envName) => {
   process.env[`GATSBY_${envName}`] = process.env[envName];
-  console.log(
-    `[DEBUG GATSBY-CONFIG] Setting environment variable ${envName}: ${
-      process.env[`GATSBY_${envName}`]
-    }`
-  );
 });
 
 const defaultOptions = {
