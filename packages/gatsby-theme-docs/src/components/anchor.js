de714f432b14c308df31e5d57a360dabb8c7b5f5
Adewale Oluyinka <davifantasia@hotmail.com>
2020-04-22 16:40:17 +0200

    refactor: moved custom anchor doc to view directory, added comments about anchor component

--- packages/gatsby-theme-docs/src/components/anchor.js
+++ packages/gatsby-theme-docs/src/components/anchor.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * This component is exposed to be used by mdx authors. It returns an anchor with name, and only name attribute.
+ *
+ * See more usability info here - https://github.com/commercetools/commercetools-docs-kit/issues/337
+ *
+ * This component also exists since we don't allow html in authoring mdx documents.
+ */
 const Anchor = (props) => <a name={props.name} />;
 
 Anchor.propTypes = {
