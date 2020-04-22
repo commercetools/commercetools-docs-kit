d7784ad4122f830ab28e7c8a65b896c74f54f6a4
Adewale Oluyinka <davifantasia@hotmail.com>
2020-04-22 15:59:29 +0200

    refactor: component should not be styled

--- packages/gatsby-theme-docs/src/components/anchor.js
+++ packages/gatsby-theme-docs/src/components/anchor.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
 
-const A = styled.a``;
-
-const Anchor = (props) => <A name={props.name} />;
+const Anchor = (props) => <a name={props.name} />;
 
 Anchor.propTypes = {
   name: PropTypes.string.isRequired,
