da19bd4f808d094f686eff70c7caf6024b84103c
Nicola Molinari <nicola.molinari@commercetools.de>
2020-04-27 21:25:40 +0200

    fix(theme): render proper 404 page as a content page

--- packages/gatsby-theme-docs/src/pages/404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PageNotFound from '../templates/page-not-found';
-
-// eslint-disable-next-line react/display-name
-export default () => (
-  <PageNotFound>
-    <div>{'Page not found'}</div>
-  </PageNotFound>
-);
