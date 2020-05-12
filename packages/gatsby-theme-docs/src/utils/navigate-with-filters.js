da16e38a8986f70e8e6faab7ab6c797dd760380d
Adewale Oluyinka <davifantasia@hotmail.com>
2020-05-12 17:18:40 +0200

    refactor: empty parameters are now removed from url

--- packages/gatsby-theme-docs/src/utils/navigate-with-filters.js
+++ packages/gatsby-theme-docs/src/utils/navigate-with-filters.js
@@ -4,14 +4,25 @@ import extractQueryParameters from './extract-query-parameters';
 
 function navigateWithFilters(filters, location) {
   const currentQueryParameters = extractQueryParameters(location);
-  const newQueryParameters = { ...currentQueryParameters, ...filters };
+  const newQueryParameters = removeUnnecessaryParameters({
+    ...currentQueryParameters,
+    ...filters,
+  });
 
-  navigate(
-    encode(
-      newQueryParameters,
-      Object.keys(newQueryParameters).length > 0 ? '?' : ''
-    )
-  );
+  navigate(encode(newQueryParameters, '?'));
+}
+
+function removeUnnecessaryParameters(parameters) {
+  const returnedParameters = {};
+  Object.keys(parameters).forEach((key) => {
+    if (Array.isArray(parameters[key]) && parameters.length > 0) {
+      returnedParameters[key] = parameters[key];
+    } else if (parameters[key]) {
+      returnedParameters[key] = parameters[key];
+    }
+  });
+
+  return returnedParameters;
 }
 
 export default navigateWithFilters;
