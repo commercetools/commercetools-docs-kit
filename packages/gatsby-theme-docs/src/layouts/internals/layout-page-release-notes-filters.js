9859749774f66c24ae45785e8e791d4de7c6b0bc
Nicola Molinari <nicola.molinari@commercetools.de>
2020-05-17 17:00:38 +0200

    fix(theme): render release notes filters in overlay for small screens

--- packages/gatsby-theme-docs/src/layouts/internals/layout-page-release-notes-filters.js
+++ packages/gatsby-theme-docs/src/layouts/internals/layout-page-release-notes-filters.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { IntlProvider } from 'react-intl';
+import { keyframes } from '@emotion/core';
 import styled from '@emotion/styled';
 import { createStyledIcon, designSystem } from '@commercetools-docs/ui-kit';
 import SpacingsStack from '@commercetools-uikit/spacings-stack';
 import SpacingsInline from '@commercetools-uikit/spacings-inline';
+import IconButton from '@commercetools-uikit/icon-button';
+import SecondaryIconButton from '@commercetools-uikit/secondary-icon-button';
+import { CloseIcon } from '@commercetools-uikit/icons';
 import UnstyledStackedLinesIndentedIcon from '../../icons/stacked-lines-indented-icon.svg';
+import { Overlay } from '../../components';
 import ReleaseNotesFilterDates from '../../components/release-notes-filter-dates';
 import ReleaseNotesFilterTopics from '../../components/release-notes-filter-topics';
 
@@ -12,6 +18,18 @@ const StackedLinesIndentedIcon = createStyledIcon(
   UnstyledStackedLinesIndentedIcon
 );
 
+const slideInAnimation = keyframes`
+  from { margin-right: -100%; }
+  to { margin-right: 0; }
+`;
+const SlidingContainer = styled.div`
+  background-color: ${designSystem.colors.light.surfacePrimary};
+  animation: ${slideInAnimation} 0.5s ease-out alternate;
+  width: calc(${designSystem.dimensions.widths.pageNavigation});
+  padding: ${designSystem.dimensions.spacings.m};
+  height: 100%;
+  overflow: auto;
+`;
 const GridContainer = styled.div`
   display: none;
   border-left: 1px solid ${designSystem.colors.light.borderPrimary};
@@ -52,29 +70,88 @@ const ReleasesTitleLink = styled.a`
     outline-width: 0;
   }
 `;
+const ToggleMenuButton = styled.div`
+  position: fixed;
+  top: calc(
+    ${designSystem.dimensions.heights.header} +
+      ${designSystem.dimensions.spacings.m}
+  );
+  right: ${designSystem.dimensions.spacings.m};
+  cursor: pointer;
+
+  @media screen and (${designSystem.dimensions.viewports.largeTablet}) {
+    display: none;
+  }
+`;
 
 const LayoutPageReleaseNotesFilters = () => {
-  return (
+  const [isMenuOpen, setMenuOpen] = React.useState(false);
+  const [modalPortalNode, setModalPortalNode] = React.useState();
+  React.useEffect(() => {
+    setModalPortalNode(document.getElementById('modal-portal'));
+  }, []);
+
+  const filtersContainer = (
     <IntlProvider locale="en">
-      <GridContainer>
-        <StickyContainer>
-          <SpacingsStack scale="m">
-            <ReleasesTitleLink href="#top">
-              <SpacingsInline scale="s" alignItems="center">
-                <div>Releases</div>
-                <div>
-                  <StackedLinesIndentedIcon color="textSecondary" />
-                </div>
-              </SpacingsInline>
-            </ReleasesTitleLink>
-
-            <ReleaseNotesFilterDates />
-
-            <ReleaseNotesFilterTopics />
-          </SpacingsStack>
-        </StickyContainer>
-      </GridContainer>
+      <SpacingsStack scale="m">
+        <SpacingsInline
+          scale="s"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <ReleasesTitleLink href="#top">
+            <SpacingsInline scale="s" alignItems="center">
+              <div>Releases</div>
+              <div>
+                <StackedLinesIndentedIcon color="textSecondary" />
+              </div>
+            </SpacingsInline>
+          </ReleasesTitleLink>
+          <SecondaryIconButton
+            icon={<CloseIcon size="medium" />}
+            label="Close release notes filters"
+            onClick={() => {
+              setMenuOpen(false);
+            }}
+          />
+        </SpacingsInline>
+        <ReleaseNotesFilterDates />
+        <ReleaseNotesFilterTopics />
+      </SpacingsStack>
     </IntlProvider>
+  );
+
+  if (isMenuOpen) {
+    return modalPortalNode
+      ? ReactDOM.createPortal(
+          <Overlay
+            justifyContent="flex-end"
+            // onClick={() => {
+            //   // setMenuOpen(false);
+            // }}
+          >
+            <SlidingContainer>{filtersContainer}</SlidingContainer>
+          </Overlay>,
+          modalPortalNode
+        )
+      : null;
+  }
+
+  return (
+    <>
+      <ToggleMenuButton>
+        <IconButton
+          icon={<StackedLinesIndentedIcon theme="textSecondary" />}
+          label="Open release notes filters"
+          onClick={() => {
+            setMenuOpen(true);
+          }}
+        />
+      </ToggleMenuButton>
+      <GridContainer>
+        <StickyContainer>{filtersContainer}</StickyContainer>
+      </GridContainer>
+    </>
   );
 };
 LayoutPageReleaseNotesFilters.displayName = 'LayoutPageReleaseNotesFilters';
