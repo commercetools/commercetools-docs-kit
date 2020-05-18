fe444b46471c2e2d29de5dba0c4b07102a085af6
Adewale Oluyinka <davifantasia@hotmail.com>
2020-05-18 12:46:49 +0200

    refactor: separate card and cards

--- packages/ui-kit/src/components/cards.js
+++ packages/ui-kit/src/components/cards.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import css from '@emotion/css';
 import ContentNotifications from './content-notifications';
 import { dimensions } from '../design-system';
 
@@ -19,24 +18,7 @@ const CardsContainer = styled.div`
     }, 1fr))`};
 `;
 
-const flatStyle = css`
-  border: 1px solid #cccccc;
-`;
-
-const clickableStyle = css`
-  cursor: pointer;
-  box-shadow: 1px 1px 4px 0 rgba(0, 0, 0, 0.24),
-    -1px 1px 3px 0 rgba(0, 0, 0, 0.12);
-`;
-
-const CardContainer = styled.div`
-  ${(props) => (props.clickable ? clickableStyle : flatStyle)};
-  background-color: #ffffff;
-  padding: 16px;
-  border-radius: 6px;
-`;
-
-export const Cards = (props) => {
+const Cards = (props) => {
   try {
     return (
       <CardsContainer {...props}>
@@ -70,15 +52,4 @@ Cards.propTypes = {
   ]).isRequired,
 };
 
-export const Card = (props) => {
-  /**
-   * todo: if clackable
-   * - check for href, throw error if not present
-   * - listen for onclick event
-   */
-  return <CardContainer {...props[0]}>{props.children}</CardContainer>;
-};
-
-Card.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-};
+export default Cards;
