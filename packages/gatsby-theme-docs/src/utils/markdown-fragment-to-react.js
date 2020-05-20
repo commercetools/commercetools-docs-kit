93619544856f5d8a1cf9620e294dfcd73024981d
Adewale Oluyinka <davifantasia@hotmail.com>
2020-05-20 18:35:33 +0200

    refactor: now rendering parsed markdown fragment

--- packages/gatsby-theme-docs/src/utils/markdown-fragment-to-react.js
+++ packages/gatsby-theme-docs/src/utils/markdown-fragment-to-react.js
@@ -5,7 +5,7 @@ import markdown from 'remark-parse';
 import remark2react from 'remark-react';
 import frontmatter from 'remark-frontmatter';
 import { designSystem, Markdown } from '@commercetools-docs/ui-kit';
-import { Link } from '../components';
+import Link from '../components/link';
 
 const Div = styled.div``;
 const Heading = styled.p`
@@ -21,12 +21,19 @@ const Heading = styled.p`
  * - tables rendered as bullet lists (one list per row)
  * - code blocks rendered as inline code
  * - none of the custom MDX components available -> for example subtitle
+ *
+ * Elements can be customized with the customElements object, its structure should be like so:
+ * {
+ *  a: <custom-anchor-tag>,
+ *  p: <custom-paragrapgh-tag>,
+ * }
+ *
  * @param {String} markdownString
+ * @param {Object} customElements
  */
-
 const removeFrontmatter = () => (tree) =>
   filter(tree, (node) => node.type !== 'yaml');
-const markdownFragmentToReact = (markdownString) =>
+const markdownFragmentToReact = (markdownString, customElements) =>
   unified()
     .use(markdown, { commonmark: true })
     .use(frontmatter)
@@ -34,7 +41,7 @@ const markdownFragmentToReact = (markdownString) =>
     .use(remark2react, {
       sanitize: true,
       remarkReactComponents: {
-        p: Markdown.p,
+        p: Markdown.Paragraph,
         a: Link,
         h1: Heading,
         h2: Heading,
@@ -42,26 +49,27 @@ const markdownFragmentToReact = (markdownString) =>
         h4: Heading,
         h5: Heading,
         h6: Heading,
-        thematicBreak: Markdown.thematicBreak,
-        blockquote: Markdown.blockquote,
-        ul: Markdown.ul,
-        ol: Markdown.ol,
-        li: Markdown.li,
-        dl: Markdown.dl,
-        dt: Markdown.dt,
-        dd: Markdown.dd,
+        thematicBreak: Markdown.ThematicBreak,
+        blockquote: Markdown.Blockquote,
+        ul: Markdown.Ul,
+        ol: Markdown.Ol,
+        li: Markdown.Li,
+        dl: Markdown.Dl,
+        dt: Markdown.Dt,
+        dd: Markdown.Dd,
         table: Div,
         thead: Div,
         tbody: Div,
-        tr: Markdown.ul,
-        td: Markdown.li,
-        th: Markdown.li,
-        code: Markdown.code,
-        pre: Markdown.code,
-        em: Markdown.em,
-        strong: Markdown.strong,
-        delete: Markdown.delete,
-        hr: Markdown.hr,
+        tr: Markdown.Ul,
+        td: Markdown.Li,
+        th: Markdown.Li,
+        code: Markdown.InlineCode,
+        pre: Markdown.CodeBlock,
+        em: Markdown.Em,
+        strong: Markdown.Strong,
+        delete: Markdown.Delete,
+        hr: Markdown.Hr,
+        ...customElements,
       },
     })
     .processSync(markdownString).result;
