934fbcd6e55cc7dd8db54a5f720b6bcfb25c1efe
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2020-06-16 17:16:54 +0200

    fix(feed): working boilerplate with SWR

--- packages/ui-kit/src/components/rss-feeds.js
+++ packages/ui-kit/src/components/rss-feeds.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import useSWR from 'swr';
+import RssParser from 'rss-parser';
 import ContentNotifications from './content-notifications';
 
+async function fetcher(...args) {
+  const rss = new RssParser();
+  const feed = await rss.parseURL(args[0]);
+  return feed;
+}
+
 const RssFeeds = (props) => {
-  const { feed, error } = useSWR(props.url);
-
-  console.log(feed);
-
-  if (error) {
-    return <ContentNotifications.Error>{error}</ContentNotifications.Error>;
-  }
-
-  return <div>Feed: {JSON.stringify(feed)}</div>;
+  const { data, error } = useSWR(props.url, fetcher);
+  if (error)
+    return (
+      <ContentNotifications.Error>
+        Error Loading RSS feed from {props.url}
+      </ContentNotifications.Error>
+    );
+  if (!data) return <div>loading...</div>;
+  return (
+    <ul>
+      {data.items.map((item) => (
+        <li key={item.guid}>
+          <a href={item.link}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 RssFeeds.propTypes = {
