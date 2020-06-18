065391dc77e610c3202e9b9f871f16ff5565b681
Adewale Oluyinka <davifantasia@users.noreply.github.com>
2020-06-18 14:25:51 +0200

    feat: rss feeds (#497)
    * feat: rss feeds
    
    * Create khaki-apples-melt.md
    
    * refactor: rss feed component should accept only one rss url
    
    * refactor: appropriate width for rss feeds
    
    * refactor: solve flicker problem
    
    * fix: localstorage is not available server side
    
    * fix: localstorage is not available server side
    
    * fix: localstorage is not available server side
    
    * feat: render dates as links
    
    * refactor: read feeds with swr
    
    * chore: include swr in packaeg.json
    
    * refactor: fix styling error on table header

---
+++ packages/ui-kit/src/components/rss-feeds.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import css from '@emotion/css';
+import useSWR from 'swr';
+import Parser from 'rss-parser';
+import moment from 'moment';
+import LoadingSpinner from '@commercetools-uikit/loading-spinner';
+import ContentNotifications from './content-notifications';
+import { colors, tokens, dimensions } from '../design-system';
+import Link from './link';
+
+const Table = styled.table`
+  width: 100%;
+  border-radius: ${tokens.borderRadiusForRssFeedTable};
+  box-shadow: ${tokens.shadowForRssFeedTable};
+
+  thead {
+    th {
+      background-color: ${colors.light.surfaceSecondary1};
+      border-radius: ${tokens.borderRadiusForRssFeedTable}
+        ${tokens.borderRadiusForRssFeedTable} 0 0;
+      padding: ${dimensions.spacings.s};
+    }
+  }
+
+  tbody {
+    tr {
+      td {
+        padding: ${dimensions.spacings.m};
+
+        :first-of-type {
+          color: ${colors.light.linkHover};
+        }
+      }
+
+      :not(:first-of-type) {
+        border-top: 1px solid ${colors.light.borderPrimary};
+      }
+    }
+  }
+`;
+
+const RssFeeds = (props) => {
+  if (!props.url) {
+    const message = 'Must pass prop url to RssFeeds component.';
+    if (process.env.NODE_ENV !== 'production') {
+      return <ContentNotifications.Error>{message}</ContentNotifications.Error>;
+    }
+
+    throw new Error(message);
+  }
+
+  async function fetcher(...args) {
+    const rssParser = new Parser();
+    const feed = await rssParser.parseURL(args[0]);
+    return feed;
+  }
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { data, error } = useSWR(props.url, fetcher);
+
+  if (error) {
+    console.log(error);
+    return (
+      <ContentNotifications.Error>
+        Error Loading RSS feed from {props.url}
+      </ContentNotifications.Error>
+    );
+  }
+
+  return (
+    <div>
+      {!data ? (
+        <LoadingSpinner size="s">{'Loading feeds'}</LoadingSpinner>
+      ) : (
+        <Table key={data.title}>
+          <thead>
+            <tr>
+              <th colSpan="2">{data.title}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.items.map((item) => (
+              <tr key={item.title}>
+                <td>
+                  <Link
+                    href={item.link}
+                    css={css`
+                      text-decoration: none;
+                    `}
+                  >
+                    {moment(item.pubDate).format('D MMMM YYYY')}
+                  </Link>
+                </td>
+                <td>{item.title}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </div>
+  );
+};
+
+RssFeeds.propTypes = {
+  url: PropTypes.string.isRequired,
+};
+
+export default RssFeeds;
