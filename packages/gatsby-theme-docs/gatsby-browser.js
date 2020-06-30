70a589d1e7dab4342bed25d0d95b0069e967eaf3
Nicola Molinari <nicola.molinari@commercetools.de>
2020-06-30 16:45:16 +0200

    refactor: rename other occurrences of now

--- packages/gatsby-theme-docs/gatsby-browser.js
+++ packages/gatsby-theme-docs/gatsby-browser.js
@@ -11,7 +11,7 @@ import { CacheProvider } from '@emotion/core';
 import { docsCache } from './utils/create-emotion-cache';
 
 const isProduction = process.env.GATSBY_NODE_ENV === 'production';
-const commitSha = process.env.GATSBY_NOW_GITHUB_COMMIT_SHA;
+const commitSha = process.env.GATSBY_VERCEL_GITHUB_COMMIT_SHA;
 
 const injectScript = (url, attributes = {}, onLoad) => {
   const script = document.createElement('script');
