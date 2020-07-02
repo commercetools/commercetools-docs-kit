8fad8648beeffb5a61a3532f117001fe08d65663
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2020-07-02 17:46:25 +0200

    feat(theme): page data hook and docs for wide

--- packages/gatsby-theme-docs/index.js
+++ packages/gatsby-theme-docs/index.js
@@ -12,5 +12,5 @@ export {
   ThemeProvider,
 } from './src/components';
 export * from './src/hooks/use-site-data';
-export * from './src/hooks/use-page-metadata';
+export * from './src/hooks/use-page-data';
 export { default as markdownFragmentToReact } from './src/utils/markdown-fragment-to-react';
