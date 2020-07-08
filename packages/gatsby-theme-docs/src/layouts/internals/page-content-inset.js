c2d457d75cb6c189fce5744ab7c5ce0b7c5b1661
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2020-07-08 21:23:56 +0200

    docs(theme): comment page inset CSS which may look counterintuitive

--- packages/gatsby-theme-docs/src/layouts/internals/page-content-inset.js
+++ packages/gatsby-theme-docs/src/layouts/internals/page-content-inset.js
@@ -4,10 +4,15 @@ import { designSystem, Markdown } from '@commercetools-docs/ui-kit';
 const PageContentInset = styled.div`
   max-width: ${(props) =>
     props.maxWidth || designSystem.dimensions.widths.pageContentWide};
+  /* The actual body content of an MDX page is in direct descendants of the page's sections.
+     By default it is limited to the pageContent width, only specific elements are allowed
+     to take the full width, which only applies in the "wideDesktop" breakpoint. These
+     override this default.  */
   section > * {
     max-width: ${(props) =>
       props.maxWidth || designSystem.dimensions.widths.pageContent};
   }
+  /* overide that gives main section headings full width to have their underline go across the page */
   section > ${Markdown.H2} {
     max-width: unset;
   }
