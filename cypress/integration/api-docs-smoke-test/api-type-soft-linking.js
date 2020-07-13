1fa246aa65900c1a484b8e71d94bb035bfad5cc4
Adewale Oluyinka <davifantasia@hotmail.com>
2020-07-13 16:06:53 +0200

    test: remove select by label text for menu loink selectors

--- cypress/integration/api-docs-smoke-test/api-type-soft-linking.js
+++ cypress/integration/api-docs-smoke-test/api-type-soft-linking.js
@@ -3,10 +3,14 @@ import { URL_API_DOCS_SMOKE_TEST } from '../../support/urls';
 describe('API type soft linking', () => {
   it('should go to page of a type, then find the page of linked property type', () => {
     cy.visit(URL_API_DOCS_SMOKE_TEST);
-    cy.findByLabelText('Open ObjectTestType').click();
-    cy.findByLabelText('ObjectTestType definition').should('be.visible');
-    cy.findByLabelText('Link to ArrayTestType').click();
+    cy.get('#navigation-scroll-container').within(() => {
+      cy.findByText('ArrayTestType').click();
+    });
     cy.findByLabelText('ArrayTestType definition').should('be.visible');
+    cy.get('.section-lead').within(() => {
+      cy.findByText('IntegerTestType').click();
+    });
+    cy.findByLabelText('IntegerTestType definition').should('be.visible');
     cy.percySnapshot();
   });
 });
