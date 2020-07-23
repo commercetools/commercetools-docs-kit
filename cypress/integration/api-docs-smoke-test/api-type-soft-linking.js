3b40e70d9f313e4eee52f1b81d913fa06b73fafc
Adewale Oluyinka <davifantasia@hotmail.com>
2020-07-23 13:11:23 +0200

    refactor: use new soft link types for e2e test

--- cypress/integration/api-docs-smoke-test/api-type-soft-linking.js
+++ cypress/integration/api-docs-smoke-test/api-type-soft-linking.js
@@ -4,13 +4,13 @@ describe('API type soft linking', () => {
   it('should go to page of a type, then find the page of linked property type', () => {
     cy.visit(URL_API_DOCS_SMOKE_TEST);
     cy.get('#navigation-scroll-container').within(() => {
-      cy.findByText('ArrayTestType').click();
+      cy.findByText('Soft Linking First Page').click();
     });
-    cy.findByLabelText('ArrayTestType definition').should('be.visible');
+    cy.findByLabelText('SoftLinkObject definition').should('be.visible');
     cy.get('.section-lead').within(() => {
-      cy.findByText('IntegerTestType').click();
+      cy.findByText('SoftLinkArray').click();
     });
-    cy.findByLabelText('IntegerTestType definition').should('be.visible');
+    cy.findByLabelText('SoftLinkArray definition').should('be.visible');
     cy.percySnapshot();
   });
 });
