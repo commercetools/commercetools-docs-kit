5f00f4bbd0ccd4e32427dae93d153e000444fe05
Adewale Oluyinka <davifantasia@hotmail.com>
2020-07-24 15:42:41 +0200

    refactor: use Cypress.$.escapeSelector

--- cypress/integration/api-docs-smoke-test/api-type-soft-linking.js
+++ cypress/integration/api-docs-smoke-test/api-type-soft-linking.js
@@ -1,7 +1,7 @@
 import { URL_API_DOCS_SMOKE_TEST } from '../../support/urls';
 
 describe('API type soft linking', () => {
-  it('should go to page of a type, then find the page of linked property type', () => {
+  it('should go to page of a type, then confirm type exists on the page', () => {
     cy.visit(URL_API_DOCS_SMOKE_TEST);
     cy.get('#navigation-scroll-container').within(() => {
       cy.findByText('Soft Linking First Page').click();
@@ -11,24 +11,16 @@ describe('API type soft linking', () => {
       cy.findByText('SoftLinkArray').click();
     });
 
-    /**
-     * As stated in ../docs-smoke-test/viewports.js:
-     *  Currently snapshots with scroll postion are not supported by Percy.
-     *  https://github.com/percy/percy-cypress/issues/2#issuecomment-533316240
-     *
-     * So this test will instead:
-     *  - check if url is as expected
-     *  - check if element with expected id exists
-     *  - in this case the id should be ctp-test-type-softlinkarray
-     */
-    const typeElementId = 'ctp-test-type-softlinkarray';
+    const typeElementId = 'ctp:test:type:SoftLinkArray';
     cy.url().should(
       'eq',
       `${
         Cypress.config().baseUrl
       }${URL_API_DOCS_SMOKE_TEST}e2e-tests/soft-linking-second-page#${typeElementId}`
     );
-    cy.get('#body-content').find(`#${typeElementId}`).should('exist');
+    cy.get('#body-content')
+      .find(`#${Cypress.$.escapeSelector(typeElementId)}`)
+      .should('exist');
     cy.percySnapshot();
   });
 });
