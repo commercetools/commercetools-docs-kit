8d24bf1e862aaf0b587e7bae696fd51532462378
Adewale Oluyinka <davifantasia@hotmail.com>
2020-07-24 10:58:17 +0200

    refactor: test not working with current urn pattern

--- cypress/integration/api-docs-smoke-test/api-type-soft-linking.js
+++ cypress/integration/api-docs-smoke-test/api-type-soft-linking.js
@@ -2,7 +2,6 @@ import { URL_API_DOCS_SMOKE_TEST } from '../../support/urls';
 
 describe('API type soft linking', () => {
   it('should go to page of a type, then find the page of linked property type', () => {
-    cy.viewport('macbook-13');
     cy.visit(URL_API_DOCS_SMOKE_TEST);
     cy.get('#navigation-scroll-container').within(() => {
       cy.findByText('Soft Linking First Page').click();
@@ -11,7 +10,29 @@ describe('API type soft linking', () => {
     cy.get('.section-lead').within(() => {
       cy.findByText('SoftLinkArray').click();
     });
-    cy.findByLabelText('SoftLinkArray definition').should('be.visible');
+
+    /**
+     * As stated in ../docs-smoke-test/viewports.js:
+     *  Currently snapshots with scroll postion are not supported by Percy.
+     *  https://github.com/percy/percy-cypress/issues/2#issuecomment-533316240
+     *
+     * So this test will instead:
+     *  - check if element with expected id exists
+     *  - in this case the id should be ctp-test-type-softlinkarray
+     */
+
+    /**
+     * TODO:
+     *
+     * - urn id does not work with tests, will need to standardize pattern by replacing
+     * ":" with "-" and making sure letters are lower cased
+     * - api-docs-smoke-test-e2e replicates snapshots, no need to have separate project on percy
+     */
+    cy.get('#body-content')
+      .find('#ctp:test:type:SoftLinkArray')
+      .should('exist');
+    // cy.get('#ctp:test:type:SoftLinkArray').should('exist');
+    // cy.findByLabelText('SoftLinkArray definition').should('be.visible');
     cy.percySnapshot();
   });
 });
