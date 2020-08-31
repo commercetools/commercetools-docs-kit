8caec47171ce2406ec36e87c2c08967c94dbff55
Adewale Oluyinka <davifantasia@hotmail.com>
2020-08-31 17:04:23 +0200

    test: include integration test for page navigation with content menu

---
+++ cypress/integration/docs-smoke-test/page-navigation.js
@@ -0,0 +1,13 @@
+import { URL_DOCS_SMOKE_TEST } from '../../support/urls';
+
+describe('Page navigation', () => {
+  it('should highlight menu item appropriately when using content menu on page with 2 nav levels', () => {
+    cy.visit(`${URL_DOCS_SMOKE_TEST}views/two-level-index-nav#ipsum`);
+    cy.findByLabelText('Page Table of Contents Navigation').within(() => {
+      cy.get('a[href="#content-menu-with-first-level-heading"]')
+        .invoke('attr', 'aria-current')
+        .should('contain', 'false');
+    });
+    cy.percySnapshot();
+  });
+});
