17b2c2b1c804c64fcabfa5b65a1700fa3d0ced11
Adewale Oluyinka <davifantasia@hotmail.com>
2020-09-01 11:50:06 +0200

    refactor: another way to test for highlighted color

--- cypress/integration/docs-smoke-test/page-navigation.js
+++ cypress/integration/docs-smoke-test/page-navigation.js
@@ -5,7 +5,9 @@ describe('Page navigation', () => {
     cy.visit(`${URL_DOCS_SMOKE_TEST}views/two-level-index-nav#ipsum`);
     cy.findByLabelText('Page Table of Contents Navigation').within(() => {
       cy.get('a[href="#content-menu-with-first-level-heading"]')
-        .should('have.css', 'color', 'rgb(7, 140, 223)')
+        // this item is highlighted
+        .should('not.have.css', 'border-left-color', 'rgba(0, 0, 0, 0)')
+        // but this is not the current item
         .invoke('attr', 'aria-current')
         .should('contain', 'false');
     });
