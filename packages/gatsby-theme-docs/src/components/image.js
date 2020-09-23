a28b14b519f793a1d455f5d71eb3962599f3639a
Adewale Oluyinka <davifantasia@hotmail.com>
2020-09-23 21:26:31 +0200

    refactor: render no image

--- packages/gatsby-theme-docs/src/components/image.js
+++ packages/gatsby-theme-docs/src/components/image.js
@@ -1,29 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
-import { useImagesByRelativePath } from '../hooks/use-images';
-
-const Container = styled.span`
-  display: flex;
-  justify-content: center;
-`;
-
-const Image = (props) => {
-  const imageSharp = useImagesByRelativePath(props.src);
-
-  if (!imageSharp || !imageSharp.publicURL) {
-    return null;
-  }
-
-  return (
-    <Container>
-      <img {...props} src={imageSharp.publicURL} />
-    </Container>
-  );
-};
-
-Image.propTypes = {
-  src: PropTypes.string.isRequired,
-};
+const Image = () => null;
 
 export default Image;
