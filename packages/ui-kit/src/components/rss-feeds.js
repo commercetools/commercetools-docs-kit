e422c275539de72ac1f63c3ec0adfca65e2ce7cc
Timon Turak <timon.turak@commercetools.de>
2020-09-24 14:00:48 +0200

    refactor: use css prop and ignore binary

--- packages/ui-kit/src/components/rss-feeds.js
+++ packages/ui-kit/src/components/rss-feeds.js
@@ -87,7 +87,11 @@ const RssFeeds = (props) => {
           <tbody>
             {data.items.map((item, index) => (
               <tr key={`${item.title}${index}`}>
-                <td style={`width: ${dimensions.widths.rssFeedDateWidth}`}>
+                <td
+                  css={css`
+                    width: ${dimensions.widths.rssFeedDateWidth};
+                  `}
+                >
                   <DateWrapper>
                     <Link
                       href={item.link}
