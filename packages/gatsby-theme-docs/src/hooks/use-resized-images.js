ea56a3c7f066198541d39ce75ab9675513833e36
Nicola Molinari <nicola.molinari@commercetools.de>
2020-09-25 09:12:43 +0200

    fix: release notes image aspect ratio and HTML structure

--- packages/gatsby-theme-docs/src/hooks/use-resized-images.js
+++ packages/gatsby-theme-docs/src/hooks/use-resized-images.js
@@ -13,6 +13,7 @@ export const useResizedImages = () => {
               presentationWidth
               presentationHeight
               originalImg
+              aspectRatio
             }
           }
         }
