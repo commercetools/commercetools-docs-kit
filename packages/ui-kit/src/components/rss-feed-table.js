791d3dcecbc5ed4984bf4cef7daa5bee92ad0514
Timon Rey Turak <31767369+TimonRey@users.noreply.github.com>
2020-11-13 09:56:45 +0100

    refactor: restructure rss feed table (#743)
    * feat: resturcture feeds into one table
    
    * refactor: add changeset
    
    * refactor: rssfeed
    
    * refactor: replace transformer functions outside of component
    
    * refactor: lastEntryOfList function
    
    * refactor: set link for release notes
    
    * refactor: remove title width
    
    * refactor: add unit tests and documentation
    
    * refactor: remove log in test
    
    * refactor: remove head row of table
    
    * refactor: add left padding for space column
    
    * refactor: rss-feed test
    
    * refactor: modify table for single feeds
    
    * fix: build error
    
    * refactor: remove title from props
    
    * refactor: ignore file for text linter
    
    * refactor: proptypes
    
    * refactor: changeset

---
+++ packages/ui-kit/src/components/rss-feed-table.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import css from '@emotion/css';
+import moment from 'moment';
+import Link from './link';
+import ContentNotifications from './content-notifications';
+import { colors, tokens, dimensions } from '../design-system';
+
+const Table = styled.table`
+  width: 100%;
+  border-radius: ${tokens.borderRadiusForRssFeedTable};
+  box-shadow: ${tokens.shadowForRssFeedTable};
+
+  tbody {
+    tr {
+      td {
+        padding: ${dimensions.spacings.m};
+
+        :first-of-type {
+          color: ${colors.light.linkHover};
+        }
+      }
+
+      :not(:first-of-type) {
+        border-top: 1px solid ${colors.light.borderPrimary};
+      }
+    }
+  }
+`;
+
+const DateWrapper = styled.span`
+  white-space: nowrap;
+`;
+
+const RssFeedTable = (props) => {
+  if (!props.data) {
+    const message = 'Must pass data to RssFeedTable component.';
+    if (process.env.NODE_ENV !== 'production') {
+      return <ContentNotifications.Error>{message}</ContentNotifications.Error>;
+    }
+
+    throw new Error(message);
+  }
+
+  return (
+    <div>
+      <Table>
+        <tbody>
+          {props.data.map((item, index) => (
+            <tr key={`${item.title}${index}`}>
+              <td
+                css={css`
+                  white-space: nowrap;
+                `}
+              >
+                <DateWrapper>
+                  <Link
+                    href={item.link}
+                    css={css`
+                      text-decoration: none;
+                    `}
+                  >
+                    {moment(item.pubDate).format('D MMMM YYYY')}
+                  </Link>
+                </DateWrapper>
+              </td>
+              {props.hasMultipleSources ? (
+                <td
+                  css={css`
+                    white-space: nowrap;
+                    padding-left: ${dimensions.spacings.l} !important;
+                  `}
+                >
+                  <Link
+                    href={item.releaseNoteUrl}
+                    css={css`
+                      text-decoration: none;
+                    `}
+                  >
+                    {item.feedName}
+                  </Link>
+                </td>
+              ) : null}
+              <td>{item.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+RssFeedTable.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      feedName: PropTypes.string.isRequired,
+      releaseNoteUrl: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  hasMultipleSources: PropTypes.bool.isRequired,
+};
+
+export default RssFeedTable;
--- packages/ui-kit/src/design-system.js
+++ packages/ui-kit/src/design-system.js
@@ -119,7 +119,7 @@ export const tokens = {
   -1px 1px 3px 0 rgba(0, 0, 0, 0.12)`,
   shadowForClickableCardOnHover: `0 14px 28px 0 rgba(0, 0, 0, 0.25),
   0 10px 10px 0 rgba(0, 0, 0, 0.23)`,
-  shadowForRssFeedTable: '0 1px 2px 0 rgba(0, 0, 0, 0.25)',
+  shadowForRssFeedTable: customProperties.shadow7,
 };
 
 export const dimensions = {
@@ -139,7 +139,6 @@ export const dimensions = {
     pageNavigation: navbarWidth,
     pageNavigationSmall: navbarWidthSmall,
     marketingContent: '1168px',
-    rssFeedDateWidth: '180px',
     searchBar: '300px',
     searchBarSmall: '150px',
     selectDropDownArrowWith: '12px',
