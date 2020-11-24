1ffa02c62487ed63481407db735ff5182f753642
Nicola Molinari <nicola.molinari@commercetools.de>
2020-11-24 21:01:45 +0100

    refactor: use generated folder

--- packages/gatsby-theme-docs/src/icons/index.js
+++ packages/gatsby-theme-docs/src/icons/index.js
@@ -1,7 +1,1 @@
-export { default as LogoHorizontalSvgIcon } from './LogoHorizontal';
-export { default as LogoSvgIcon } from './Logo';
-export { default as ReleaseNotesSvgIcon } from './ReleaseNotes';
-export { default as RssSvgIcon } from './Rss';
-export { default as SearchSvgIcon } from './Search';
-export { default as SlashSvgIcon } from './Slash';
-export { default as StackedLinesIndentedIconSvgIcon } from './StackedLinesIndentedIcon';
+export * from './generated';
