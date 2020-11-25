11cf525cf477658c3b46a3aa0f2d2cd49c208c65
Timon Turak <timon.turak@commercetools.de>
2020-11-25 11:12:05 +0100

    refactor: margin for banner in top menu

--- packages/gatsby-theme-docs/src/overrides/top-menu-banner-area.js
+++ packages/gatsby-theme-docs/src/overrides/top-menu-banner-area.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { css } from '@emotion/core';
+import { designSystem } from '@commercetools-docs/ui-kit';
 import UserResearchBanner from '../components/user-research-banner';
 
 // A React component to be rendered on the right side of the top-menu
 // eslint-disable-next-line react/display-name
 export default () => {
-  return <UserResearchBanner />;
+  return (
+    <div>
+      <div
+        css={css`
+          margin-top: ${designSystem.dimensions.spacings.m};
+        `}
+      >
+        <UserResearchBanner />
+      </div>
+    </div>
+  );
 };
