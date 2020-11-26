417c868016d1ca2e375ec3b1dcce99743d8f04b6
Nicola Molinari <nicola.molinari@commercetools.de>
2020-11-26 20:30:22 +0100

    fix(theme): restore SVG loader for webpack

--- packages/gatsby-theme-docs/gatsby-node.js
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -447,6 +447,41 @@ exports.onCreateWebpackConfig = ({ actions, getConfig }, themeOptions) => {
   const pluginOptions = { ...defaultOptions, ...themeOptions };
 
   const config = getConfig();
+  config.module.rules = [
+    ...config.module.rules.map((rule) => ({
+      ...rule,
+      test:
+        // Strip out the svg files from the following built-in rule
+        // See https://github.com/zabute/gatsby-plugin-svgr/blob/5087926076e61a0d5681c842af42c73d55a89653/gatsby-node.js#L10-L21
+        String(rule.test) ===
+        String(/\.(ico|svg|jpg|jpeg|png|gif|webp)(\?.*)?$/)
+          ? /\.(ico|jpg|jpeg|png|gif|webp)(\?.*)?$/
+          : rule.test,
+    })),
+    {
+      // Fix for react-intl
+      // https://github.com/formatjs/formatjs/issues/143#issuecomment-518774786
+      test: /\.mjs$/,
+      type: 'javascript/auto',
+    },
+    {
+      test: /\.svg$/,
+      include: /icons/,
+      use: [
+        {
+          loader: require.resolve('@svgr/webpack'),
+          options: {
+            // NOTE: disable this and manually add `removeViewBox: false` in the SVGO plugins list
+            // See related PR: https://github.com/smooth-code/svgr/pull/137
+            icon: false,
+            svgoConfig: {
+              plugins: [{ removeViewBox: false }, { cleanupIDs: true }],
+            },
+          },
+        },
+      ],
+    },
+  ];
   config.resolve = {
     ...config.resolve,
     // Add support for absolute imports
