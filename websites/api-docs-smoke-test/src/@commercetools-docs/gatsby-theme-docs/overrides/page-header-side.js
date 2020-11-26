f2e9590c6059154dd2519ab212f1836529e20f45
Timon Turak <timon.turak@commercetools.de>
2020-11-26 14:08:33 +0100

    fix: lint test

--- websites/api-docs-smoke-test/src/@commercetools-docs/gatsby-theme-docs/overrides/page-header-side.js
+++ websites/api-docs-smoke-test/src/@commercetools-docs/gatsby-theme-docs/overrides/page-header-side.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import GitHubIcon from '../../../icons/github.svg';
+import GithubSvgIcon from '../../../icons/github.svg';
 
 // A React component to be rendered in the top-right corner of a content page
 // eslint-disable-next-line react/display-name
 export default () => {
   return (
     <div>
-      <GitHubIcon />
+      <GithubSvgIcon />
     </div>
   );
 };
