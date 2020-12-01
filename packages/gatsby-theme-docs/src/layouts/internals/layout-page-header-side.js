94369514641d9201fe5d5ac3e9434e9eaac39fad
Timon Turak <timon.turak@commercetools.de>
2020-12-01 11:35:25 +0100

    fix: width on wide layout

--- packages/gatsby-theme-docs/src/layouts/internals/layout-page-header-side.js
+++ packages/gatsby-theme-docs/src/layouts/internals/layout-page-header-side.js
@@ -4,7 +4,8 @@ import { designSystem } from '@commercetools-docs/ui-kit';
 const LayoutPageHeaderSide = styled.aside`
   grid-area: page-header-side;
   display: none;
-  padding: 0 ${designSystem.dimensions.spacings.m};
+  padding: 0 ${designSystem.dimensions.spacings.l} 0
+    ${designSystem.dimensions.spacings.m};
   margin: ${designSystem.dimensions.spacings.l} 0;
   border-left: 1px solid ${designSystem.colors.light.borderPrimary};
 
@@ -24,7 +25,7 @@ const LayoutPageHeaderSide = styled.aside`
   @media screen and (${designSystem.dimensions.viewports.desktop}) {
     width: calc(
       ${designSystem.dimensions.widths.pageNavigation} -
-        ${designSystem.dimensions.spacings.m} * 2
+        ${designSystem.dimensions.spacings.l} * 2
     );
   }
 `;
