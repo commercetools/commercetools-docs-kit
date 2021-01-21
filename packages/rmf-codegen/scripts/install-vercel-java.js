b68c47dc8fac90588b168c58973aa904ac19b3d2
Adewale Oluyinka <davifantasia@hotmail.com>
2021-01-21 15:56:14 +0100

    refactor: move log into appropriate block

--- packages/rmf-codegen/scripts/install-vercel-java.js
+++ packages/rmf-codegen/scripts/install-vercel-java.js
@@ -1,9 +1,9 @@
 const shelljs = require('shelljs');
 const { javaIsInstalled, abortIfError } = require('./helpers');
 
-console.info('Installing Java on Vercel.');
-
 if (!javaIsInstalled() && shelljs.which('amazon-linux-extras')) {
+  console.info('Installing Java on Vercel.');
+
   abortIfError(
     shelljs.exec('amazon-linux-extras install java-openjdk11', { silent: true })
   );
