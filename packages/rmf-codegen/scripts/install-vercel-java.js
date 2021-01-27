cb1fe979e9ea55519fefcdcb8cda6467e26446bc
Adewale Oluyinka <davifantasia@hotmail.com>
2021-01-27 11:55:37 +0100

    refactor: change log text

--- packages/rmf-codegen/scripts/install-vercel-java.js
+++ packages/rmf-codegen/scripts/install-vercel-java.js
@@ -2,7 +2,7 @@ const shelljs = require('shelljs');
 const { javaIsInstalled, abortIfError } = require('./helpers');
 
 if (!javaIsInstalled() && shelljs.which('amazon-linux-extras')) {
-  console.info('Installing Java on Vercel.');
+  console.info('Installing Java with amazon-linux-extras.');
 
   abortIfError(
     shelljs.exec('amazon-linux-extras install java-openjdk11', { silent: true })
