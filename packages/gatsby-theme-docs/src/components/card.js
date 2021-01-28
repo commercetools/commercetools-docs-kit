921c136cc5703fad7f504c5eeab033b22f0bbc1c
Nicola Molinari <nicola.molinari@commercetools.de>
2021-01-28 18:23:27 +0100

    fix: emotion import, update lockfile (#850)

--- packages/gatsby-theme-docs/src/components/card.js
+++ packages/gatsby-theme-docs/src/components/card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import css from '@emotion/css';
+import { css } from '@emotion/react';
 import { designSystem, Markdown } from '@commercetools-docs/ui-kit';
 import markdownFragmentToReact from '../utils/markdown-fragment-to-react';
 import Link from './link';
