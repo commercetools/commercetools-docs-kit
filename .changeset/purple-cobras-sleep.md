a6cc2ae2d2cf542fff654918798f0f1cc50f27eb
Nikolaus Kühn <nikolaus.kuehn@commercetools.com>
2021-03-05 09:17:57 +0100

    Format union type members as heading and make them linkable (#879)
    * feat: format union type members as heading, linkable
    
    * Add changeset

---
+++ .changeset/purple-cobras-sleep.md
@@ -0,0 +1,5 @@
+---
+"@commercetools-docs/gatsby-theme-api-docs": patch
+---
+
+Format union type members as heading and make them linkable
--- packages/gatsby-theme-api-docs/src/components/type/children-union-like.js
+++ packages/gatsby-theme-api-docs/src/components/type/children-union-like.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SpacingsStack from '@commercetools-uikit/spacings-stack';
+import { Markdown } from '@commercetools-docs/ui-kit';
 import Description from '../description';
 import Children from './children';
+import { generateTypeURN } from '../../utils/ctp-urn';
+
+const Heading = Markdown.withAnchorLink(Markdown.H4);
 
 const ChildrenUnionLike = (props) => {
   return (
@@ -12,19 +16,21 @@ const ChildrenUnionLike = (props) => {
       )}
 
       <SpacingsStack scale="m">
-        {props.apiTypeSubTypes.map((subType) => (
-          <SpacingsStack key={subType.displayName} scale="s">
-            <p>
-              <strong>{subType.displayName}</strong>
-            </p>
-            <Children
-              apiKey={props.apiKey}
-              apiType={subType}
-              doNotRenderExamples={props.doNotRenderExamples}
-              hideInheritedProperties={props.hideInheritedProperties}
-            />
-          </SpacingsStack>
-        ))}
+        {props.apiTypeSubTypes.map((subType) => {
+          return (
+            <SpacingsStack key={subType.displayName} scale="s">
+              <Heading id={generateTypeURN(subType)}>
+                {subType.displayName}
+              </Heading>
+              <Children
+                apiKey={props.apiKey}
+                apiType={subType}
+                doNotRenderExamples={props.doNotRenderExamples}
+                hideInheritedProperties={props.hideInheritedProperties}
+              />
+            </SpacingsStack>
+          );
+        })}
       </SpacingsStack>
     </SpacingsStack>
   );
