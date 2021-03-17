0aaed64e8468bc1bd3590575c552e62022519314
Timon Turak <timon.turak@commercetools.de>
2021-03-17 13:47:30 +0100

    feat: add selectable topics

--- packages/gatsby-theme-docs/bin/create-release-note.js
+++ packages/gatsby-theme-docs/bin/create-release-note.js
@@ -5,68 +5,67 @@ const path = require('path');
 const slugify = require('slugify');
 const prompts = require('prompts');
 const moment = require('moment');
+const { cosmiconfigSync } = require('cosmiconfig');
 
-const questions = [
-  {
-    type: 'multiselect',
-    name: 'type',
-    message: 'Pick the type of the release note',
-    choices: [
-      {
-        title: 'enhancement',
-        value: 'enhancement',
-        description: 'for updates of existing features',
-      },
-      {
-        title: 'feature',
-        value: 'feature',
-        description: 'for the introduction of an entirely new feature',
-      },
-      {
-        title: 'fix',
-        value: 'fix',
-        description: 'for bug fixes',
-      },
-      {
-        title: 'announcement',
-        value: 'announcement',
-        description:
-          'for release notes with no product change nor immediate product change',
-      },
-    ],
-  },
-  {
-    type: 'text',
-    name: 'date',
-    message: 'Insert the release date with the following format: YYYY-MM-DD',
-    validate: (date) => {
-      const checkDateFormat = moment(date, 'YYYY-MM-DD', true);
-      return checkDateFormat.isValid() ? true : 'Please use a valid format!';
+const selectQuestions = (customizedQuestions) => {
+  return [
+    {
+      type: 'multiselect',
+      name: 'type',
+      message: 'Pick the type of the release note',
+      choices: [
+        {
+          title: 'enhancement',
+          value: 'enhancement',
+          description: 'for updates of existing features',
+        },
+        {
+          title: 'feature',
+          value: 'feature',
+          description: 'for the introduction of an entirely new feature',
+        },
+        {
+          title: 'fix',
+          value: 'fix',
+          description: 'for bug fixes',
+        },
+        {
+          title: 'announcement',
+          value: 'announcement',
+          description:
+            'for release notes with no product change nor immediate product change',
+        },
+      ],
     },
-  },
-  {
-    type: 'text',
-    name: 'title',
-    message: 'Insert the title for the release note. Write it in plain text.',
-    validate: (title) =>
-      title.length > 256 ? 'Title is longer than 256 chars!' : true,
-  },
-  {
-    type: 'text',
-    name: 'description',
-    message:
-      'Describe the key changes and their advantages briefly. Max 256 character plain text. This text is tweet-able and used in RSS feeds.',
-    validate: (description) =>
-      description.length > 256 ? 'Description is longer than 256 chars!' : true,
-  },
-  {
-    type: 'list',
-    name: 'topics',
-    message:
-      'Fill in the topics for this release note. Seperate them with commas.',
-    separator: ',',
-  },
-];
+    {
+      type: 'text',
+      name: 'date',
+      message: 'Insert the release date with the following format: YYYY-MM-DD',
+      validate: (date) => {
+        const checkDateFormat = moment(date, 'YYYY-MM-DD', true);
+        return checkDateFormat.isValid() ? true : 'Please use a valid format!';
+      },
+    },
+    {
+      type: 'text',
+      name: 'title',
+      message: 'Insert the title for the release note. Write it in plain text.',
+      validate: (title) =>
+        title.length > 256 ? 'Title is longer than 256 chars!' : true,
+    },
+    {
+      type: 'text',
+      name: 'description',
+      message:
+        'Describe the key changes and their advantages briefly. Max 256 character plain text. This text is tweet-able and used in RSS feeds.',
+      validate: (description) =>
+        description.length > 256
+          ? 'Description is longer than 256 chars!'
+          : true,
+    },
+    customizedQuestions,
+  ];
+};
 
 (async () => {
   const folderpath = path.join(process.cwd(), `src/releases`);
@@ -75,7 +74,42 @@ const questions = [
     process.exit();
   }
 
-  const response = await prompts(questions);
+  const getQuestions = () => {
+    const moduleName = 'release-note-topic-config';
+    const explorerSync = cosmiconfigSync(moduleName, {
+      searchPlaces: [`${moduleName}.yml`],
+    });
+    const configFile = explorerSync.search();
+
+    if (configFile) {
+      const topics = configFile.config;
+
+      const topicChoices = (topics) => {
+        return Object.keys(topics).map((topic) => {
+          const title = topics[topic].name;
+          const description = topics[topic].description;
+          return { title, value: title, description: description || '' };
+        });
+      };
+      const multiselectTopicQuestion = {
+        type: 'multiselect',
+        name: 'topics',
+        message: 'Select the topics for this release note.',
+        choices: topicChoices(topics),
+      };
+      return selectQuestions(multiselectTopicQuestion);
+    }
+    const listTopicQuestion = {
+      type: 'list',
+      name: 'topics',
+      message:
+        'No topic configuration found. Please fill in the topics for this release note. Seperate them with commas.',
+      separator: ',',
+    };
+    return selectQuestions(listTopicQuestion);
+  };
+
+  const response = await prompts(getQuestions());
 
   const template = `---
 date: ${response.date}
