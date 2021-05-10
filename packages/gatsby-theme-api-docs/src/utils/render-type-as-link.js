b508d318ce9addb12c874e1a3ddc572ba9ec0207
Timon Turak <timon.turak@commercetools.de>
2021-05-10 08:47:27 +0200

    fix: rename to url

--- packages/gatsby-theme-api-docs/src/utils/render-type-as-link.js
+++ packages/gatsby-theme-api-docs/src/utils/render-type-as-link.js
@@ -5,7 +5,7 @@ import { locationForType } from '../hooks/use-type-locations';
 function renderTypeAsLink(apiKey, type, typeLocations) {
   const typeLocation = locationForType(apiKey, type, typeLocations);
 
-  const originalTypeLocation = typeLocation ? typeLocation.urlAnchorTag : '';
+  const originalTypeLocation = typeLocation ? typeLocation.url : '';
 
   return originalTypeLocation ? (
     <Link href={originalTypeLocation}>{type}</Link>
