adab23aa765698dee538f94bbbb38d3870beccb5
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-29 10:44:01 +0100

    refactor(ci): yarn cache to use gastby cache artifacts folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,21 +6,17 @@ echo "Preparing production builds."
 
 yarn build-packages
 yarn generate-icons
-
-if [ -n "$VERCEL" ]; then
-  yarn node ./scripts/gatsby-cache.mjs pre
-fi
+yarn node ./scripts/gatsby-cache.mjs pre
 
 echo "Building Gatsby websites."
 
 cross-env NODE_ENV=production yarn workspaces foreach --include '@commercetools-website/*' run build
-
-if [ -n "$VERCEL" ]; then
-  yarn node ./scripts/gatsby-cache.mjs post
-fi
+yarn node ./scripts/gatsby-cache.mjs post
 
 echo "Copying test index.html file into public folder."
+
 cp -f ./websites/index.html ./public
 
 echo "Running link checker"
+
 yarn check-links
