8a2c3d6b9389c9aa92ddfad38aed754c43cc6ed7
Nicola Molinari <nicola.molinari@commercetools.com>
2021-11-29 12:40:47 +0100

    chore: use gatsby cache incremental builds (#1158)
    * chore: properly cache Gatsby artifacts to use incremental build optimizations
    
    * chore: trigger ci
    
    * fix: run gatsby cache script only on vercel
    
    * refactor(ci): yarn cache to use gastby cache artifacts folder

--- websites/site-template/scripts/build.sh
+++ websites/site-template/scripts/build.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-echo "Preparing production build for @commercetools-website/site-template."
-
-yarn clean
-
 echo "Building Gatsby website @commercetools-website/site-template."
 
-yarn gatsby build --prefix-paths
-
-echo "Copying public folder of @commercetools-website/site-template into root public folder."
-
-rm -rf ../../public/site-template
-mkdir -p ../../public
-mv public ../../public/site-template
+yarn gatsby build --prefix-paths --verbose --log-pages
