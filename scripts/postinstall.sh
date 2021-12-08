16790df87622c4c890b369cda8135c8a93aa957d
Nicola Molinari <nicola.molinari@commercetools.com>
2021-12-08 10:26:32 +0100

    fix: canary builds (#1171)

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,11 +6,19 @@ if [ -n "$VERCEL" ]; then
   echo "Running on Vercel, skipping development setup."
 
 else
-  echo "Preparing development setup."
 
-  yarn husky install
-  yarn manypkg check
-  yarn preconstruct dev
+  if [ -n "$SKIP_POSTINSTALL_DEV_SETUP" ]; then
+  echo "Skipping development setup."
+
+  else
+    echo "Preparing development setup."
+    yarn husky install
+    yarn manypkg check
+    yarn preconstruct dev
+
+  fi
+
+  echo "Running prettier on package.json files"
   # We need to run prettier to avoid unnecessary formatting changes to package.json (due to Yarn install).
   yarn prettier --write --parser json '**/package.json' &>/dev/null
 fi
