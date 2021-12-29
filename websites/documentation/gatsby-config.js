b2a4502bebff33efc371a83ac935099a36349f88
Timon Turak <timon.turak@commercetools.de>
2021-12-29 14:20:03 +0100

    chore: set betaLink default to '.'

--- websites/documentation/gatsby-config.js
+++ websites/documentation/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
   siteMetadata: {
     title: 'Docs Kit Docs',
     description: '',
-    betaLink: '',
+    betaLink: '.',
   },
   plugins: [
     ...configureThemeWithAddOns({
