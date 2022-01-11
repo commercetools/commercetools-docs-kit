99173a2916d3bd094637b9908a069410c8701155
Nikolaus Kuehn <nikolaus.kuehn@commercetools.com>
2022-01-11 16:08:59 +0100

    Revert "feat: proposal for different rendering of enums"
    This reverts commit b26f17f1ce7219b5c0b6ebb41b7327d92e5aae80.

--- packages/gatsby-theme-api-docs/src/components/type/enum.js
+++ packages/gatsby-theme-api-docs/src/components/type/enum.js
@@ -3,61 +3,36 @@ import PropTypes from 'prop-types';
 import { Markdown } from '@commercetools-docs/ui-kit';
 import { DescriptionText } from '../description';
 
-const hasDescriptions = (enumDescriptions) => {
-  return (
-    enumDescriptions &&
-    enumDescriptions.some(
-      (enumDesc) => enumDesc.description && enumDesc.description.length > 0
-    )
-  );
-};
-
 const Enum = ({ values, enumDescriptions }) => {
   if (!values) {
     throw new Error(
       'Must pass values props to Enum component --- <Enum values={<Array>} />.'
     );
   }
-  if (hasDescriptions(enumDescriptions)) {
-    return (
-      <Markdown.Table>
-        <Markdown.TableRow>
-          <Markdown.TableHeader>Value</Markdown.TableHeader>
-          <Markdown.TableHeader>Description</Markdown.TableHeader>
-        </Markdown.TableRow>
-        {values &&
-          values.map((value) => {
-            const enumDescription = enumDescriptions.find(
-              (enumDesc) => enumDesc.name === value
-            );
-            return (
-              <Markdown.TableRow key={value}>
-                <Markdown.TableCell>
-                  <Markdown.InlineCode>{value}</Markdown.InlineCode>
-                </Markdown.TableCell>
-                <Markdown.TableCell>
-                  {enumDescription && (
-                    <DescriptionText
-                      markdownString={enumDescription.description}
-                    />
-                  )}
-                </Markdown.TableCell>
-              </Markdown.TableRow>
-            );
-          })}
-      </Markdown.Table>
-    );
-  } else {
-    return (
-      <Markdown.Ul>
-        {values.map((value) => (
-          <Markdown.Li key={value}>
-            <Markdown.InlineCode>{value}</Markdown.InlineCode>
-          </Markdown.Li>
-        ))}
-      </Markdown.Ul>
-    );
-  }
+
+  return (
+    <Markdown.Dl>
+      {values &&
+        values.map((value) => {
+          const enumDescription =
+            enumDescriptions &&
+            enumDescriptions.find((enumDesc) => enumDesc.name === value);
+
+          return (
+            <React.Fragment key={value}>
+              <Markdown.Dt>{value}</Markdown.Dt>
+              {enumDescription && enumDescription.description && (
+                <Markdown.Dd>
+                  <DescriptionText
+                    markdownString={enumDescription.description}
+                  />
+                </Markdown.Dd>
+              )}
+            </React.Fragment>
+          );
+        })}
+    </Markdown.Dl>
+  );
 };
 
 Enum.propTypes = {
