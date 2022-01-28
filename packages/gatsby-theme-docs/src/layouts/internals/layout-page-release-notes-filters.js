58eb0a6aab81ff5ce0f3de9746205e1643181683
Timon Turak <timon.turak@commercetools.de>
2022-01-28 09:57:46 +0100

    refactor: use designSystem for zIndex + remove scrollbarWith

--- packages/gatsby-theme-docs/src/layouts/internals/layout-page-release-notes-filters.js
+++ packages/gatsby-theme-docs/src/layouts/internals/layout-page-release-notes-filters.js
@@ -54,7 +54,6 @@ const stickyContainerCss = css({
 const stuckContainerCss = css(stickyContainerCss, {
   maxHeight: '100vh',
   overflow: 'auto',
-  scrollbarWidth: 'thin',
 });
 
 const StickyContainer = (props) => {
@@ -199,7 +198,7 @@ const LayoutPageReleaseNotesFilters = (props) => {
     return modalPortalNode
       ? ReactDOM.createPortal(
           <Overlay
-            zIndex="22"
+            zIndex={designSystem.dimensions.stacks.aboveOverlay}
             justifyContent="flex-end"
             onClick={() => {
               setMenuOpen(false);
