a362fa4e614aecc7037845f6b4b52de6fd5b7813
Nikolaus Kühn <nikolaus.kuehn@commercetools.com>
2022-03-28 14:38:22 +0200

    Ability to write mermaid diagrams via code blocks (like gitHub) (#1253)
    * feat: first working version of mermaid diagrams
    
    * chore: cleanup and propose better architecture
    
    * refactor: proper react implementation and style fixes
    
    * refactor: move mermaid diagram rendering to ui-kit
    
    * docs: add changeset
    
    * docs: document mermaid, images and add docs homepage with feature list
    
    * fix: adjust import and pre-commit typecheck
    
    * fix: part 2 of adjusting precommit (forgotten files)
    
    * fix: downgrade unified packages to pe-ESM again
    
    * fix: writing style in docs
    
    * fix: remove leftover dependencies from theme package
    
    * docs: remove wrong documentation text from smoke test

---
+++ packages/ui-kit/src/components/mermaid.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import LoadingSpinner from '@commercetools-uikit/loading-spinner';
+
+// This is a client-side only implementation of mermaid, which is its primary development target.
+// https://www.gatsbyjs.com/docs/using-client-side-only-packages/
+// Server side rendering mermaid diagrams is technically possible
+// and some remark and gatsby plugins do it. But all are badly maintained
+// and the server side rendering use case is not covered in the mermaid documentation,
+// which makes it a hard to maintain architecture.
+// server side rendering also has the disadvantage that clickable elements cannot
+// work because this requires attaching event handlers.
+
+const MermaidLazy = React.lazy(() => import('./mermaid-client-side'));
+
+type MermaidProps = {
+  graph: string;
+};
+
+const Mermaid = (props: MermaidProps) => {
+  const isClientSide = typeof window !== 'undefined';
+  return (
+    <>
+      {isClientSide && (
+        <React.Suspense
+          fallback={<LoadingSpinner scale="l" maxDelayDuration={500} />}
+        >
+          <MermaidLazy graph={props.graph} />
+        </React.Suspense>
+      )}
+    </>
+  );
+};
+
+export default Mermaid;
