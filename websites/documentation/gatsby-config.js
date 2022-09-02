5d359c7967d92f3d0c74bafebfcf627da545385c
Timon Turak <timon.turak@commercetools.com>
2022-09-02 13:30:24 +0200

    chore: add sso plugin to root microsite

--- websites/documentation/gatsby-config.js
+++ websites/documentation/gatsby-config.js
@@ -20,5 +20,12 @@ module.exports = {
       additionalPrismLanguages: ['java', 'scala', 'csharp', 'swift', 'php'],
       addOns: ['@commercetools-docs/gatsby-theme-code-examples'],
     }),
+    {
+      resolve: '@commercetools-docs/gatsby-theme-sso-ui-kit',
+      options: {
+        auth0Domain: 'commercetools-professionals.eu.auth0.com',
+        auth0ClientId: 'nZwQWPaoTq8IIk67VV8o0Ska9lGp4hnW',
+      },
+    },
   ],
 };
