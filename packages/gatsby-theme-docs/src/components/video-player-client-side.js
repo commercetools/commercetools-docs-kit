20735b088d00c643d17d9f7929b2d885a93514f1
Gabriele Antonini <gabriele.antonini@commercetools.com>
2022-10-20 15:07:25 +0200

    feat(video player): initial working implementation (no ssr)

--- packages/gatsby-theme-docs/src/components/video-player-client-side.js
+++ packages/gatsby-theme-docs/src/components/video-player-client-side.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import videojs from 'video.js';
 import PropTypes from 'prop-types';
+import 'video.js/dist/video-js.css';
 
-const VideoPlayer = () => {
-  return <div>Client Site VideoPlayer</div>;
+const VideoPlayer = (props) => {
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    if (!playerRef.current) {
+      const videoElement = videoRef.current;
+
+      if (!videoElement) return;
+
+      const player = (playerRef.current = videojs(
+        videoElement,
+        props.options,
+        () => {
+          videojs.log('player is ready');
+          props.onReady && props.onReady(player);
+        }
+      ));
+    }
+  }, [props, videoRef]);
+
+  useEffect(() => {
+    const player = playerRef.current;
+    return () => {
+      if (player) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player>
+      <video ref={videoRef} className="video-js vjs-big-play-centered" />
+    </div>
+  );
 };
-// VideoPlayer.propTypes = {
-//   video: PropTypes.string.isRequired,
-// };
+VideoPlayer.propTypes = {
+  onReady: PropTypes.func,
+  options: PropTypes.object,
+};
 
 export default VideoPlayer;
