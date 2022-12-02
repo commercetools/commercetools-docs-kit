1c56b3b5f9d262f658081a65f466b72deb9165b2
Timon Rey Turak <31767369+timonrey@users.noreply.github.com>
2022-12-02 10:54:47 +0100

    Search result improvements (#1443)
    * chore: display 'commercetools' before the product name
    
    * chore: define new meta tag for docsearch
    
    * chore: add changeset
    
    * chore: add fallbacks

--- algolia-crawler.js
+++ algolia-crawler.js
@@ -29,7 +29,10 @@ new Crawler({
           // FYI selector documentation: https://github.com/cheeriojs/cheerio#selectors
           recordProps: {
             lvl0: {
-              selectors: '#site-title',
+              selectors:
+                $('meta[name=commercetools:title-for-onsite-search]').attr(
+                  'content'
+                ) || '#site-title',
               defaultValue: 'General Topics',
             },
             lvl1: 'article h1',
