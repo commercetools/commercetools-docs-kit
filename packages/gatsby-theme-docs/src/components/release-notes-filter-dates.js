eec45068e202a60ab87428ee54b524fa91b4d456
Gabriele Antonini <gabriele.antonini@commercetools.com>
2022-12-07 22:18:19 +0100

    chore: sync state onblur

--- packages/gatsby-theme-docs/src/components/release-notes-filter-dates.js
+++ packages/gatsby-theme-docs/src/components/release-notes-filter-dates.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import { designSystem, IsoDateFormat } from '@commercetools-docs/ui-kit';
 import SpacingsStack from '@commercetools-uikit/spacings-stack';
@@ -50,16 +50,16 @@ const ClearAll = styled.button`
 
 const ReleaseNotesFilterDates = () => {
   const [filterParams, setFilterParams] = useReleaseNotesFilterParams();
+  const [fromDate, setFromFilterDate] = useState();
+  const [toDate, setToFilterDate] = useState();
   const maximumDate = IsoDateFormat.format(new Date());
 
   useEffect(() => {
     if (filterParams.fromFilterDate) {
-      document.getElementById('from-filter-date').value =
-        filterParams.fromFilterDate;
+      setFromFilterDate(filterParams.fromFilterDate);
     }
     if (filterParams.toFilterDate) {
-      document.getElementById('to-filter-date').value =
-        filterParams.fromFilterDate;
+      setToFilterDate(filterParams.toFilterDate);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -80,7 +80,9 @@ const ReleaseNotesFilterDates = () => {
             type="date"
             id="from-filter-date"
             max={maximumDate}
+            value={fromDate}
             onChange={handleOnFromFilterDateChange}
+            onBlur={(e) => handleOnBlur(e, 'from')}
           />
         </div>
       </SpacingsStack>
@@ -92,7 +94,9 @@ const ReleaseNotesFilterDates = () => {
             type="date"
             id="to-filter-date"
             max={maximumDate}
+            value={toDate}
             onChange={handleOnToFilterDateChange}
+            onBlur={(e) => handleOnBlur(e, 'to')}
           />
         </div>
       </SpacingsStack>
@@ -105,9 +109,9 @@ const ReleaseNotesFilterDates = () => {
       toFilterDate: undefined,
       filterTopics: [],
     });
-    document.getElementById('from-filter-date').value = '';
-    document.getElementById('to-filter-date').value = '';
-    scrollToTop();
+    setFromFilterDate('');
+    setToFilterDate('');
+    setFilterParams({ fromFilterDate: undefined, toFilterDate: undefined });
   }
 
   function handleOnFromFilterDateChange(e) {
@@ -118,8 +122,9 @@ const ReleaseNotesFilterDates = () => {
       return;
     }
     if (date.length === 10) {
-      setFilterParams({ fromFilterDate: e.target.value || undefined });
-      scrollToTop();
+      setFromFilterDate(date);
+    } else if (date.length !== 10 && fromDate) {
+      setFromFilterDate(undefined);
     }
   }
 
@@ -131,9 +136,20 @@ const ReleaseNotesFilterDates = () => {
       return;
     }
     if (date.length === 10) {
-      setFilterParams({ toFilterDate: e.target.value || undefined });
-      scrollToTop();
+      setToFilterDate(date);
+    } else if (date.length !== 10 && fromDate) {
+      setToFilterDate(undefined);
     }
+  }
+
+  function handleOnBlur(event, field) {
+    event.stopPropagation();
+    if (field === 'from') {
+      setFilterParams({ fromFilterDate: fromDate || undefined });
+    } else {
+      setFilterParams({ toFilterDate: toDate || undefined });
+    }
+    scrollToTop();
   }
 };
 
