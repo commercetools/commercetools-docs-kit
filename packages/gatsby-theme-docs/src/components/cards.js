976afbb12ca2ef3c2040951dcce03ff951e76aa8
Gabriele Antonini <gabriele.antonini@commercetools.com>
2023-01-11 17:39:10 +0100

    feat(mdx2): remove un-necessary safe check from the cards wrapper component

--- packages/gatsby-theme-docs/src/components/cards.js
+++ packages/gatsby-theme-docs/src/components/cards.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
 import { ContentNotifications, cardElements } from '@commercetools-docs/ui-kit';
 
 const Cards = (props) => {
   try {
     return (
       <cardElements.CardsContainer {...props} data-search-key="cards-container">
-        {React.Children.map(props.children, (child) => {
-          if (!React.isValidElement(child)) {
-            throwErrorMessage(child);
-          } else if (
-            child.type.displayName === 'MDXCreateElement' &&
-            child.props.mdxType !== 'Card'
-          ) {
-            // this is created in mdx but it is not a card
-            throwErrorMessage(child.props.mdxType);
-          }
-
-          return React.cloneElement(
-            child,
-            {
-              clickable: props.clickable,
-              narrow: props.narrow,
-              smallTitle: props.smallTitle,
-            },
-            child.props.children
-          );
-        })}
+        {props.children}
       </cardElements.CardsContainer>
     );
   } catch (e) {
@@ -38,12 +17,6 @@ const Cards = (props) => {
     }
 
     throw e;
-  }
-
-  function throwErrorMessage(type) {
-    throw new Error(
-      `Children of <Cards> must be a <Card> component and not "${type}"`
-    );
   }
 };
 
