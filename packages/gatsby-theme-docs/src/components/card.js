4750c840f5c889b0e1f4d8b65c0e19cec520bd74
Gabriele Antonini <gabriele.antonini@commercetools.com>
2023-01-20 12:50:24 +0100

    feat(mdx2): clickable cards processing re-write

--- packages/gatsby-theme-docs/src/components/card.js
+++ packages/gatsby-theme-docs/src/components/card.js
@@ -24,6 +24,69 @@ WrapWith.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
+const isReactLink = (alienElement) =>
+  React.isValidElement(alienElement) &&
+  (alienElement?.type?.name || '') === 'Link';
+
+/**
+ * Expects a React Link element, returns just the text contained in the link.
+ * It handles only the simple case where the link text is a simple string.
+ */
+const removeLink = (linkNode) => {
+  if (isReactLink(linkNode)) {
+    if (typeof linkNode.props.children === 'string') {
+      return linkNode.props.children;
+    }
+    return '';
+  }
+};
+
+/**
+ * This component main purpose is to manage the case in which a clickable card has
+ * a body with a link defined. This is an invalid case as you'll end up with 2 nesed links.
+ * So the processChildren function run through the body content removing any link and just
+ * leaving a plain string (non linked).
+ */
+const BodyContent = (props) => {
+  if (props.clickable) {
+    return processChildren(props.children);
+  }
+  return props.children;
+};
+
+BodyContent.displayName = 'BodyContent';
+BodyContent.propTypes = {
+  clickable: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+const processChildren = (childrenNode) => {
+  // it's just a string... phew!
+  if (typeof childrenNode === 'string') {
+    return childrenNode;
+  }
+  // React component witch is a Link
+  if (isReactLink(childrenNode)) {
+    return removeLink(childrenNode);
+  }
+  // any other React component
+  if (React.isValidElement(childrenNode)) {
+    return React.cloneElement(
+      childrenNode,
+      undefined,
+      processChildren(childrenNode.props.children)
+    );
+  }
+  // array
+  if (Array.isArray(childrenNode)) {
+    const result = [];
+    childrenNode.forEach((nodeItem) => {
+      result.push(processChildren(nodeItem));
+    });
+    return result;
+  }
+};
+
 const Card = (props) => (
   <CardContainer {...props}>
     <WrapWith
@@ -54,7 +117,11 @@ const Card = (props) => (
                 <Title smallTitle={props.smallTitle}>{props.title}</Title>
               )}
               {props.children && (
-                <BodyContainer>{props.children}</BodyContainer>
+                <BodyContainer>
+                  <BodyContent clickable={props.clickable}>
+                    {props.children}
+                  </BodyContent>
+                </BodyContainer>
               )}
               {props.href && props.textLink && (
                 <ReadMoreContainer>
