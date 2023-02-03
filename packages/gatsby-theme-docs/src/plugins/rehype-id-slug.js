096df8e2927a016a2cacbcfd7f797680907255a8
Gabriele Antonini <gabriele.antonini@commercetools.com>
2023-02-03 13:50:27 +0100

    chore: fix comment

--- packages/gatsby-theme-docs/src/plugins/rehype-id-slug.js
+++ packages/gatsby-theme-docs/src/plugins/rehype-id-slug.js
@@ -9,11 +9,8 @@ const slugs = new Slugger();
 /**
  * This plugins is a re-implementation of rehype-slug https://github.com/rehypejs/rehype-slug
  * The purpose of the plugin is to add id to headings based on the text content.
- * This re-implementation simply ensure that the ids don't contain trailing dashes or underscores, the
+ * This re-implementation simply ensure that the ids get postProcessed by a custom `postProcess` function, the
  * rest of the implementation works exactly like the original.
- *
- * TODO: ehnancement: add the possibility of passing in a 'options.postProcess' function to be able to sanitize
- * the slugs with a custom function implementation
  */
 module.exports =
   (options = {}) =>
