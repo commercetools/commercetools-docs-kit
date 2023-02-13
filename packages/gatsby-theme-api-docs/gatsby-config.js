250e670c5d48d9104a202f6e9be794449aca10d6
Nikolaus Kuehn <nikolaus.kuehn@commercetools.com>
2023-02-13 22:01:59 +0100

    chore: remove leftovers from mdx-introspection

--- packages/gatsby-theme-api-docs/gatsby-config.js
+++ packages/gatsby-theme-api-docs/gatsby-config.js
@@ -1,17 +1,6 @@
 const path = require('path');
 
-const getTagListOption = (themeOptions) => {
-  if (!themeOptions.transformerMdx) return [];
-  return (
-    themeOptions.transformerMdx.tagList ||
-    // backwards compatibility
-    themeOptions.transformerMdx.tagWhitelist
-  );
-};
-
 module.exports = (themeOptions = {}) => {
-  // Extract the `tagList` from the theme options if specified
-  const additionalTags = getTagListOption(themeOptions);
   return {
     plugins: [
       {
