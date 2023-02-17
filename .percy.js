f0e8403ac9522846a62107156ff00de8cd5016e2
Nikolaus Kuehn <nikolaus.kuehn@commercetools.com>
2023-02-17 08:59:42 +0100

    fix: fix percy config and lint it as CI

--- .percy.js
+++ .percy.js
@@ -2,26 +2,26 @@ module.exports = {
   version: 2,
   static: {
     include: [
-      'docs-smoke-test/components/**/index.html',
-      'docs-smoke-test/views/markdown/index.html',
-      'docs-smoke-test/views/beta/index.html',
-      'docs-smoke-test/releases/index.html',
-      'api-docs-smoke-test/types/**/index.html',
-      'api-docs-smoke-test/layouts/**/index.html',
-      'api-docs-smoke-test/endpoints/**/index.html',
+      /(.*-smoke-test.*)\/components\/(.*)\.html/,
+      /(.*-smoke-test.*)\/endpoints\/(.*)\.html/,
+      /(.*-smoke-test.*)\/layouts\/(.*)\.html/,
+      /(.*-smoke-test.*)\/releases\/index\.html/,
+      /(.*-smoke-test.*)\/types\/(.*)\.html/,
+      /(.*-smoke-test.*)\/views\/markdown\/(.*)\.html/,
+      /(.*-smoke-test.*)\/views\/beta\/(.*)\.html/,
     ],
-    exclude: ['site-template/*', 'documentation/*', 'index.html'],
+    exclude: ['site-template/*'],
     options: [
       {
-        include: 'docs-smoke-test/components/rss-feeds/index.html',
+        include: /(.*-smoke-test.*)\/components\/rss-feeds\/index\.html/,
         waitForSelector: 'table[data-table-name="rss-feed"]',
       },
       {
-        include: 'docs-smoke-test/components/mermaid-diagram/index.html',
+        include: /(.*-smoke-test.*)\/components\/mermaid-diagram\/index\.html/,
         waitForSelector: '#body-content svg.statediagram',
       },
       {
-        include: 'docs-smoke-test/components/video-player/index.html',
+        include: /(.*-smoke-test.*)\/components\/video-player\/index\.html/,
         waitForSelector: '#body-content div#vjs_video_998',
       },
     ],
