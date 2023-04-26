7660724ab7950226c2d9d63abd10e9003abb946e
Timon Rey Turak <31767369+timonrey@users.noreply.github.com>
2023-04-26 14:11:51 +0200

    Adjust avatar component according to Zoe's design (#1615)
    * refactor: adjust avatar according to Zoes design
    
    * chore: add changeset
    
    * refactor: use parameters for signup
    
    * refactor: use designsystem props
    
    * fix: tests
    
    * refactor: use correct spacing between signup button and search box
    
    * fix: tests
    
    * refactor: remove unused dependencies
    
    * Toggle login/logout buttons over the `hideLogin` flag in the sso plugin config (#1622)
    
    * feat: hide login buttons in the top bar with a flag in the config
    
    * chore: add changeset
    
    * chore: remove changeset for this PR
    
    * chore: fix tests
    
    * chore: adjust tests
    
    * chore: toggle login buttons via new plugin config parameter
    
    * feat: enhance quiz login button + restructure buttons
    
    * refactor: rename override components + move hideLogin evaluation to top lvl
    
    * chore: change data-test-id in e2e test
    
    * chore: add gatsby-ssr.js to sso plugin
    
    * refactor: use right test id to identify login button
    
    * fix: lint test
    
    * chore: adjust e2e common steps
    
    * fix: last e2e test
    
    * refactor: logout e2e test
    
    * chore: add appState/returnTo parameter to signup button

--- packages/gatsby-theme-sso-ui-kit/src/components/config-context.ts
+++ packages/gatsby-theme-sso-ui-kit/src/components/config-context.ts
@@ -4,12 +4,14 @@ export type Config = {
   learnApiBaseUrl: string;
   auth0Domain: string;
   auth0ClientId: string;
+  hideLogin: boolean;
 };
 
 const ConfigContext = createContext<Config>({
   learnApiBaseUrl: '',
   auth0Domain: '',
   auth0ClientId: '',
+  hideLogin: false,
 });
 
 export default ConfigContext;
