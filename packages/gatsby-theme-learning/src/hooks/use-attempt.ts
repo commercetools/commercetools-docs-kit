559ef9d0816b1b9d7c0ae6f304d6be9615f50637
Gabriele Antonini <106664971+gabriele-ct@users.noreply.github.com>
2023-04-27 11:21:06 +0200

    chore: fix lint warnings for missing hook dependencies (#1637)
    * chore: fix lint warnings for missing hook dependencies
    
    * chore: address PR comments

--- packages/gatsby-theme-learning/src/hooks/use-attempt.ts
+++ packages/gatsby-theme-learning/src/hooks/use-attempt.ts
@@ -45,6 +45,7 @@ export const useAttempt = (fetchAttemptParams: FetchAttemptParams) => {
       });
       return data;
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [courseId, getAuthToken, learnApiBaseUrl, quizId]
   );
 
