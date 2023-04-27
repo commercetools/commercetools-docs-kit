f8cf44024c0cdfbe140a150874dcbe7c9b17d68a
Gabriele Antonini <106664971+gabriele-ct@users.noreply.github.com>
2023-04-27 12:01:05 +0200

    feat: add IsUserLoggedIn or IsUserLoggedOut component (#1634)
    * feat: add IsUserLoggedIn or IsUserLoggedOut component
    
    * chore: add changeset
    
    * chore: add documentation
    
    * chore: pr  comments
    
    * chore: added assumeTrue prop
    
    * chore: disable assumetrue

---
+++ packages/gatsby-theme-learning/src/components/if-user-logged.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { markdownFragmentToReact } from '@commercetools-docs/ui-kit';
+
+const content = (children: ReactNode | string) => {
+  if (typeof children === 'string') {
+    return markdownFragmentToReact(children as string);
+  }
+  return children;
+};
+
+type IfLoggedInProps = {
+  children: ReactNode;
+  assumeTrue: boolean;
+};
+
+export const IfUserLoggedIn = (props: IfLoggedInProps) => {
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading && props.assumeTrue) {
+    return content(props.children);
+  } else {
+    return isAuthenticated ? content(props.children) : null;
+  }
+};
+
+type IsLoggedOutProps = {
+  children: ReactNode;
+  assumeTrue: boolean;
+};
+
+export const IfUserLoggedOut = (props: IsLoggedOutProps) => {
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading && props.assumeTrue) {
+    return content(props.children);
+  } else {
+    return !isAuthenticated ? content(props.children) : null;
+  }
+};
