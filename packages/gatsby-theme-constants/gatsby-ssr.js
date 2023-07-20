cfda60f631b2d9aa66d60693e00d07b3f697d5a0
Gabriele Antonini <gabriele.antonini@commercetools.com>
2023-07-20 11:02:19 +0200

    chore: revert ssr bundling hack

--- packages/gatsby-theme-constants/gatsby-ssr.js
+++
@@ -1,5 +0,0 @@
-// start build hack to force components are are injected into all MDX pages into a central bundle at SSR generation time (memory problems)
-import additionalContentComponents from './shortcodes';
-// eslint-disable-next-line no-unused-vars
-const doSomethingFakeWithTheImports = [additionalContentComponents];
-// end build hack to force shared components into a central place at SSR bundling time
