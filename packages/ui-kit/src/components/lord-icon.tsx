e1f6c9dadd8aaf9f49348352f1f75f9381d2a34c
Gabriele Antonini <gabriele.antonini@commercetools.com>
2023-08-01 09:49:29 +0200

    feat: add lordicon component with dynamic lottie json loading

--- packages/ui-kit/src/components/lord-icon.tsx
+++ packages/ui-kit/src/components/lord-icon.tsx
@@ -1,32 +1,45 @@
 import { useEffect, useState } from 'react';
-import Lottie from 'react-lottie';
+import Lottie from 'lottie-react';
 
 type LordIconProps = {
   iconName: string;
+  loop: boolean;
+  autoplay: boolean;
+  stype: object;
 };
 
 const LordIcon = (props: LordIconProps) => {
   const [isClient, setClient] = useState(false);
   const [animationData, setAnimationData] = useState(null);
+  const { iconName, ...rest } = props;
 
   useEffect(() => {
     setClient(true);
 
-    // Dynamically import the JSON file based on the icon name
-    import(`../icons/lord-icon/${props.iconName}.json`)
-      .then((data) => {
+    const loadAnimationData = async () => {
+      console.log('loading');
+      try {
+        const data = await import(
+          `@commercetools-docs/ui-kit/dist/icons/lord-icon/${iconName}.json`
+        );
         setAnimationData(data.default);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error(`Error loading icon animation: ${error}`);
-      });
-  }, [props.iconName]);
+      }
+    };
+    loadAnimationData();
+  }, [iconName]);
 
-  useEffect(() => {
-    if (!isClient) {
-      return <p>SSR</p>;
-    }
-  }, [isClient, animationData]);
+  if (!isClient) {
+    // If SSR, return the static SVG
+    return <p>SSR</p>;
+  }
+  // if (!animationData) {
+  //   // If the animation data is not loaded yet, you can display a loading state or a fallback.
+  //   return <div>Loading...</div>;
+  // }
+
+  return <Lottie animationData={animationData} {...rest} />;
 };
 
 export default LordIcon;
