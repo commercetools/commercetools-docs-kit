4bdbb23da1858958a85fad99bb2efc6754fc93f6
Gabriele Antonini <gabriele.antonini@commercetools.com>
2023-08-02 13:10:46 +0200

    chore: exclude generated folder from eslint

--- jest.eslint.config.js
+++ jest.eslint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   runner: 'jest-runner-eslint',
   displayName: 'eslint',
   moduleFileExtensions: ['js', 'ts', 'tsx', 'mjs'],
-  modulePathIgnorePatterns: ['.yarn', '.cache', 'dist', 'public'],
+  modulePathIgnorePatterns: ['.yarn', '.cache', 'dist', 'public', 'generated'],
   testMatch: ['<rootDir>/**/*.js', '<rootDir>/**/*.ts', '<rootDir>/**/*.tsx'],
   watchPlugins: ['jest-watch-typeahead/filename'],
 };
