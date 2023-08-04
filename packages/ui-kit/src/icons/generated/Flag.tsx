78ffc2ce26b1842ae176f759276e8e88105e6bf4
Gabriele Antonini <gabriele.antonini@commercetools.com>
2023-08-04 13:09:01 +0200

    chore: added generated megamenu icons

---
+++ packages/ui-kit/src/icons/generated/Flag.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgFlag = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={500}
+    height={500}
+    style={{
+      width: '100%',
+      height: '100%',
+      transform: 'translate3d(0,0,0)',
+      contentVisibility: 'visible',
+    }}
+    viewBox="0 0 500 500"
+    {...props}
+  >
+    <defs>
+      <clipPath id="flag_svg__d">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <clipPath id="flag_svg__b">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <clipPath id="flag_svg__c">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <filter id="flag_svg__f" width="300%" height="300%" x="-100%" y="-100%">
+        <feGaussianBlur result="filter_result_0" stdDeviation="52.5 52.5" />
+      </filter>
+      <mask id="flag_svg__e" mask-type="alpha">
+        <use href="#flag_svg__a" />
+      </mask>
+      <g
+        id="flag_svg__a"
+        fillOpacity={0}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        clipPath="url(#flag_svg__b)"
+        style={{
+          display: 'block',
+        }}
+      >
+        <path
+          stroke="#121331"
+          strokeWidth={12}
+          d="m-115.914-13.182-.207 180.636"
+          style={{
+            display: 'block',
+          }}
+          transform="translate(250.151 248.818)"
+        />
+        <g
+          clipPath="url(#flag_svg__c)"
+          style={{
+            display: 'block',
+          }}
+        >
+          <path
+            stroke="#08A88A"
+            strokeWidth={12}
+            d="M-116.185-3.241v-162.577c53.284-9.75 89.039 11.576 139.537 11.576 38.247 0 100.257-16.267 100.257-16.267L71.856-79.221l51.253 71.789s-66.004 19.847-98.757 19.77c-46.84-.11-113.52-32.156-140.537-15.579z"
+            style={{
+              display: 'block',
+            }}
+            transform="translate(250.151 240.818)"
+          />
+        </g>
+      </g>
+    </defs>
+    <g
+      clipPath="url(#flag_svg__d)"
+      mask="url(#flag_svg__e)"
+      style={{
+        display: 'block',
+      }}
+    >
+      <g
+        filter="url(#flag_svg__f)"
+        transform="rotate(-94 262.075 8.435) scale(2.4)"
+      >
+        <path
+          fill="#4AE0EB"
+          d="M0-250c137.975 0 250 112.025 250 250S137.975 250 0 250-250 137.975-250 0-137.975-250 0-250z"
+          className="flag_svg__design"
+        />
+        <path
+          fill="#CA5EED"
+          d="M113.242-295.384c97.478 0 176.5 79.022 176.5 176.5s-79.022 176.5-176.5 176.5c-43.948 0-74.396-34.057-105.29-60.631-37.631-32.369-71.21-62.338-71.21-115.869 0-97.478 79.022-176.5 176.5-176.5z"
+          className="flag_svg__design"
+        />
+      </g>
+    </g>
+  </svg>
+);
+export default SvgFlag;
---
+++ packages/ui-kit/src/icons/generated/Heartbeat.tsx
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgHeartbeat = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={500}
+    height={500}
+    style={{
+      width: '100%',
+      height: '100%',
+      transform: 'translate3d(0,0,0)',
+      contentVisibility: 'visible',
+    }}
+    viewBox="0 0 500 500"
+    {...props}
+  >
+    <defs>
+      <clipPath id="heartbeat_svg__f">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <clipPath id="heartbeat_svg__c">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <clipPath id="heartbeat_svg__d">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <mask id="heartbeat_svg__e" mask-type="alpha">
+        <use href="#heartbeat_svg__a" />
+      </mask>
+      <mask id="heartbeat_svg__g" mask-type="alpha">
+        <use xmlns:ns2="http://www.w3.org/1999/xlink" ns2:href="#b" />
+      </mask>
+      <filter
+        id="heartbeat_svg__h"
+        width="300%"
+        height="300%"
+        x="-100%"
+        y="-100%"
+      >
+        <feGaussianBlur result="filter_result_0" stdDeviation="52.5 52.5" />
+      </filter>
+      <path
+        id="heartbeat_svg__a"
+        fill="red"
+        d="M250 161.6c19.2-34.81 58.04-55.78 96.64-39.33 0 0 52.18 17.66 48.26 79.43-3.91 61.78-82.8 148.33-144.9 181.73m0-221.83c-19.2-34.81-58.04-55.78-96.64-39.33 0 0-52.18 17.66-48.26 79.43 3.91 61.78 82.8 148.33 144.9 181.73"
+        style={{
+          display: 'block',
+        }}
+      />
+      <g
+        id="heartbeat_svg__b"
+        fillOpacity={0}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        clipPath="url(#heartbeat_svg__c)"
+        style={{
+          display: 'block',
+        }}
+      >
+        <g
+          clipPath="url(#heartbeat_svg__d)"
+          mask="url(#heartbeat_svg__e)"
+          style={{
+            display: 'block',
+          }}
+        >
+          <path
+            stroke="#08A284"
+            strokeWidth={12}
+            d="m-146.3.186 60.09-.191 14.42 28.83 28.82-57.65 14.42 28.82h57.41l14.42 28.83 28.82-57.65L86.52-.005l58.78-.061"
+            style={{
+              display: 'block',
+            }}
+            transform="translate(250 243.318)"
+          />
+        </g>
+        <path
+          stroke="#121331"
+          strokeWidth={12}
+          d="M72.558-88.4c19.201-34.813 58.038-55.777 96.644-39.328 0 0 52.18 17.653 48.261 79.428-3.919 61.775-82.802 148.332-144.905 181.733m0-221.833c-19.201-34.813-58.038-55.777-96.644-39.328 0 0-52.18 17.653-48.261 79.428 3.919 61.775 82.802 148.332 144.905 181.733"
+          style={{
+            display: 'block',
+          }}
+          transform="translate(177.442 250)"
+        />
+      </g>
+    </defs>
+    <g
+      clipPath="url(#heartbeat_svg__f)"
+      mask="url(#heartbeat_svg__g)"
+      style={{
+        display: 'block',
+      }}
+    >
+      <g
+        filter="url(#heartbeat_svg__h)"
+        transform="rotate(-94 262.075 8.435) scale(2.4)"
+      >
+        <path
+          fill="#4AE0EB"
+          d="M0-250c137.975 0 250 112.025 250 250S137.975 250 0 250-250 137.975-250 0-137.975-250 0-250z"
+          className="heartbeat_svg__design"
+        />
+        <path
+          fill="#CA5EED"
+          d="M113.242-295.384c97.478 0 176.5 79.022 176.5 176.5s-79.022 176.5-176.5 176.5c-43.948 0-74.396-34.057-105.29-60.631-37.631-32.369-71.21-62.338-71.21-115.869 0-97.478 79.022-176.5 176.5-176.5z"
+          className="heartbeat_svg__design"
+        />
+      </g>
+    </g>
+  </svg>
+);
+export default SvgHeartbeat;
