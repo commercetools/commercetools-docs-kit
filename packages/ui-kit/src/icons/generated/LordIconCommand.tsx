fa0c97fd4ffaebb213a1ad9ed97bfa41fdc87829
Gabriele Antonini <gabriele.antonini@commercetools.com>
2023-08-07 10:30:28 +0200

    chore: move svg lord-icons to sg folder and streamline the icon generation process

---
+++ packages/ui-kit/src/icons/generated/LordIconCommand.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgLordIconCommand = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={500}
+    height={500}
+    style={{
+      width: '100%',
+      height: '100%',
+      transform: 'translate3d(0,0,0)',
+      contentVisibility: 'visible',
+    }}
+    viewBox="0 0 500 500"
+    {...props}
+  >
+    <defs>
+      <clipPath id="lord-icon-command_svg__b">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <filter
+        id="lord-icon-command_svg__d"
+        width="300%"
+        height="300%"
+        x="-100%"
+        y="-100%"
+      >
+        <feGaussianBlur result="filter_result_0" stdDeviation="52.5 52.5" />
+      </filter>
+      <mask id="lord-icon-command_svg__c" mask-type="alpha">
+        <use href="#lord-icon-command_svg__a" />
+      </mask>
+      <g
+        id="lord-icon-command_svg__a"
+        fillOpacity={0}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        style={{
+          display: 'block',
+        }}
+      >
+        <path
+          stroke="#121331"
+          strokeWidth={12}
+          d="M387.712 410.12H112.288c-12.375 0-22.407-10.033-22.407-22.408V112.288c0-12.375 10.032-22.407 22.407-22.407h275.424c12.375 0 22.407 10.032 22.407 22.407v275.424c0 12.375-10.032 22.408-22.407 22.408zm20.407-268.866H89.881"
+        />
+        <path
+          stroke="#08A88A"
+          strokeWidth={18}
+          d="M177.864 115.567H178m-56.432 0h.136m28.012 0h.136"
+        />
+        <path
+          stroke="#08A88A"
+          strokeWidth={12}
+          d="M247.76 328.335H338m-167.996 0 52.648-52.649-52.648-52.649"
+        />
+      </g>
+    </defs>
+    <g
+      clipPath="url(#lord-icon-command_svg__b)"
+      mask="url(#lord-icon-command_svg__c)"
+      style={{
+        display: 'block',
+      }}
+    >
+      <g
+        filter="url(#lord-icon-command_svg__d)"
+        transform="rotate(-94 262.075 8.435) scale(2.4)"
+      >
+        <path
+          fill="#4AE0EB"
+          d="M0-250c137.975 0 250 112.025 250 250S137.975 250 0 250-250 137.975-250 0-137.975-250 0-250z"
+          className="lord-icon-command_svg__design"
+        />
+        <path
+          fill="#CA5EED"
+          d="M113.242-295.384c97.478 0 176.5 79.022 176.5 176.5s-79.022 176.5-176.5 176.5c-43.948 0-74.396-34.057-105.29-60.631-37.631-32.369-71.21-62.338-71.21-115.869 0-97.478 79.022-176.5 176.5-176.5z"
+          className="lord-icon-command_svg__design"
+        />
+      </g>
+    </g>
+  </svg>
+);
+export default SvgLordIconCommand;
---
+++ packages/ui-kit/src/icons/generated/LordIconDocument.tsx
@@ -0,0 +1,216 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgLordIconDocument = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={500}
+    height={500}
+    style={{
+      width: '100%',
+      height: '100%',
+      transform: 'translate3d(0,0,0)',
+      contentVisibility: 'visible',
+    }}
+    viewBox="0 0 500 500"
+    {...props}
+  >
+    <defs>
+      <clipPath id="lord-icon-document_svg__j">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <clipPath id="lord-icon-document_svg__e">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <clipPath id="lord-icon-document_svg__h">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <clipPath id="lord-icon-document_svg__g">
+        <path d="M0 0h500v500H0z" />
+      </clipPath>
+      <mask id="lord-icon-document_svg__i" mask-type="alpha">
+        <use href="#lord-icon-document_svg__a" />
+      </mask>
+      <mask id="lord-icon-document_svg__f" mask-type="alpha">
+        <g filter="url(#lord-icon-document_svg__b)">
+          <use href="#lord-icon-document_svg__c" />
+        </g>
+      </mask>
+      <mask id="lord-icon-document_svg__k" mask-type="alpha">
+        <use href="#lord-icon-document_svg__d" />
+      </mask>
+      <g
+        id="lord-icon-document_svg__d"
+        clipPath="url(#lord-icon-document_svg__e)"
+        style={{
+          display: 'block',
+        }}
+      >
+        <g
+          mask="url(#lord-icon-document_svg__f)"
+          style={{
+            display: 'block',
+          }}
+        >
+          <g
+            fillOpacity={0}
+            stroke="#08A88A"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3.5}
+            clipPath="url(#lord-icon-document_svg__g)"
+            transform="translate(-.751 .001)"
+          >
+            <path
+              d="M-16.959-19.849H20.2"
+              style={{
+                display: 'block',
+              }}
+              transform="matrix(3.7 0 0 3.7 245.375 425.288)"
+            />
+            <path
+              d="M-16.959-19.849H20.2"
+              style={{
+                display: 'block',
+              }}
+              transform="matrix(3.7 0 0 3.7 245.375 373.488)"
+            />
+            <path
+              d="M-16.959-19.849H20.2"
+              style={{
+                display: 'block',
+              }}
+              transform="matrix(3.7 0 0 3.7 245.375 321.688)"
+            />
+            <path
+              d="M-16.959-19.849H20.2"
+              style={{
+                display: 'block',
+              }}
+              transform="matrix(3.7 0 0 3.7 245.375 269.888)"
+            />
+          </g>
+        </g>
+        <path
+          fillOpacity={0}
+          stroke="#08A88A"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={3.5}
+          d="M28.966 43.958h-57.932v-86.963h41.893l8.02 8.02 8.019 8.019v70.924z"
+          style={{
+            display: 'block',
+          }}
+          transform="matrix(3.7 0 0 3.7 248.149 251.85)"
+        />
+        <path
+          fillOpacity={0}
+          stroke="#08A88A"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={3.5}
+          d="M-15.898 43.958h-13.068V28.989"
+          style={{
+            display: 'block',
+          }}
+          transform="matrix(3.7 0 0 3.7 403.549 -12.7)"
+        />
+        <g
+          style={{
+            display: 'block',
+          }}
+        >
+          <path d="M299.915 96.3h-3.5l-.25 53h53.5l-.125-3.375L299.915 96.3z" />
+          <path
+            fillOpacity={0}
+            stroke="red"
+            strokeWidth={0}
+            d="M299.915 96.3h-3.5l-.25 53h53.5l-.125-3.375L299.915 96.3z"
+          />
+        </g>
+      </g>
+      <g
+        id="lord-icon-document_svg__a"
+        style={{
+          display: 'block',
+        }}
+      >
+        <path
+          fill="red"
+          d="M510.723 149.946H296.374v-321.764H451.38l29.674 29.674 29.67 29.67v262.42z"
+        />
+        <path
+          fillOpacity={0}
+          stroke="#131432"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={12.95}
+          d="M510.723 149.946H296.374v-321.764H451.38l29.674 29.674 29.67 29.67v262.42z"
+        />
+      </g>
+      <g
+        id="lord-icon-document_svg__c"
+        style={{
+          display: 'block',
+        }}
+      >
+        <path
+          fill="red"
+          d="M510.723 149.946H296.374v-321.764H451.38l29.674 29.674 29.67 29.67v262.42z"
+        />
+        <path
+          fillOpacity={0}
+          stroke="#131432"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={12.95}
+          d="M510.723 149.946H296.374v-321.764H451.38l29.674 29.674 29.67 29.67v262.42z"
+        />
+      </g>
+      <filter
+        id="lord-icon-document_svg__b"
+        width="100%"
+        height="100%"
+        x="0%"
+        y="0%"
+        filterUnits="objectBoundingBox"
+      >
+        <feComponentTransfer in="SourceGraphic">
+          <feFuncA tableValues="1.0 0.0" type="table" />
+        </feComponentTransfer>
+      </filter>
+      <filter
+        id="lord-icon-document_svg__l"
+        width="300%"
+        height="300%"
+        x="-100%"
+        y="-100%"
+      >
+        <feGaussianBlur result="filter_result_0" stdDeviation="52.5 52.5" />
+      </filter>
+    </defs>
+    <g
+      clipPath="url(#lord-icon-document_svg__j)"
+      mask="url(#lord-icon-document_svg__k)"
+      style={{
+        display: 'block',
+      }}
+    >
+      <g
+        filter="url(#lord-icon-document_svg__l)"
+        transform="rotate(-94 262.075 8.435) scale(2.4)"
+      >
+        <path
+          fill="#4AE0EB"
+          d="M0-250c137.975 0 250 112.025 250 250S137.975 250 0 250-250 137.975-250 0-137.975-250 0-250z"
+          className="lord-icon-document_svg__design"
+        />
+        <path
+          fill="#CA5EED"
+          d="M113.242-295.384c97.478 0 176.5 79.022 176.5 176.5s-79.022 176.5-176.5 176.5c-43.948 0-74.396-34.057-105.29-60.631-37.631-32.369-71.21-62.338-71.21-115.869 0-97.478 79.022-176.5 176.5-176.5z"
+          className="lord-icon-document_svg__design"
+        />
+      </g>
+    </g>
+  </svg>
+);
+export default SvgLordIconDocument;
