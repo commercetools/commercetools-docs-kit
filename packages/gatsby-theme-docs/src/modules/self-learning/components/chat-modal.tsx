f1a174c1dde12076942e926aa2ed8682e996d436
Gabriele Antonini <gabriele.antonini@commercetools.com>
2023-08-10 08:20:04 +0200

    feat: add mode selector and expandible text input

--- packages/gatsby-theme-docs/src/modules/self-learning/components/chat-modal.tsx
+++ packages/gatsby-theme-docs/src/modules/self-learning/components/chat-modal.tsx
@@ -1,11 +1,27 @@
 import { useModalState, FormDialog } from '@commercetools-docs/ui-kit';
 import { useContext, useEffect } from 'react';
+import MultilineTextField from '@commercetools-uikit/multiline-text-field';
+import SelectField from '@commercetools-uikit/select-field';
+import MultilineTextInput from '@commercetools-uikit/multiline-text-input';
+import SpacingsStack from '@commercetools-uikit/spacings-stack';
 import { LearningContextApi, LearningContextState } from './learning-context';
+import { useFormik } from 'formik';
+import FirstName from './first-name';
 
 export type TProfileFormValues = {
-  firstName: string;
-  lastName: string;
-  company: string;
+  chatInput: string;
+  chatMode: string;
+};
+
+const submitChatMessage = (message: string, chatMode: string) => {
+  console.log('submitting message', message);
+};
+
+const renderError = (errorKey: string) => {
+  if (errorKey === 'missing') {
+    return 'Required field.';
+  }
+  return null;
 };
 
 const ChatModal = () => {
@@ -15,6 +31,28 @@ const ChatModal = () => {
   } = useContext(LearningContextState);
 
   const { closeModal, openModal, isModalOpen } = useModalState();
+
+  const formik = useFormik<TProfileFormValues>({
+    initialValues: {
+      chatInput: '',
+      chatMode: chatModal?.chatSelectedMode || '',
+    },
+    validate: (formikValues: TProfileFormValues) => {
+      const missingFields: Record<string, { missing: boolean }> = {};
+      if (MultilineTextInput.isEmpty(formikValues.chatInput)) {
+        missingFields.chatInput = { missing: true };
+      }
+      return missingFields;
+    },
+    onSubmit: async (formikValues: TProfileFormValues) => {
+      submitChatMessage(formikValues.chatInput, formikValues.chatMode);
+    },
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleChatModeChange = (event: any) => {
+    formik.setFieldValue('chatMode', event.target.value);
+  };
 
   // binds the modal state to the context isProfileModalOpen property
   useEffect(() => {
@@ -34,12 +72,44 @@ const ChatModal = () => {
       labelPrimary="Send"
       isOpen={isModalOpen}
       onClose={chatModal?.isDismissable ? () => closeChatModal() : undefined}
-      isPrimaryButtonDisabled={true}
+      isPrimaryButtonDisabled={
+        !(formik.isValid && formik.dirty) || formik.isSubmitting
+      }
       displaySecondaryButton={false}
       onPrimaryButtonClick={() => null}
     >
-      <p>Chat launched with selected mode: {chatModal?.chatSelectedMode}</p>
-      <p>Available modes: {chatModal?.chatAvailableModes}</p>
+      <SpacingsStack scale="l">
+        <h1>
+          Hey, <FirstName />
+        </h1>
+        <SelectField
+          title=""
+          value={formik.values.chatMode}
+          options={chatModal?.chatAvailableModes.map((mode) => ({
+            value: mode,
+            label: mode,
+          }))}
+          horizontalConstraint={7}
+          onChange={handleChatModeChange}
+        />
+        <p>I am Clippy, your assistat, how can I help you today?</p>
+        <MultilineTextField
+          key="chatInput"
+          name="chatInput"
+          title=""
+          placeholder="Send a message"
+          value={formik.values.chatInput}
+          touched={formik.touched.chatInput}
+          errors={
+            MultilineTextField.toFieldErrors<TProfileFormValues>(formik.errors)
+              .chatInput
+          }
+          defaultExpandMultilineText
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          renderError={renderError}
+        />
+      </SpacingsStack>
     </FormDialog>
   );
 };
