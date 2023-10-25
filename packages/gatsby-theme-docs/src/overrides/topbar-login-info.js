2ff5d6c499706fddfc10b15fe632918a852deb0e
Fady Fawzy <fady.fawzy19@hotmail.com>
2023-10-25 19:39:17 +0200

    feat: always showing the avatar when logged in (#1865)
    * feat: always showing the avatar when logged in
    
    * Create good-singers-lay.md
    
    * chore: addressing PR comments

--- packages/gatsby-theme-docs/src/overrides/topbar-login-info.js
+++ packages/gatsby-theme-docs/src/overrides/topbar-login-info.js
@@ -1,10 +1,14 @@
 import { useContext } from 'react';
 import ConfigContext from '../components/config-context';
 import UserProfile from '../modules/sso/components/avatar';
-
+import { LearningContextState } from '../modules/self-learning/components/learning-context';
 const LoginInfo = () => {
   const { hideLogin } = useContext(ConfigContext);
-  return hideLogin ? null : <UserProfile />;
+  const {
+    user: { profile },
+  } = useContext(LearningContextState);
+
+  return hideLogin && !profile ? null : <UserProfile />;
 };
 
 export default LoginInfo;
