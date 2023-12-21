40f4f7de8fb9850bbb55e57f07d16c54f0e6dc90
Timon Turak <timon.turak@commercetools.com>
2023-12-21 09:16:09 +0100

    refactor: adjust e2e-tests

--- cypress/e2e/docs-smoke-test/ai-assistant.cy.ts
+++ cypress/e2e/docs-smoke-test/ai-assistant.cy.ts
@@ -4,9 +4,15 @@ const LONG_TIMEOUT = 10000;
 describe('Ai Assistant', () => {
   it('should render login button when user is logged out', () => {
     cy.visit(`${URL_DOCS_SMOKE_TEST}components/ai-assistant`);
+    cy.get('[data-testid="ai-assistant-launch-button"]', {
+      timeout: LONG_TIMEOUT,
+    }).should('have.text', 'Start Assistant');
+    cy.get('[data-testid="ai-assistant-launch-button"]', {
+      timeout: LONG_TIMEOUT,
+    }).click();
     cy.get('[data-testid="quiz-login-button"]', { timeout: LONG_TIMEOUT })
       // Use should to assert the text content
-      .should('have.text', 'Log in or sign up to use the chat assistant');
+      .should('have.text', 'Log in to start the Assistant');
   });
 
   it('should render chat launch button when user is logged in and launch chat', () => {
@@ -17,6 +23,9 @@ describe('Ai Assistant', () => {
     cy.get('div[data-testid="mermaid-diagram"]'); // just ensuring javascript is loaded
     cy.findByText('Components').click();
     cy.findByText('Ai Assistant').click({ force: true });
+    cy.get('[data-testid="ai-assistant-launch-button"]', {
+      timeout: LONG_TIMEOUT,
+    }).click();
     cy.get('div[data-testid="quiz-login-button"]').click();
     cy.origin('https://auth.id.commercetools.com', () => {
       cy.get('input[id="username"]').type('test.user.chat@commercetools.com');
