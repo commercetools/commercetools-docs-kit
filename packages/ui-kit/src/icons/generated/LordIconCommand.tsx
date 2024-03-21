6f769deb3919e54e17e1bf3c16fcd0906ea60d68
Gabriele Antonini <gabriele.antonini@commercetools.com>
2024-03-21 10:19:51 +0100

    chore: flag command

--- packages/ui-kit/src/icons/generated/LordIconCommand.tsx
+++ packages/ui-kit/src/icons/generated/LordIconCommand.tsx
@@ -3,82 +3,29 @@ import type { SVGProps } from 'react';
 const SvgLordIconCommand = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={500}
-    height={500}
-    style={{
-      width: '100%',
-      height: '100%',
-      transform: 'translate3d(0,0,0)',
-      contentVisibility: 'visible',
-    }}
-    viewBox="0 0 500 500"
+    width={430}
+    height={430}
+    fill="none"
+    viewBox="0 0 430 430"
     {...props}
   >
+    <path
+      fill="url(#lord-icon-command_svg__a)"
+      d="M352.8 49H77.4C61.8 49 49 61.7 49 77.4v275.4c0 15.7 12.7 28.4 28.4 28.4h275.4c15.7 0 28.4-12.7 28.4-28.4V77.4c0-15.7-12.7-28.4-28.4-28.4ZM77.4 61h275.4c9 0 16.4 7.4 16.4 16.4v23H61v-23c0-9 7.4-16.4 16.4-16.4Zm275.4 308.2H77.4c-9.1 0-16.4-7.4-16.4-16.4V112.4h308.2v240.5c0 9-7.3 16.3-16.4 16.3ZM192 236.6c1.1 1.1 1.8 2.6 1.8 4.2s-.6 3.1-1.8 4.2l-52.6 52.6c-1.2 1.2-2.7 1.8-4.2 1.8s-3.1-.6-4.2-1.8c-2.3-2.3-2.3-6.1 0-8.5l48.4-48.4-48.4-48.4c-2.3-2.3-2.3-6.1 0-8.5 2.3-2.4 6.1-2.3 8.5 0l52.5 52.8ZM134 80.7c0-5 4-9 9-9h.1c5 0 9 4 9 9s-4 9-9 9h-.1c-5 0-9-4-9-9Zm-56.3 0c0-5 4-9 9-9h.1c5 0 9 4 9 9s-4 9-9 9h-.1c-5 0-9-4-9-9Zm28.1 0c0-5 4-9 9-9h.2c5 0 9 4 9 9s-4 9-9 9h-.1c-5 0-9.1-4-9.1-9Zm203.3 212.8c0 3.3-2.7 6-6 6h-90.2c-3.3 0-6-2.7-6-6s2.7-6 6-6h90.2c3.3 0 6 2.6 6 6Z"
+    />
     <defs>
-      <clipPath id="lord-icon-command_svg__b">
-        <path d="M0 0h500v500H0z" />
-      </clipPath>
-      <filter
-        id="lord-icon-command_svg__d"
-        width="300%"
-        height="300%"
-        x="-100%"
-        y="-100%"
-      >
-        <feGaussianBlur result="filter_result_0" stdDeviation="52.5 52.5" />
-      </filter>
-      <mask id="lord-icon-command_svg__c" mask-type="alpha">
-        <use href="#lord-icon-command_svg__a" />
-      </mask>
-      <g
+      <linearGradient
         id="lord-icon-command_svg__a"
-        fillOpacity={0}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        style={{
-          display: 'block',
-        }}
+        x1={57.331}
+        x2={372.91}
+        y1={57.33}
+        y2={372.908}
+        gradientUnits="userSpaceOnUse"
       >
-        <path
-          stroke="#121331"
-          strokeWidth={12}
-          d="M387.712 410.12H112.288c-12.375 0-22.407-10.033-22.407-22.408V112.288c0-12.375 10.032-22.407 22.407-22.407h275.424c12.375 0 22.407 10.032 22.407 22.407v275.424c0 12.375-10.032 22.408-22.407 22.408zm20.407-268.866H89.881"
-        />
-        <path
-          stroke="#08A88A"
-          strokeWidth={18}
-          d="M177.864 115.567H178m-56.432 0h.136m28.012 0h.136"
-        />
-        <path
-          stroke="#08A88A"
-          strokeWidth={12}
-          d="M247.76 328.335H338m-167.996 0 52.648-52.649-52.648-52.649"
-        />
-      </g>
+        <stop stopColor="#4E4ED7" />
+        <stop offset={1} stopColor="#4E4ED7" />
+      </linearGradient>
     </defs>
-    <g
-      clipPath="url(#lord-icon-command_svg__b)"
-      mask="url(#lord-icon-command_svg__c)"
-      style={{
-        display: 'block',
-      }}
-    >
-      <g
-        filter="url(#lord-icon-command_svg__d)"
-        transform="rotate(-94 262.075 8.435) scale(2.4)"
-      >
-        <path
-          fill="#00B39E"
-          d="M0-250c137.975 0 250 112.025 250 250S137.975 250 0 250-250 137.975-250 0-137.975-250 0-250z"
-          className="lord-icon-command_svg__design"
-        />
-        <path
-          fill="#068BDF"
-          d="M113.242-295.384c97.478 0 176.5 79.022 176.5 176.5s-79.022 176.5-176.5 176.5c-43.948 0-74.396-34.057-105.29-60.631-37.631-32.369-71.21-62.338-71.21-115.869 0-97.478 79.022-176.5 176.5-176.5z"
-          className="lord-icon-command_svg__design"
-        />
-      </g>
-    </g>
   </svg>
 );
 export default SvgLordIconCommand;
