4882da5ee026989235f41450e2af669ff1e5007d
Gabriele Antonini <gabriele.antonini@commercetools.com>
2024-03-21 11:08:28 +0100

    chore: completed icon refactoring

---
+++ packages/ui-kit/src/icons/generated/LordIconMmSpreadsheet.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgLordIconMmSpreadsheet = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={430}
+    height={430}
+    fill="none"
+    viewBox="0 0 430 430"
+    {...props}
+  >
+    <path
+      fill="url(#lord-icon-mm-spreadsheet_svg__a)"
+      d="M352.8 49H77.4C61.8 49 49 61.7 49 77.4v275.4c0 15.7 12.7 28.4 28.4 28.4h275.4c15.7 0 28.4-12.7 28.4-28.4V77.4c0-15.7-12.7-28.4-28.4-28.4ZM77.4 61h275.4c9 0 16.4 7.4 16.4 16.4v23H61v-23c0-9 7.4-16.4 16.4-16.4Zm275.4 308.2H77.4c-9.1 0-16.4-7.4-16.4-16.4V112.4h308.2v240.5c0 9-7.3 16.3-16.4 16.3ZM137 80.7c0-5 4-9 9-9h.1c5 0 9 4 9 9s-4 9-9 9h-.1c-5 0-9-4-9-9Zm-56.3 0c0-5 4-9 9-9h.1c5 0 9 4 9 9s-4 9-9 9h-.1c-5 0-9-4-9-9Zm28.1 0c0-5 4-9 9-9h.2c5 0 9 4 9 9s-4 9-9 9h-.1c-5 0-9.1-4-9.1-9Zm228.1 55.7H93.4c-3.3 0-6 2.7-6 6V337.5c0 3.3 2.7 6 6 6h243.5c3.3 0 6-2.7 6-6V142.4c0-3.3-2.7-6-6-6ZM156.6 282.7v-36.8h81.1v36.8h-81.1Zm81.1 12v36.8h-81.1v-36.8h81.1Zm-81.1-97.5h81.1V234h-81.1v-36.8Zm93.1 0h81.1V234h-81.1v-36.8Zm-11.9-12h-81.1v-36.8h81.1v36.8Zm-93.2 48.7H99.4v-36.8h45.2v36.8Zm-45.2 12h45.2v36.8H99.4v-36.8Zm150.3 0h81.1v36.8h-81.1v-36.8Zm81.2-60.7h-81.1v-36.8h81.1v36.8Zm-186.3-36.8v36.8H99.4v-36.8h45.2ZM99.4 294.7h45.2v36.8H99.4v-36.8Zm150.3 36.7v-36.8h81.1v36.8h-81.1Z"
+    />
+    <defs>
+      <linearGradient
+        id="lord-icon-mm-spreadsheet_svg__a"
+        x1={57.331}
+        x2={372.91}
+        y1={57.33}
+        y2={372.908}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#4E4ED7" />
+        <stop offset={1} stopColor="#4E4ED7" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+export default SvgLordIconMmSpreadsheet;
---
+++ packages/ui-kit/src/icons/generated/LordIconMmUnlocked.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgLordIconMmUnlocked = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={430}
+    height={430}
+    fill="none"
+    viewBox="0 0 430 430"
+    {...props}
+  >
+    <path
+      fill="url(#lord-icon-mm-unlocked_svg__a)"
+      fillRule="evenodd"
+      d="M174 250.5a6 6 0 0 1 6-6h70a6 6 0 0 1 6 6v70a6 6 0 0 1-6 6h-70a6 6 0 0 1-6-6v-70Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="url(#lord-icon-mm-unlocked_svg__b)"
+      fillRule="evenodd"
+      d="M119 125.5c0-53.02 42.981-96 96-96s96 42.98 96 96v59h34a6 6 0 0 1 6 6v150c0 25.405-20.595 46-46 46H125c-25.405 0-46-20.595-46-46v-150a6 6 0 0 1 6-6h34v-59Zm12 59h168v-59c0-46.392-37.608-84-84-84s-84 37.608-84 84v59Zm-40 156v-144h248v144c0 18.778-15.222 34-34 34H125c-18.778 0-34-15.222-34-34Z"
+      clipRule="evenodd"
+    />
+    <defs>
+      <linearGradient
+        id="lord-icon-mm-unlocked_svg__a"
+        x1={99.564}
+        x2={305.647}
+        y1={146.071}
+        y2={291.68}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#4E4ED7" />
+        <stop offset={1} stopColor="#4E4ED7" />
+      </linearGradient>
+      <linearGradient
+        id="lord-icon-mm-unlocked_svg__b"
+        x1={99.564}
+        x2={305.647}
+        y1={146.071}
+        y2={291.68}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#4E4ED7" />
+        <stop offset={1} stopColor="#4E4ED7" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+export default SvgLordIconMmUnlocked;
