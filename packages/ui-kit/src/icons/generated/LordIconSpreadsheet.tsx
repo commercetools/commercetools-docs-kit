2f1f79ff6d6bbd56b0672b2f953475070fc62567
Gabriele Antonini <gabriele.antonini@commercetools.com>
2024-03-21 10:15:26 +0100

    chore: flag spreadsheet

--- packages/ui-kit/src/icons/generated/LordIconSpreadsheet.tsx
+++ packages/ui-kit/src/icons/generated/LordIconSpreadsheet.tsx
@@ -3,107 +3,29 @@ import type { SVGProps } from 'react';
 const SvgLordIconSpreadsheet = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={500}
-    height={500}
-    style={{
-      width: '100%',
-      height: '100%',
-      transform: 'translate3d(0,0,0)',
-      contentVisibility: 'visible',
-    }}
-    viewBox="0 0 500 500"
+    width={430}
+    height={430}
+    fill="none"
+    viewBox="0 0 430 430"
     {...props}
   >
+    <path
+      fill="url(#lord-icon-spreadsheet_svg__a)"
+      d="M352.8 49H77.4C61.8 49 49 61.7 49 77.4v275.4c0 15.7 12.7 28.4 28.4 28.4h275.4c15.7 0 28.4-12.7 28.4-28.4V77.4c0-15.7-12.7-28.4-28.4-28.4ZM77.4 61h275.4c9 0 16.4 7.4 16.4 16.4v23H61v-23c0-9 7.4-16.4 16.4-16.4Zm275.4 308.2H77.4c-9.1 0-16.4-7.4-16.4-16.4V112.4h308.2v240.5c0 9-7.3 16.3-16.4 16.3ZM137 80.7c0-5 4-9 9-9h.1c5 0 9 4 9 9s-4 9-9 9h-.1c-5 0-9-4-9-9Zm-56.3 0c0-5 4-9 9-9h.1c5 0 9 4 9 9s-4 9-9 9h-.1c-5 0-9-4-9-9Zm28.1 0c0-5 4-9 9-9h.2c5 0 9 4 9 9s-4 9-9 9h-.1c-5 0-9.1-4-9.1-9Zm228.1 55.7H93.4c-3.3 0-6 2.7-6 6V337.5c0 3.3 2.7 6 6 6h243.5c3.3 0 6-2.7 6-6V142.4c0-3.3-2.7-6-6-6ZM156.6 282.7v-36.8h81.1v36.8h-81.1Zm81.1 12v36.8h-81.1v-36.8h81.1Zm-81.1-97.5h81.1V234h-81.1v-36.8Zm93.1 0h81.1V234h-81.1v-36.8Zm-11.9-12h-81.1v-36.8h81.1v36.8Zm-93.2 48.7H99.4v-36.8h45.2v36.8Zm-45.2 12h45.2v36.8H99.4v-36.8Zm150.3 0h81.1v36.8h-81.1v-36.8Zm81.2-60.7h-81.1v-36.8h81.1v36.8Zm-186.3-36.8v36.8H99.4v-36.8h45.2ZM99.4 294.7h45.2v36.8H99.4v-36.8Zm150.3 36.7v-36.8h81.1v36.8h-81.1Z"
+    />
     <defs>
-      <clipPath id="lord-icon-spreadsheet_svg__c">
-        <path d="M0 0h500v500H0z" />
-      </clipPath>
-      <clipPath id="lord-icon-spreadsheet_svg__b">
-        <path d="M0 0h500v500H0z" />
-      </clipPath>
-      <filter
-        id="lord-icon-spreadsheet_svg__e"
-        width="300%"
-        height="300%"
-        x="-100%"
-        y="-100%"
-      >
-        <feGaussianBlur result="filter_result_0" stdDeviation="52.5 52.5" />
-      </filter>
-      <mask id="lord-icon-spreadsheet_svg__d" mask-type="alpha">
-        <use href="#lord-icon-spreadsheet_svg__a" />
-      </mask>
-      <g
+      <linearGradient
         id="lord-icon-spreadsheet_svg__a"
-        fillOpacity={0}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        clipPath="url(#lord-icon-spreadsheet_svg__b)"
-        style={{
-          display: 'block',
-        }}
+        x1={57.331}
+        x2={372.91}
+        y1={57.33}
+        y2={372.908}
+        gradientUnits="userSpaceOnUse"
       >
-        <g
-          style={{
-            display: 'block',
-          }}
-        >
-          <path
-            stroke="#121331"
-            strokeWidth={12}
-            d="M387.712 410.119H112.288c-12.375 0-22.407-10.032-22.407-22.407V112.288c0-12.375 10.032-22.407 22.407-22.407h275.424c12.375 0 22.407 10.032 22.407 22.407v275.424c0 12.375-10.032 22.407-22.407 22.407zm20.407-268.865H89.881"
-          />
-          <path
-            stroke="#08A789"
-            strokeWidth={18}
-            d="M180.864 115.567H181m-56.432 0h.136m28.012 0h.136"
-          />
-        </g>
-        <g
-          strokeWidth={12}
-          style={{
-            display: 'block',
-          }}
-        >
-          <path
-            stroke="#121331"
-            d="M185.5 226.084v146.25m93.125-146.25v146.25m93.125-146.25v146.25h-243.5V226.002"
-          />
-          <path
-            stroke="#08A789"
-            d="M128.25 226.084v-48.75h243.5v48.75m0 0h-243.5"
-          />
-          <path
-            stroke="#121331"
-            d="M371.75 274.834h-243.5m243.5 48.75h-243.5"
-          />
-          <path stroke="#08A789" d="M185.5 177.334v48.75m93.126-48.75v48.75" />
-        </g>
-      </g>
+        <stop stopColor="#4E4ED7" />
+        <stop offset={1} stopColor="#4E4ED7" />
+      </linearGradient>
     </defs>
-    <g
-      clipPath="url(#lord-icon-spreadsheet_svg__c)"
-      mask="url(#lord-icon-spreadsheet_svg__d)"
-      style={{
-        display: 'block',
-      }}
-    >
-      <g
-        filter="url(#lord-icon-spreadsheet_svg__e)"
-        transform="rotate(-94 262.075 8.435) scale(2.4)"
-      >
-        <path
-          fill="#00B39E"
-          d="M0-250c137.975 0 250 112.025 250 250S137.975 250 0 250-250 137.975-250 0-137.975-250 0-250z"
-          className="lord-icon-spreadsheet_svg__design"
-        />
-        <path
-          fill="#068BDF"
-          d="M113.242-295.384c97.478 0 176.5 79.022 176.5 176.5s-79.022 176.5-176.5 176.5c-43.948 0-74.396-34.057-105.29-60.631-37.631-32.369-71.21-62.338-71.21-115.869 0-97.478 79.022-176.5 176.5-176.5z"
-          className="lord-icon-spreadsheet_svg__design"
-        />
-      </g>
-    </g>
   </svg>
 );
 export default SvgLordIconSpreadsheet;
