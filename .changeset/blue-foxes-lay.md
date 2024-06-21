2f2ed2bda66a69222545e3a7d3d310714703daba
Gabriele Antonini <106664971+gabriele-ct@users.noreply.github.com>
2024-06-21 14:20:55 +0200

    chore: make release notes tag responsive (#2019)
    * chore: release notes tags responsive
    
    * chore: release notes tags responsive
    
    * chore: changeset

---
+++ .changeset/blue-foxes-lay.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-docs/gatsby-theme-docs': patch
+---
+
+Make release notes tags responsive
--- packages/gatsby-theme-docs/src/layouts/internals/layout-release-note-body.js
+++ packages/gatsby-theme-docs/src/layouts/internals/layout-release-note-body.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import SpacingsStack from '@commercetools-uikit/spacings-stack';
-import SpacingsInline from '@commercetools-uikit/spacings-inline';
 import Stamp from '@commercetools-uikit/stamp';
 import { designSystem } from '@commercetools-docs/ui-kit';
 import { useSiteData } from '../../hooks/use-site-data';
@@ -37,6 +36,20 @@ const CustomStamp = styled.div`
 const SeparatorLine = styled.div`
   height: ${designSystem.dimensions.heights.separatorLine};
   border-left: 1px solid ${designSystem.colors.light.surfaceSecondary3};
+  @media screen and (${designSystem.dimensions.viewports.mobile}) {
+    display: none;
+  }
+`;
+
+const TagsWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  gap: ${designSystem.dimensions.spacings.s};
+  @media screen and (${designSystem.dimensions.viewports.mobile}) {
+    flex-direction: column;
+    width: auto;
+  }
 `;
 
 const ReleaseNoteBody = (props) => {
@@ -53,47 +66,51 @@ const ReleaseNoteBody = (props) => {
 
   return (
     <SpacingsStack scale="m">
-      <SpacingsStack scale="s">
+      <SpacingsStack scale="s" alignItems="flex-start">
         <DateElement>{props.date}</DateElement>
-        <div
-          style={designSystem.tokensToCssVars({
-            fontSizeDefault: designSystem.typography.fontSizes.extraSmall,
-            fontSizeForStamp: designSystem.typography.fontSizes.extraSmall,
-            // Override the `critical` style which is used for the "fix" type
-            colorError95:
-              designSystem.colors.light.surfaceForReleaseNoteTypeFix,
-            colorError: designSystem.colors.light.borderForReleaseNoteTypeFix,
-          })}
-        >
-          <SpacingsInline alignItems="center">
-            {!hideProductLabels && (
-              <>
-                {product && (
-                  <CustomStamp>
-                    <span>{product}</span>
-                  </CustomStamp>
-                )}
-                {/* If product and productArea have the same value, we only want to show it once. */}
-                {productArea && productArea !== product && (
-                  <CustomStamp>
-                    <span>{productArea}</span>
-                  </CustomStamp>
-                )}
-                <SeparatorLine />
-              </>
-            )}
-            {releaseNoteType.map((type) => {
-              return (
+
+        <TagsWrapper>
+          {!hideProductLabels && (
+            <>
+              {product && (
+                <CustomStamp>
+                  <span>{product}</span>
+                </CustomStamp>
+              )}
+              {/* If product and productArea have the same value, we only want to show it once. */}
+              {productArea && productArea !== product && (
+                <CustomStamp>
+                  <span>{productArea}</span>
+                </CustomStamp>
+              )}
+              <SeparatorLine />
+            </>
+          )}
+          {releaseNoteType.map((type) => {
+            return (
+              <div
+                key={type}
+                style={designSystem.tokensToCssVars({
+                  fontSizeDefault: designSystem.typography.fontSizes.extraSmall,
+                  fontSizeForStamp:
+                    designSystem.typography.fontSizes.extraSmall,
+                  // Override the `critical` style which is used for the "fix" type
+                  colorError95:
+                    designSystem.colors.light.surfaceForReleaseNoteTypeFix,
+                  colorError:
+                    designSystem.colors.light.borderForReleaseNoteTypeFix,
+                })}
+              >
                 <Stamp
-                  key={type}
                   isCondensed
                   tone={mapTypeToTone(type)}
                   label={mapTypeToLabel(type)}
                 />
-              );
-            })}
-          </SpacingsInline>
-        </div>
+              </div>
+            );
+          })}
+        </TagsWrapper>
+
         {props.topics.length > 0 && (
           <Topics>
             {props.topics.map((topic) => (
