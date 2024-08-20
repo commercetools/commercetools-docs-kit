522ec0f99028a0747b52cb81e461631f2e51d769
Gabriele Antonini <106664971+gabriele-ct@users.noreply.github.com>
2024-08-20 14:10:52 +0200

    chore: mega menu prep changes for glossary (#2047)
    * chore: mega menu prep changes for glossary
    
    * chore: add glossary lord icon
    
    * chore: changeset
    
    * chore: apply suggested changes
    
    ---------
    
    Co-authored-by: Timon Rey Turak <31767369+timonrey@users.noreply.github.com>
    Co-authored-by: Timon Turak <timon.turak@commercetools.com>

---
+++ packages/ui-kit/src/icons/generated/LordIconMmGlossary.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgLordIconMmGlossary = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={430}
+    height={430}
+    fill="none"
+    viewBox="0 0 430 430"
+    {...props}
+  >
+    <path
+      fill="url(#lord-icon-mm-glossary_svg__a)"
+      d="M251 211a6 6 0 1 0 0 12h32l-37.8 50.4a6 6 0 0 0 4.8 9.6h46a6 6 0 1 0 0-12h-34l37.8-50.4a6 6 0 0 0-4.8-9.6h-44Z"
+    />
+    <path
+      fill="url(#lord-icon-mm-glossary_svg__b)"
+      d="M191 247.5a6 6 0 0 1 6-6h35a6 6 0 1 1 0 12h-35a6 6 0 0 1-6-6Z"
+    />
+    <path
+      fill="url(#lord-icon-mm-glossary_svg__c)"
+      fillRule="evenodd"
+      d="M149.623 211a6 6 0 0 0-5.674 4.05l-20.623 60a6 6 0 1 0 11.348 3.9l4.28-12.45h30.09l4.282 12.451a6 6 0 0 0 11.348-3.902l-20.637-60a6 6 0 0 0-5.674-4.049h-8.74Zm15.293 43.5h-21.838l10.828-31.5h.176l10.834 31.5Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="url(#lord-icon-mm-glossary_svg__d)"
+      fillRule="evenodd"
+      d="M74 61.661C74 40.9 90.9 24 111.661 24H350a6 6 0 0 1 6 6v370a6 6 0 0 1-6 6H111.661c-10.414 0-19.786-4.128-26.66-11.002C78.274 388.269 74 378.908 74 368.454V61.661Zm37.661 37.662H344V394H111.661c-7.149 0-13.492-2.805-18.174-7.487C88.892 381.918 86 375.564 86 368.454V89.2c6.731 6.277 15.757 10.123 25.661 10.123Zm0-63.323C97.527 36 86 47.527 86 61.661c0 14.135 11.527 25.662 25.661 25.662H344V36H111.661Z"
+      clipRule="evenodd"
+    />
+    <defs>
+      <linearGradient
+        id="lord-icon-mm-glossary_svg__a"
+        x1={70.321}
+        x2={408.962}
+        y1={33.233}
+        y2={273.512}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#4e4ed7" />
+        <stop offset={1} stopColor="#4e4ed7" />
+      </linearGradient>
+      <linearGradient
+        id="lord-icon-mm-glossary_svg__b"
+        x1={70.321}
+        x2={408.962}
+        y1={33.233}
+        y2={273.512}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#4e4ed7" />
+        <stop offset={1} stopColor="#4e4ed7" />
+      </linearGradient>
+      <linearGradient
+        id="lord-icon-mm-glossary_svg__c"
+        x1={70.321}
+        x2={408.962}
+        y1={33.233}
+        y2={273.512}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#4e4ed7" />
+        <stop offset={1} stopColor="#4e4ed7" />
+      </linearGradient>
+      <linearGradient
+        id="lord-icon-mm-glossary_svg__d"
+        x1={70.321}
+        x2={408.962}
+        y1={33.233}
+        y2={273.512}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#4e4ed7" />
+        <stop offset={1} stopColor="#4e4ed7" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+export default SvgLordIconMmGlossary;
