a2d9d406ce4145226338458c3af103242eb42a9f
Gabriele Antonini <gabriele.antonini@commercetools.com>
2024-09-05 15:02:49 +0200

    chore: add e2e tests

---
+++ cypress/e2e/docs-smoke-test/site-overrides.cy.ts
@@ -0,0 +1,27 @@
+import { URL_DOCS_SMOKE_TEST } from '../../support/urls';
+
+const overridesPageUrl = `${URL_DOCS_SMOKE_TEST}views/overrides`;
+
+describe('Site overrides', () => {
+  beforeEach(() => {
+    cy.visit(overridesPageUrl);
+  });
+  describe('when the page is rendered', () => {
+    it('should render "Custom Site Title" as site title in the sidebar', () => {
+      cy.get('span[id="site-title"]').contains('Custom Site Title');
+      cy.get('span#site-title')
+        .closest('a')
+        .should('have.attr', 'href', '/../views/custom-anchor');
+    });
+    it('should render "Custom Site Title" as site title breadcrumb section', () => {
+      cy.get('div[id="top-menu-switcher"] > div > span').contains(
+        'Custom Site Title'
+      );
+    });
+    it('should render "Custom Breadcrumb" as root breadcrumb', () => {
+      cy.get('div[id="top-menu-switcher"] > div > div').contains(
+        'Custom Breadcrumb'
+      );
+    });
+  });
+});
