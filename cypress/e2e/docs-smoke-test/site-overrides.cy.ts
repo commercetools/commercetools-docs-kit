3815947dfc0cd72b898a3c724d5458e72399c8f4
Gabriele Antonini <gabriele.antonini@commercetools.com>
2024-09-05 16:07:03 +0200

    chore: fix test case

--- cypress/e2e/docs-smoke-test/site-overrides.cy.ts
+++ cypress/e2e/docs-smoke-test/site-overrides.cy.ts
@@ -7,11 +7,12 @@ describe('Site overrides', () => {
     cy.visit(overridesPageUrl);
   });
   describe('when the page is rendered', () => {
-    it('should render "Custom Site Title" as site title in the sidebar', () => {
+    it('should render "Custom Site Title" as site title and custom link in the sidebar', () => {
       cy.get('span[id="site-title"]').contains('Custom Site Title');
       cy.get('span#site-title')
         .closest('a')
-        .should('have.attr', 'href', '/../views/custom-anchor');
+        .should('have.attr', 'href')
+        .and('include', '/../views/custom-anchor');
     });
     it('should render "Custom Site Title" as site title breadcrumb section', () => {
       cy.get('div[id="top-menu-switcher"] > div > span').contains(
