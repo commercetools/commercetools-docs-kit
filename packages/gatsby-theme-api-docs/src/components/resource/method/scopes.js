b5f0f7f3462ca60189e4829df1445c6aabccdd95
Colin Rosati <colin.rosati@commercetools.com>
2024-09-10 14:43:44 +0200

    feat: format scope in columns

--- packages/gatsby-theme-api-docs/src/components/resource/method/scopes.js
+++ packages/gatsby-theme-api-docs/src/components/resource/method/scopes.js
@@ -1,19 +1,34 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import SpacingsStack from '@commercetools-uikit/spacings-stack';
 import { Markdown } from '@commercetools-docs/ui-kit';
 import Title from './title';
+import { css } from '@emotion/react';
+
+const scopesGridStyle = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 0.5rem;
+`;
 
 const Scopes = (props) => {
+  const hasMultiColumnScope = props.scopes.length >= 10;
+  const canAppendComma = (index) =>
+    !hasMultiColumnScope && index < props.scopes.length - 1;
+
   return (
     <SpacingsStack scale="xs">
       <Title>OAuth 2.0 Scopes:</Title>
 
-      <div>
+      <div css={hasMultiColumnScope && scopesGridStyle}>
         {props.scopes.map((scope, index) => (
-          <Markdown.InlineCodeWithoutBox key={scope}>
+          <Markdown.InlineCodeWithoutBox
+            key={scope}
+            css={css`
+              word-break: break-all;
+            `}
+          >
             {scope}
-            {index < props.scopes.length - 1 && ' , '}
+            {canAppendComma(index) && ' , '}
           </Markdown.InlineCodeWithoutBox>
         ))}
       </div>
