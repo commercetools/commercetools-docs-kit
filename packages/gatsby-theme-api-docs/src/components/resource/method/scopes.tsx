8ead14ebd5bff945347f21ba4e3bfc78161b862c
Colin Rosati <colin.rosati@commercetools.com>
2024-09-11 09:14:16 +0200

    chore: refactor column with no limit

--- packages/gatsby-theme-api-docs/src/components/resource/method/scopes.tsx
+++ packages/gatsby-theme-api-docs/src/components/resource/method/scopes.tsx
@@ -1,42 +1,44 @@
 import SpacingsStack from '@commercetools-uikit/spacings-stack';
 import { Markdown } from '@commercetools-docs/ui-kit';
 import Title from './title';
-import { css } from '@emotion/react';
-
-const scopesGridStyle = css`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 0.5rem;
-`;
+import styled from '@emotion/styled';
 
 type ScopesProps = {
   scopes: string[];
 };
 
-const Scopes = (props: ScopesProps) => {
-  const hasMultiColumnScope = props.scopes.length >= 10;
-  const canAppendComma = (index: number) =>
-    !hasMultiColumnScope && index < props.scopes.length - 1;
+const Container = styled.div`
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 0.5rem;
+    > code {
+      word-break: break-all;
+    }
+  }
+`;
 
-  return (
-    <SpacingsStack scale="xs">
-      <Title>OAuth 2.0 Scopes:</Title>
+const Text = styled(Markdown.InlineCodeWithoutBox)`
+  :not(:last-child)&::after {
+    content: ', ';
+  }
+  @media only screen and (min-width: 768px) {
+    &::after {
+      content: ' ';
+    }
+  }
+`;
 
-      <div css={hasMultiColumnScope && scopesGridStyle}>
-        {props.scopes.map((scope, index) => (
-          <Markdown.InlineCodeWithoutBox
-            key={scope}
-            css={css`
-              word-break: break-all;
-            `}
-          >
-            {scope}
-            {canAppendComma(index) && ' , '}
-          </Markdown.InlineCodeWithoutBox>
-        ))}
-      </div>
-    </SpacingsStack>
-  );
-};
+const Scopes = (props: ScopesProps) => (
+  <SpacingsStack scale="xs">
+    <Title>OAuth 2.0 Scopes:</Title>
+
+    <Container>
+      {props.scopes.map((scope, index) => (
+        <Text key={index}>{scope}</Text>
+      ))}
+    </Container>
+  </SpacingsStack>
+);
 
 export default Scopes;
