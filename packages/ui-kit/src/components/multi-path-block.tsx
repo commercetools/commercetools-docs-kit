7d6effc76e72f65ffde7fc4c9345da3ee33ff6d4
Colin Rosati <colin.rosati@commercetools.com>
2024-10-01 15:25:19 +0200

    feat: scroll behaviour

--- packages/ui-kit/src/components/multi-path-block.tsx
+++ packages/ui-kit/src/components/multi-path-block.tsx
@@ -1,8 +1,13 @@
 import styled from '@emotion/styled';
-import React, { ReactElement, ReactNode, useEffect, useState } from 'react';
+import React, {
+  forwardRef,
+  ReactElement,
+  ReactNode,
+  useEffect,
+  useState,
+} from 'react';
 import { designSystem } from '..';
 import Text from '@commercetools-uikit/text';
-import Spacings from '@commercetools-uikit/spacings';
 import { Theme, Interpolation, css, SerializedStyles } from '@emotion/react';
 import useSelectedPath from '../hooks/use-selected-path';
 
@@ -144,11 +149,59 @@ const TabHeader = (props: TTabHeaderProps) => {
   );
 };
 
+const scrollFocusedTab = (
+  elem: React.ForwardedRef<HTMLDivElement>,
+  rightToLeft: boolean
+) => {
+  if (!elem || !('current' in elem)) {
+    return;
+  }
+
+  if (rightToLeft) {
+    elem.current?.scrollTo({
+      left: elem.current.offsetLeft + elem.current.offsetWidth,
+      behavior: 'smooth',
+    });
+  } else {
+    elem.current?.scrollTo({
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+};
+
+const ScrollChevronWrapper = styled.span`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: ${designSystem.dimensions.spacings.big};
+`;
+
+const Chevron = styled.div`
+  height: 1rem;
+`;
+
+type OverFlowScrollProps = {
+  rightToLeft: boolean;
+  isVisible: boolean;
+};
+
+const OverFlowScroll = forwardRef<HTMLDivElement, OverFlowScrollProps>(
+  ({ rightToLeft, isVisible }, ref) => {
+    return (
+      <ScrollChevronWrapper onClick={() => scrollFocusedTab(ref, rightToLeft)}>
+        {isVisible && <Chevron>{rightToLeft ? '>' : '<'}</Chevron>}
+      </ScrollChevronWrapper>
+    );
+  }
+);
+
+OverFlowScroll.displayName = 'OverFlowScroll';
+
 const SelectorsContainer = styled.div`
+  display: flex;
   border-bottom: 1px solid ${designSystem.colors.light.borderPrimary};
-  padding-top: 8px;
-  padding-left: 16px;
-  overflow-x: auto;
+  overflow-x: hidden;
 `;
 
 const ComponentWrapper = styled.div`
@@ -160,14 +213,74 @@ const PathsContainer = styled.div`
   padding: 10px;
 `;
 
+const ScrollWrapper = styled.span`
+  display: inline-flex;
+  align-items: flex-end;
+  overflow-x: auto;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+`;
+
 const MultiPathBlock = (props: MultiPathBlockProps) => {
   const labelSyncItems = extractLabelSyncPair(props.children);
   const [selected, setSelected] = React.useState<LabelSyncPair>(
     labelSyncItems[0]
   );
   const [activePathIndex, setActivePathIndex] = useState<number>(0);
+  const [displayStartScroll, setDisplayStartScroll] = React.useState(false);
+  const [displayEndScroll, setDisplayEndScroll] = React.useState(false);
 
   const { selectedPath, updateSelectedPath } = useSelectedPath();
+
+  const tabsRef = React.useRef<HTMLDivElement>(null);
+  const tabListRef = React.useRef<HTMLDivElement>(null);
+
+  const handleScrollButtonStart = (entries: IntersectionObserverEntry[]) => {
+    setDisplayStartScroll(!entries[0].isIntersecting);
+  };
+  const handleScrollButtonEnd = (entries: IntersectionObserverEntry[]) => {
+    setDisplayEndScroll(!entries[entries.length - 1].isIntersecting);
+  };
+
+  useEffect(() => {
+    if (!tabListRef.current?.children) {
+      return;
+    }
+
+    const tabListChildren = Array.from(tabListRef.current?.children);
+
+    if (
+      typeof IntersectionObserver !== 'undefined' &&
+      labelSyncItems.length > 0
+    ) {
+      const firstTab = tabListChildren[0];
+      const lastTab = tabListChildren[tabListChildren.length - 1];
+      const observerOptions = {
+        root: tabsRef.current,
+        threshold: 0.99,
+      };
+
+      const firstObserver = new IntersectionObserver(
+        handleScrollButtonStart,
+        observerOptions
+      );
+      firstObserver.observe(firstTab);
+
+      const lastObserver = new IntersectionObserver(
+        handleScrollButtonEnd,
+        observerOptions
+      );
+      lastObserver.observe(lastTab);
+
+      return () => {
+        firstObserver.disconnect();
+        lastObserver.disconnect();
+      };
+    }
+  }, [labelSyncItems.length]);
 
   useEffect(() => {
     if (selectedPath) {
@@ -210,8 +323,13 @@ const MultiPathBlock = (props: MultiPathBlockProps) => {
 
   return (
     <ComponentWrapper>
-      <SelectorsContainer role="tablist">
-        <Spacings.Inline alignItems="flex-end">
+      <SelectorsContainer role="tablist" ref={tabsRef}>
+        <OverFlowScroll
+          rightToLeft={false}
+          isVisible={displayStartScroll}
+          ref={tabListRef}
+        />
+        <ScrollWrapper ref={tabListRef}>
           {labelSyncItems.map((labelSyncItem, index) => (
             <TabHeader
               index={index}
@@ -221,7 +339,12 @@ const MultiPathBlock = (props: MultiPathBlockProps) => {
               onClick={(e) => onTabHeaderClick(e)(labelSyncItem)}
             />
           ))}
-        </Spacings.Inline>
+        </ScrollWrapper>
+        <OverFlowScroll
+          rightToLeft={true}
+          isVisible={displayEndScroll}
+          ref={tabListRef}
+        />
       </SelectorsContainer>
       <PathsContainer>
         <TabContentChildren activePathIndex={activePathIndex}>
