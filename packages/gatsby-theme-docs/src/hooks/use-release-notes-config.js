fd5c04d66b95c2bcdb25ce232fbbacb0cff45f72
Gabriele Antonini <gabriele.antonini@commercetools.com>
2024-10-04 11:02:53 +0200

    chore: don't select any product area in doc microsite release notes

--- packages/gatsby-theme-docs/src/hooks/use-release-notes-config.js
+++ packages/gatsby-theme-docs/src/hooks/use-release-notes-config.js
@@ -2,6 +2,7 @@ import { buildReleaseNotesQueryString } from '../utils/release-notes';
 import { useSiteData } from './use-site-data';
 
 const OTHER_GROUP_SITE_PREFIXES = []; // let's use pathPrefix to identify websites that point to others tab (works only on prod), docs-team asked to point all to product sofar
+const NO_PRODUCT_AREA_SITE_PREFIXES = ['/docs']; // when matching these site prefixes, the productArea param will not be added to the release notes url
 const RELEASE_NOTES_BASE_URL = '/docs/release-notes'; // only prod url
 
 const useReleaseNotesConfig = () => {
@@ -16,7 +17,7 @@ const useReleaseNotesConfig = () => {
     const queryString = buildReleaseNotesQueryString(
       OTHER_GROUP_SITE_PREFIXES.includes(pathPrefix) ? 'other' : 'product',
       product,
-      title
+      NO_PRODUCT_AREA_SITE_PREFIXES.includes(pathPrefix) ? undefined : title
     );
     return `${RELEASE_NOTES_BASE_URL}?${queryString}`;
   };
