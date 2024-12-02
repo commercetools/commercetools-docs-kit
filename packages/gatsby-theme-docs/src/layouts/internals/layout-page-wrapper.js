901814f7b0a70d92707372a7737cba9d3b4a2baf
colinrosati <colin.rosati@commercetools.com>
2024-12-02 14:24:58 +0100

    Revert "chore: remove styling"
    This reverts commit 9dca1ed130d6505607e3159be7bcef616b96b62a.

--- packages/gatsby-theme-docs/src/layouts/internals/layout-page-wrapper.js
+++ packages/gatsby-theme-docs/src/layouts/internals/layout-page-wrapper.js
@@ -1,5 +1,12 @@
 import styled from '@emotion/styled';
+import { designSystem } from '@commercetools-docs/ui-kit';
 
-const LayoutPageWrapper = styled.article``;
+const LayoutPageWrapper = styled.article`
+  grid-area: page;
+  display: block;
+  max-width: 100vw;
+  box-shadow: ${designSystem.tokens.shadowForPageContent};
+  z-index: ${designSystem.dimensions.stacks.base};
+`;
 
 export default LayoutPageWrapper;
